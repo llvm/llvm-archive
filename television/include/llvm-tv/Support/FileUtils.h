c19740ec1aa9d989fd8673c98a66da5d373f0d16
Misha Brukman <brukman+llvm@gmail.com>
2004-04-13 17:49:50 +0000

    Added ability to get a listing of a directory.
    llvm-svn: 12898

--- television/include/llvm-tv/Support/FileUtils.h
+++ television/include/llvm-tv/Support/FileUtils.h
@@ -15,10 +15,17 @@
 #define LLVM_TV_FILEUTILS_H
 
 #include <string>
+#include <vector>
 
 namespace llvm {
 
+/// GetNumFilesInDir - returns a count of files in directory
+///
 unsigned GetNumFilesInDir(const std::string &path);
+
+/// GetFilesInDir - populate vector with a listing of files in directory
+///
+void GetFilesInDir(const std::string &path, std::vector<std::string> &list);
 
 };
 

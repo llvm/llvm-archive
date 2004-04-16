e1683e924d6b3c726d9eceef4e63ab6a88d609a5
Misha Brukman <brukman+llvm@gmail.com>
2004-04-16 19:16:57 +0000

    Save pid of llvm-tv into a file.
    llvm-svn: 13004

--- television/tools/llvm-tv/TVApplication.cpp
+++ television/tools/llvm-tv/TVApplication.cpp
@@ -7,11 +7,21 @@
 #include "wx/wx.h"
 #include "TVApplication.h"
 #include "TVFrame.h"
+#include "llvm-tv/Config.h"
+#include <fstream>
+#include <sys/types.h>
+#include <unistd.h>
 
 IMPLEMENT_APP(TVApplication)
 
 bool TVApplication::OnInit() {
-  
+  // Save my pid into a file  
+  std::ofstream pidFile(llvmtvPID.c_str());
+  if (pidFile.good() && pidFile.is_open()) {
+    pidFile << getpid();
+    pidFile.close();
+  }
+
   TVFrame *frame = new TVFrame("llvm-tv", 50, 50, 450, 300);
   
   frame->Show(TRUE);

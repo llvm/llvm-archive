8ff7ddcef044dccdc1ebf188042423f29f677f5c
Brian Gaeke <gaeke@uiuc.edu>
2004-04-26 07:50:46 +0000

    Move the topmost window's pointer into a private data member of class TVApplication. Add list of picture windows (allMyWindows). Add prototypes for the other methods I've added.
    llvm-svn: 13168

--- television/tools/llvm-tv/TVApplication.h
+++ television/tools/llvm-tv/TVApplication.h
@@ -9,17 +9,27 @@
 
 #include "wx/wx.h"
 #include <string>
-#include "TVFrame.h"
+#include <vector>
+#include "llvm/Module.h"
 
 ///==---------------------------------------------------------------------==///
+
+class TVFrame;
 
 /// TVApplication - This class shows a window containing a status bar and a menu
 /// bar, and a list of files from a directory, that can be refreshed
 /// using a menu item.
 ///
 class TVApplication : public wxApp {
- public:
-  virtual bool OnInit ();
+  TVFrame *myFrame;
+  std::vector<wxWindow *> allMyWindows;
+public:
+  bool OnInit ();
+  void GoodbyeFrom (wxWindow *dyingWindow);
+  void ReceivedSignal ();
+  void OpenCallGraphView (llvm::Module *M);
+  void OpenCFGView (llvm::Function *F);
+  void Quit ();
 };
 
 DECLARE_APP (TVApplication)

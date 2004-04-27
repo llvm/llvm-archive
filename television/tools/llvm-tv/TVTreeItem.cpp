6bac40fc23cda77671b6d244fc05fe4827afde40
Misha Brukman <brukman+llvm@gmail.com>
2004-04-27 02:36:46 +0000

    Implementation of the HTML code view of tree items (Functions/Modules).
    llvm-svn: 13179

---
+++ television/tools/llvm-tv/TVTreeItem.cpp
@@ -0,0 +1,177 @@
+#include "TVTreeItem.h"
+#include "llvm/Module.h"
+#include "llvm/Type.h"
+#include "wx/treectrl.h"
+using namespace llvm;
+
+static inline void htmlHeader(std::ostream &os) {
+  os << "<html><body><tt>\n";
+}
+
+static inline void htmlFooter(std::ostream &os) {
+  os << "</tt></body></html>";
+}
+
+static inline void htmlBB(std::ostream &os, const BasicBlock *BB) {
+  os << "<font color=\"#cc0000\"><b>" << BB->getName() << ":</b><br>";
+}
+
+static inline void htmlType(std::ostream &os, const Type* type) {
+  os << "<font color=\"green\"><b>";
+  type->print(os);
+  os << "</b></font>";
+}
+
+static inline std::string wrapType(const std::string &word) {
+  return ("<font color=\"green\"><b>" + word + "</b></font>");
+}
+
+static inline std::string wrapKeyword(const std::string &word) {
+  return ("<font color=\"navy\"><b>" + word + "</b></font>");
+}
+
+// LLVM types
+static const char* types[] = { 
+  "void", "bool", "sbyte", "ubyte", "short", "ushort", "int", "uint",
+  "long", "ulong", "float", "double", "type", "label", "opaque"
+};
+
+// LLVM keywords
+static const char* keywords[] = { 
+  // Arithmetic instructions
+  "add", "sub", "mul", "div", "rem", "and", "or", "xor",
+  "setne", "seteq", "setlt", "setgt", "setle", "setge",
+  // Misc
+  "phi", "call", "cast", "to", "select", "shl", "shr", "vaarg", "vanext",
+  // Control flow
+  "ret", "br", "switch", "invoke", "unwind",
+  // Memory
+  "malloc", "alloca", "free", "load", "store", "getelementptr",
+  // Language constants/keywords
+  "begin", "end", "true", "false",
+  "declare", "global", "constant", "const",
+  "internal", "uninitialized", "external", "implementation",
+  "linkonce", "weak", "appending",
+  "null", "to", "except", "target", "endian", "pointersize",
+  "big", "little", "volatile"
+};
+
+
+static inline std::string stylizeTypesAndKeywords(std::string &str) {
+  if (str == "") return "";
+
+  // Tokenize
+  std::vector<std::string> tokens;
+  unsigned prev = 0;
+  for (unsigned i = 0, e = str.size(); i != e; ++i) {
+    if (str[i] == ' ') {
+      tokens.push_back(str.substr(prev, i-prev));
+      prev = i+1;
+    }
+  }
+  // tack on the last segment
+  tokens.push_back(str.substr(prev, str.size()-prev));
+
+#if 0
+  std::cerr << "\n";
+  for (unsigned i=0, e = tokens.size(); i!=e; ++i)
+    std::cerr << "[" << tokens[i] << "], ";
+  std::cerr << "\n";
+#endif
+  
+  for (unsigned i = 0, e = tokens.size(); i != e; ++i)
+  {
+    // "Wrap" keywords
+    for (unsigned k = 0, ke = sizeof(keywords)/sizeof(char*); k != ke; ++k)
+      if (tokens[i] == keywords[k])
+        tokens[i] = wrapKeyword(tokens[i]);
+
+    // "Wrap" types
+    for (unsigned t = 0, te = sizeof(types)/sizeof(char*); t != te; ++t) {
+      std::string type(types[t]);
+      if (tokens[i].substr(0, type.size()) == type)
+        tokens[i] = wrapType(tokens[i]);
+    }
+  }
+
+#if 0
+  std::cerr << "\n";
+  for (unsigned i=0, e = tokens.size(); i!=e; ++i)
+    std::cerr << "[" << tokens[i] << "], ";
+  std::cerr << "\n";
+#endif
+
+  // Assemble back into one
+  std::string styled = "";
+  for (unsigned i = 0, e = tokens.size(); i != e; ++i)
+    styled += tokens[i] + " ";
+  return styled;
+}
+
+void TVTreeItemData::printFunction(Function *F, std::ostream &os) {
+  // print out function return type, name, and arguments
+  htmlType(os, F->getReturnType());
+  os << " " << F->getName() << "(";
+  for (Function::aiterator arg = F->abegin(), ae = F->aend(); arg != ae; ++arg){
+    htmlType(os, arg->getType());
+    os << " " << arg->getName() << ", ";
+  }
+
+  os << ") {<br>";
+
+  for (Function::iterator BB = F->begin(), BBe = F->end(); BB != BBe; ++BB) {
+    htmlBB(os, BB);
+    for (BasicBlock::iterator I = BB->begin(), Ie = BB->end(); I != Ie; ++I) {
+      std::ostringstream oss;
+      I->print(oss);
+      std::string InstrVal = oss.str();
+
+      // Prettify the instruction for HTML view
+      for (unsigned i = 0; i != InstrVal.length(); ++i)
+        if (InstrVal[i] == '\n') {                          // \n => <br>
+          InstrVal[i] = '<';
+          std::string br = "br>";
+          InstrVal.insert(InstrVal.begin()+i+1, br.begin(), br.end());
+          i += br.size();
+        } else if (InstrVal[i] == '\t') {                   // \t => &nbsp;
+          InstrVal[i] = '&';
+          std::string nbsp = "nbsp; &nbsp; ";
+          InstrVal.insert(InstrVal.begin()+i+1, nbsp.begin(), nbsp.end());
+          i += nbsp.size();
+        } else if (InstrVal[i] == ';') {                    // Delete comments!
+          unsigned Idx = InstrVal.find('\n', i+1);          // Find end of line
+          InstrVal.erase(InstrVal.begin()+i, InstrVal.begin()+Idx);
+          --i;
+        }
+
+      os << stylizeTypesAndKeywords(InstrVal);
+    }
+  }
+  os << "}<br>";
+}
+
+void TVTreeItemData::printModule(Module *M, std::ostream &os) {
+  // display target, endianness types
+
+  // display all functions
+  for (Module::iterator F = M->begin(), Fe = M->end(); F != Fe; ++F) {
+    printFunction(F, os);
+    os << "<br>";
+  }
+}
+
+void TVTreeModuleItem::print(std::ostream &os) {
+  htmlHeader(os);
+  myModule->print(os); 
+  htmlFooter(os);
+}
+
+void TVTreeFunctionItem::print(std::ostream &os) { 
+  htmlHeader(os);
+  myFunc->print(os);
+  htmlFooter(os);
+}
+
+Module* TVTreeFunctionItem::getModule() {
+  return myFunc ? myFunc->getParent() : 0; 
+}
---
+++ television/tools/llvm-tv/TVTreeItem.h
@@ -0,0 +1,67 @@
+#ifndef TVTREEITEM_H
+#define TVTREEITEM_H
+
+#include "wx/wx.h"
+#include "wx/treectrl.h"
+#include <ostream>
+#include <sstream>
+
+namespace llvm {
+  class Function;
+  class Module;
+}
+
+/// TVTreeItemData - Base class for LLVM TV Tree Data
+///  
+class TVTreeItemData : public wxTreeItemData {
+public:
+  TVTreeItemData(const wxString& desc) : m_desc(desc) { }
+  
+  void ShowInfo(wxTreeCtrl *tree);
+  const wxChar *GetDesc() const { return m_desc.c_str(); }
+  virtual void print(std::ostream&) {};
+  virtual llvm::Module *getModule() { return 0; }
+  virtual llvm::Function *getFunction() { return 0; }
+  virtual void printHTML(std::ostream &os) {}
+protected:
+  void printFunction(llvm::Function *F, std::ostream &os);
+  void printModule(llvm::Module *M, std::ostream &os);
+private:
+  wxString m_desc;
+};
+
+
+/// TVTreeModuleItem - Tree Item containing a Module
+///  
+class TVTreeModuleItem : public TVTreeItemData {
+public:
+  TVTreeModuleItem(const wxString& desc, llvm::Module *mod) 
+    : TVTreeItemData(desc), myModule(mod) {}
+  
+  void print(std::ostream &out);
+  void printHTML(std::ostream &os) { if (myModule) printModule(myModule, os); }
+
+  llvm::Module *getModule() { return myModule; }
+private:
+  llvm::Module *myModule;
+};
+
+
+/// TVTreeFunctionItem - Tree Item containing a Function
+///  
+class TVTreeFunctionItem : public TVTreeItemData {
+public:
+  TVTreeFunctionItem(const wxString& desc, llvm::Function *func)
+    : TVTreeItemData(desc),  myFunc(func) {}
+  
+  void print(std::ostream &out);
+  void printHTML(std::ostream &os) { if (myFunc) printFunction(myFunc, os); }
+
+  llvm::Module *getModule();
+  llvm::Function *getFunction() { return myFunc; }
+private:
+  llvm::Function *myFunc;
+};
+
+
+#endif

26d6b7c2c3b6c2fba46de9902c72a569dfab312c
Brian Gaeke <gaeke@uiuc.edu>
2004-04-28 19:35:28 +0000

    Make TVHtmlWindow and TVTextCtrl both be ItemDisplayers.
    llvm-svn: 13251

--- television/tools/llvm-tv/TVHtmlWindow.h
+++ television/tools/llvm-tv/TVHtmlWindow.h
@@ -1,14 +1,16 @@
 #ifndef TVHTMLWINDOW_H
 #define TVHTMLWINDOW_H
 
+#include "ItemDisplayer.h"
 #include "wx/html/htmlwin.h"
 
-class TVHtmlWindow : public wxHtmlWindow {
+class TVHtmlWindow : public wxHtmlWindow, public ItemDisplayer {
  public:
   TVHtmlWindow (wxWindow *_parent, wxWindowID _id, const wxString &_init = "")
      : wxHtmlWindow (_parent, _id) {
     AppendToPage (_init);
   }
+  void displayItem (TVTreeItemData *item);
 };
 
 #endif // TVHTMLWINDOW_H
--- television/tools/llvm-tv/TVTextCtrl.h
+++ television/tools/llvm-tv/TVTextCtrl.h
@@ -3,11 +3,12 @@
 
 #include "wx/textctrl.h"
 
-class TVTextCtrl : public wxTextCtrl {
+class TVTextCtrl : public wxTextCtrl, public ItemDisplayer {
  public:
   TVTextCtrl (wxWindow *_parent, wxWindowID _id, const wxString &_value = "")
      : wxTextCtrl (_parent, _id, _value, wxDefaultPosition, wxDefaultSize,
                    wxTE_READONLY | wxTE_MULTILINE | wxHSCROLL) { }
+  void displayItem (TVTreeItemData *item);
 };
 
 #endif // TVTEXTCTRL_H

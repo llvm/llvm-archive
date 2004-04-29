9f0b20e009b1f96c866db28bb18aa9ba392fa9e0
Brian Gaeke <gaeke@uiuc.edu>
2004-04-29 05:30:41 +0000

    Make a new pure virtual method getWindow(); ie., ItemDisplayers are responsible for explicitly keeping track of whatever "window" they are displaying the item into. The multiple inheritance thing here was never going to fly... :-(
    llvm-svn: 13275

--- television/tools/llvm-tv/ItemDisplayer.h
+++ television/tools/llvm-tv/ItemDisplayer.h
@@ -2,10 +2,16 @@
 #define ITEMDISPLAYER_H
 
 class TVTreeItemData;
+class wxWindow;
 
 class ItemDisplayer {
  public:
   virtual void displayItem (TVTreeItemData *item) = 0;
+
+  /// getWindow - ItemDisplayers are responsible for explicitly keeping track of
+  /// whatever "window" they are displaying the item into.
+  ///
+  virtual wxWindow *getWindow () = 0;
 };
 
 #endif // ITEMDISPLAYER_H

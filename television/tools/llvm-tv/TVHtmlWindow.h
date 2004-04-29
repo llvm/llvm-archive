f824b10189e8fe80a17b27ec92410ae82148a791
Brian Gaeke <gaeke@uiuc.edu>
2004-04-29 05:31:13 +0000

    Both ItemDisplayers now have to use private data members to access their respective display widgets, instead of inheriting from those widgets. Essentially, each ItemDisplayer's relationship with its display widget has been changed from an "is a" to "has a" relationship...
    Implement the new virtual method getWindow() inherited from ItemDisplayer..
    
    llvm-svn: 13278

--- television/tools/llvm-tv/TVHtmlWindow.h
+++ television/tools/llvm-tv/TVHtmlWindow.h
@@ -4,13 +4,15 @@
 #include "ItemDisplayer.h"
 #include "wx/html/htmlwin.h"
 
-class TVHtmlWindow : public wxHtmlWindow, public ItemDisplayer {
+class TVHtmlWindow : public ItemDisplayer {
+  wxHtmlWindow *myHtmlWindow;
  public:
-  TVHtmlWindow (wxWindow *_parent, wxWindowID _id, const wxString &_init = "")
-     : wxHtmlWindow (_parent, _id) {
-    AppendToPage (_init);
+  TVHtmlWindow (wxWindow *_parent, wxWindowID _id, const wxString &_init = "") {
+    myHtmlWindow = new wxHtmlWindow (_parent, _id);
+    myHtmlWindow->AppendToPage (_init);
   }
   void displayItem (TVTreeItemData *item);
+  wxWindow *getWindow () { return myHtmlWindow; }
 };
 
 #endif // TVHTMLWINDOW_H
--- television/tools/llvm-tv/TVTextCtrl.h
+++ television/tools/llvm-tv/TVTextCtrl.h
@@ -1,14 +1,18 @@
 #ifndef TVTEXTCTRL_H
 #define TVTEXTCTRL_H
 
+#include "ItemDisplayer.h"
 #include "wx/textctrl.h"
 
-class TVTextCtrl : public wxTextCtrl, public ItemDisplayer {
+class TVTextCtrl : public ItemDisplayer {
+  wxTextCtrl *myTextCtrl;
  public:
-  TVTextCtrl (wxWindow *_parent, wxWindowID _id, const wxString &_value = "")
-     : wxTextCtrl (_parent, _id, _value, wxDefaultPosition, wxDefaultSize,
-                   wxTE_READONLY | wxTE_MULTILINE | wxHSCROLL) { }
+  TVTextCtrl (wxWindow *_parent, wxWindowID _id, const wxString &_value = "") {
+    myTextCtrl = new wxTextCtrl (_parent, _id, _value, wxDefaultPosition,
+      wxDefaultSize, wxTE_READONLY | wxTE_MULTILINE | wxHSCROLL);
+  }
   void displayItem (TVTreeItemData *item);
+  wxWindow *getWindow () { return myTextCtrl; }
 };
 
 #endif // TVTEXTCTRL_H

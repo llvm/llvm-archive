72277ab2e9d78ab231aac253605f0a868dccd06c
Brian Gaeke <gaeke@uiuc.edu>
2004-05-03 07:14:18 +0000

    Add virtual TreeItem methods: getTitle, dsGraphName, and graphOn. The first two are used only for informational purposes -- what the user sees as the title of a window containing a visualization of this item. The third is for graph drawing support: it turns around and calls whichever of drawModuleGraph or drawFunctionGraph is appropriate for the given TreeItem.
    llvm-svn: 13334

--- television/tools/llvm-tv/TVTreeItem.h
+++ television/tools/llvm-tv/TVTreeItem.h
@@ -8,9 +8,11 @@
 #define TVTREEITEM_H
 
 #include "llvm/Assembly/CachedWriter.h"
+#include "GraphDrawer.h"
 #include <wx/wx.h>
 #include <wx/treectrl.h>
 #include <ostream>
+#include <string>
 
 namespace llvm {
   class Function;
@@ -26,16 +28,18 @@ public:
   
   void ShowInfo(wxTreeCtrl *tree);
   const wxChar *GetDesc() const { return m_desc.c_str(); }
-  virtual void print(std::ostream&) {};
+  virtual void print(std::ostream&) { }
   virtual llvm::Module *getModule() { return 0; }
   virtual llvm::Function *getFunction() { return 0; }
-  virtual void printHTML(std::ostream &os) {}
+  virtual wxImage *graphOn(GraphDrawer *grapher) { return 0; }
+  virtual void printHTML(std::ostream &os) { }
+  virtual std::string getTitle () { return "an untitled object"; }
+  virtual std::string dsGraphName () { return "graph of " + getTitle (); }
 protected:
   void printFunctionHeader(llvm::Function *F);
   void printFunction(llvm::Function *F);
   void printModule(llvm::Module *M);
   llvm::CachedWriter cw;
-private:
   wxString m_desc;
 };
 
@@ -51,6 +55,12 @@ public:
   
   void print(std::ostream &out);
   void printHTML(std::ostream &os);
+  wxImage *graphOn(GraphDrawer *grapher) {
+    return grapher->drawModuleGraph (myModule);
+  }
+
+  virtual std::string getTitle () { return m_desc.c_str(); }
+  virtual std::string dsGraphName () { return "globals graph"; }
 
   llvm::Module *getModule() { return myModule; }
 };
@@ -67,6 +77,11 @@ public:
   
   void print(std::ostream &out);
   void printHTML(std::ostream &os);
+  wxImage *graphOn(GraphDrawer *grapher) {
+    return grapher->drawFunctionGraph (myFunc); 
+  }
+
+  virtual std::string getTitle ();
 
   llvm::Module *getModule();
   llvm::Function *getFunction() { return myFunc; }

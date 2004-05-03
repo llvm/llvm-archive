49f094a8f88ab93cd9bc39de60ec3543467f0916
Brian Gaeke <gaeke@uiuc.edu>
2004-05-03 07:13:12 +0000

    GraphDrawer has been substantially rewritten so that it is now an ItemDisplayer just like the HTML View or Text View. It now has a displayItem method and should now be tabifiable (tm).
    getGraphImage is gone; it is now perfectly ok for a GraphDrawer to
    be reused and draw new graphs of new functions and whatnot.
    
    drawGraphImage is gone. In its place, there are now two virtual methods,
    drawModuleGraph and drawFunctionGraph. They return NULL if you don't override
    them. If the particular GraphDrawer supports graphing a function, it should
    override drawFunctionGraph; likewise for drawModuleGraph.
    
    llvm-svn: 13328

--- television/tools/llvm-tv/GraphDrawer.cpp
+++ television/tools/llvm-tv/GraphDrawer.cpp
@@ -1,5 +1,6 @@
 #include "wx/image.h"
 #include "GraphDrawer.h"
+#include "TVTreeItem.h"
 #include "Support/FileUtilities.h"
 #include <unistd.h>
 #include <iostream>
@@ -10,16 +11,6 @@ using namespace llvm;
 // GraphDrawer shared implementation
 
 extern void FatalErrorBox (const std::string msg);
-
-wxImage *GraphDrawer::getGraphImage() {
-  if (!graphImage) {
-    graphImage = drawGraphImage ();
-    // Make sure that it worked
-    if (!graphImage)
-      FatalErrorBox ("drawGraphImage() failed to produce an image\n");
-  }
-  return graphImage;
-}
 
 wxImage *GraphDrawer::buildwxImageFromDotFile (const std::string filename) {
   if (!FileOpenable (filename))
@@ -38,5 +29,21 @@ wxImage *GraphDrawer::buildwxImageFromDotFile (const std::string filename) {
 
   unlink ("image.png");
   return img;
+}
+
+void GraphDrawer::displayItem (TVTreeItemData *item) {
+  wxImage *graphImage = item->graphOn (this);
+  if (!graphImage) {
+    // If we're drawing into a window, don't leave behind an embarrassing
+    // empty window AND an error message. (But don't destroy the window,
+    // because TVApp will get around to destroying it later.)
+    if (wxFrame *frame = dynamic_cast<wxFrame *>(myPictureCanvas->GetParent ()))
+      frame->Show (false);
+    std::string errMsg = "Sorry, you can't draw that kind of graph on "
+                         + item->getTitle() + ".";
+    wxMessageBox (errMsg.c_str (), "Error");
+    return;
+  }
+  myPictureCanvas->SetImage (graphImage);
 }
 
--- television/tools/llvm-tv/GraphDrawer.h
+++ television/tools/llvm-tv/GraphDrawer.h
@@ -7,24 +7,40 @@
 #ifndef GRAPHDRAWER_H
 #define GRAPHDRAWER_H
 
-#include <string>
+#include "PictureCanvas.h"
+#include "ItemDisplayer.h"
+#include "wx/wx.h"
+#include <iostream>
 
-class wxImage;
+namespace llvm {
+  class Module;
+  class Function;
+};
+
+class TVTreeItemData;
 
 //===----------------------------------------------------------------------===//
 
 // GraphDrawer abstract class
 
-class GraphDrawer {
-  wxImage *graphImage;
-  virtual wxImage *drawGraphImage () = 0;
+class GraphDrawer : public ItemDisplayer {
+  PictureCanvas *myPictureCanvas;
 
- protected:
+protected:
   static wxImage *buildwxImageFromDotFile (const std::string filename);
 
- public:
-  GraphDrawer () : graphImage (0) { }
-  wxImage *getGraphImage ();
+public:
+  GraphDrawer (wxWindow *parent)
+    : myPictureCanvas (new PictureCanvas (parent)) { }
+  virtual ~GraphDrawer () { std::cerr << "GraphDrawer is being destroyed\n"; delete myPictureCanvas; }
+  wxWindow *getWindow () { return myPictureCanvas; }
+  void displayItem (TVTreeItemData *item);
+
+  /// Subclasses should override these methods to return images if they
+  /// support visualizing the corresponding kind of object.
+  ///
+  virtual wxImage *drawModuleGraph (llvm::Module *M) { return 0; }
+  virtual wxImage *drawFunctionGraph (llvm::Function *F) { return 0; }
 };
 
 #endif // GRAPHDRAWER_H

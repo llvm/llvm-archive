996b119031f2a28e2428365ef33e42b7679470b8
Brian Gaeke <gaeke@uiuc.edu>
2004-05-03 04:18:04 +0000

    setupBitmap() renamed to imageChanged(). Make it more robust against image changes. Wrap long lines. Get rid of loadPicture(), which is not called anywhere anymore. Get rid of the empty event table.
    llvm-svn: 13321

--- television/tools/llvm-tv/PictureCanvas.cpp
+++ television/tools/llvm-tv/PictureCanvas.cpp
@@ -10,23 +10,17 @@ void PictureCanvas::OnDraw (wxDC &aDC) {
   aDC.DrawBitmap (*myBitmap, 0, 0, false);
 }
 
-void PictureCanvas::setupBitmap () {
+void PictureCanvas::imageChanged () {
+  if (myBitmap)
+    delete myBitmap; // Don't leak memory.
+
+  if (!myImage)
+    return; // Maybe there isn't a new image.
+
   myBitmap = new wxBitmap (*myImage);
   SetVirtualSize (myImage->GetWidth (), myImage->GetHeight ());
   SetScrollRate (1, 1);
-  GetParent()->SetSizeHints (-1, -1, myImage->GetWidth (), myImage->GetHeight ());
-  GetParent()->Refresh ();
+  GetParent ()->SetSizeHints (-1, -1, myImage->GetWidth (),
+                             myImage->GetHeight ());
+  GetParent ()->Refresh ();
 }
-
-void PictureCanvas::loadPicture (const wxString filename) {
-  if (!myImage)
-    myImage = new wxImage;
-  if (!myImage->LoadFile (filename)) {
-    GetParent()->Close ();
-    return;
-  }
-  setupBitmap ();
-}
-
-BEGIN_EVENT_TABLE (PictureCanvas, wxScrolledWindow)
-END_EVENT_TABLE ()

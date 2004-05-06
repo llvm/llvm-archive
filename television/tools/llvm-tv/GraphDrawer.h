1ba61df16e08d3e2b0be7bcb40caf69e98d64309
Brian Gaeke <gaeke@uiuc.edu>
2004-05-06 05:16:09 +0000

    Update comments; add prototype for getDisplayTitle.
    llvm-svn: 13372

--- television/tools/llvm-tv/GraphDrawer.h
+++ television/tools/llvm-tv/GraphDrawer.h
@@ -32,15 +32,22 @@ protected:
 public:
   GraphDrawer (wxWindow *parent)
     : myPictureCanvas (new PictureCanvas (parent)) { }
-  virtual ~GraphDrawer () { std::cerr << "GraphDrawer is being destroyed\n"; delete myPictureCanvas; }
+  virtual ~GraphDrawer () { delete myPictureCanvas; }
   wxWindow *getWindow () { return myPictureCanvas; }
   void displayItem (TVTreeItemData *item);
 
-  /// Subclasses should override these methods to return images if they
-  /// support visualizing the corresponding kind of object.
+  /// drawModuleGraph, drawFunctionGraph - Subclasses should override
+  /// these methods to return images if they support visualizing the
+  /// corresponding kind of object.
   ///
   virtual wxImage *drawModuleGraph (llvm::Module *M) { return 0; }
   virtual wxImage *drawFunctionGraph (llvm::Function *F) { return 0; }
+
+  /// getDisplayTitle - What should it be called when a subclass displays a
+  /// graph of the given item? If you want the graph to have a descriptive
+  /// title, like "Spiffy graph of foo()", override this method.
+  ///
+  static std::string getDisplayTitle (TVTreeItemData *item);
 };
 
 #endif // GRAPHDRAWER_H

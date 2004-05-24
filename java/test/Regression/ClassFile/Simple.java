bb3895f5be7d9edfc3c764394a951e404ae5633c
Alkis Evlogimenos <alkis@evlogimenos.com>
2004-05-24 20:01:26 +0000

    Remove private constructor. The constructor is still emitted, but javap -c doesn't output it.
    llvm-svn: 13725

--- java/test/Regression/ClassFile/Simple.java
+++ java/test/Regression/ClassFile/Simple.java
@@ -2,10 +2,6 @@ package edu.uiuc.cs.llvm;
 
 public class Simple
 {
-    private Simple() {
-        // declare constructor private so that it is not emitted
-    }
-
     public static int main(String[] args) {
         return 0;
     }

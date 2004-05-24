f57c451f3163565cf4768fe40fb20aa642cf2dc5
Alkis Evlogimenos <alkis@evlogimenos.com>
2004-05-24 08:27:20 +0000

    Declare constructor as private so that code for it is not emitted.
    llvm-svn: 13719

--- java/test/Regression/ClassFile/Simple.java
+++ java/test/Regression/ClassFile/Simple.java
@@ -2,8 +2,11 @@ package edu.uiuc.cs.llvm;
 
 public class Simple
 {
+    private Simple() {
+        // declare constructor private so that it is not emitted
+    }
+
     public static int main(String[] args) {
         return 0;
     }
 }
-

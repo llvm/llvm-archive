5d999d9a91d3768ceb6a1d18f9fb67c662adaf99
Alkis Evlogimenos <alkis@evlogimenos.com>
2004-05-27 21:08:56 +0000

    Add test case for LCMP bytecode.
    llvm-svn: 13853

---
+++ java/test/Programs/SingleSource/UnitTests/LongCompare.java
@@ -0,0 +1,9 @@
+public class LongCompare
+{
+    public static int main(String[] args) {
+        long l1 = 123456789123456789L;
+        long l2 = 987654321987654321L;
+
+        return l1 == l2 ? 1 : 0;
+    }
+}

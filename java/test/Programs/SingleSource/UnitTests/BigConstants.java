571d20e540e1d01c4ea8452471560ff5ba8510c0
Alkis Evlogimenos <alkis@evlogimenos.com>
2004-05-27 20:24:49 +0000

    Add test case for testing LDC, LDC_W and LDC2_W bytecodes.
    llvm-svn: 13843

---
+++ java/test/Programs/SingleSource/UnitTests/BigConstants.java
@@ -0,0 +1,11 @@
+public class BigConstants
+{
+    public static int main(String[] args) {
+        int i = 1234567890;
+        long l = 1234567890123456789L;
+        float f = -1.23456789e10F;
+        double d = 1.23456789e100;
+
+        return 0;
+    }
+}

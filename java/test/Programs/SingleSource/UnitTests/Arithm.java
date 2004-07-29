781883db6a474f415011bde6fe392a7dedc1b17e
CVS to SVN Conversion <nobody@llvm.org>
2004-07-29 17:05:13 +0000

    This commit was manufactured by cvs2svn to create branch 'release_13'.
    llvm-svn: 15330

--- java/test/Programs/SingleSource/UnitTests/Arithm.java
+++
@@ -1,9 +0,0 @@
-public class Arithm
-{
-    public static void main(String[] args) {
-        int one = 1;
-        int two = 2;
-
-        System.out.println((one + two) - (two * two) + (two / one) + (two % one) + (two << one) - (two >> 1) + (-two)); // = 2
-    }
-}
--- java/test/Regression/ClassFile/Hello.java
+++
@@ -1,8 +0,0 @@
-public class Hello
-{
-    public static int main(String[] args) {
-        System.out.println("Hello world");
-        return 0;
-    }
-}
-

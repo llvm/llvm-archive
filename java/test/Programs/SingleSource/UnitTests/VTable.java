61b9bb01a9305c33fa181146783816d540e8cd21
Alkis Evlogimenos <alkis@evlogimenos.com>
2004-09-15 20:27:20 +0000

    Modify test to include dynamic dispatch through an interface vtable.
    llvm-svn: 16375

--- java/test/Programs/SingleSource/UnitTests/VTable.java
+++ java/test/Programs/SingleSource/UnitTests/VTable.java
@@ -1,12 +1,18 @@
 class VTableBase
 {
-    int foo() { return 0; }
-    int bar() { return 0; }
+    public int foo() { return 0; }
+    public int bar() { return 0; }
 }
 
-public class VTable extends VTableBase
+interface VTableInterface
 {
-    int foo() { return 1; }
+    public int baz();
+}
+
+public class VTable extends VTableBase implements VTableInterface
+{
+    public int foo() { return 1; }
+    public int baz() { return 2; }
 
     public static void main(String[] args) {
         VTableBase a = new VTableBase();
@@ -16,5 +22,9 @@ public class VTable extends VTableBase
         a = new VTable();
         a.foo();
         a.bar();
+        ((VTableInterface)a).baz();
+
+        VTableInterface i = new VTable();
+        i.baz();
     }
 }

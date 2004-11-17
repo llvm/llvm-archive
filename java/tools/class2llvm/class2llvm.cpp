e090728fc24b5ae4cbc839b0b3af08de0448baf5
Alkis Evlogimenos <alkis@evlogimenos.com>
2004-11-17 23:53:49 +0000

    Make tool take a '-o' option for the output filename.
    llvm-svn: 17928

--- java/tools/class2llvm/class2llvm.cpp
+++ java/tools/class2llvm/class2llvm.cpp
@@ -21,6 +21,7 @@
 #include <llvm/System/Signals.h>
 
 #include <cstddef>
+#include <fstream>
 #include <iostream>
 
 using namespace llvm;
@@ -28,24 +29,37 @@ using namespace llvm;
 static cl::opt<std::string>
 InputClass(cl::Positional, cl::desc("<input class>"));
 
+static cl::opt<std::string>
+OutputFilename("o", cl::desc("Output filename"), cl::value_desc("filename"));
+
 int main(int argc, char* argv[])
 {
   sys::PrintStackTraceOnErrorSignal();
   cl::ParseCommandLineOptions(argc, argv,
                               "classfile to llvm utility");
 
+  std::ostream* out = &std::cout;
+  if (!OutputFilename.empty()) {
+    sys::RemoveFileOnSignal(sys::Path(OutputFilename));
+    out = new std::ofstream(OutputFilename.c_str());
+  }
+
   try {
     std::auto_ptr<Module> module = Java::compile(InputClass);
 
     PassManager passes;
     passes.add(createVerifierPass());
-    passes.add(new WriteBytecodePass(&std::cout));
+    passes.add(new WriteBytecodePass(out));
     passes.run(*module);
   }
   catch (std::exception& e) {
     std::cerr << e.what() << '\n';
+    sys::Path(OutputFilename).destroyFile();
     return EXIT_FAILURE;
   }
+
+  if (!OutputFilename.empty())
+    delete out;
 
   return EXIT_SUCCESS;
 }

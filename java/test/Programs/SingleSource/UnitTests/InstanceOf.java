45b85f3c9dd150a7a10c7a6d233d4c4539d8b91b
Alkis Evlogimenos <alkis@evlogimenos.com>
2004-12-06 23:39:58 +0000

    Clean up test a bit.
    llvm-svn: 18587

--- java/test/Programs/SingleSource/UnitTests/InstanceOf.java
+++ java/test/Programs/SingleSource/UnitTests/InstanceOf.java
@@ -4,30 +4,35 @@ class InstanceOfBase
     int bar() { return 0; }
 }
 
-interface InstanceOfInterface {
+interface InstanceOfInterface
+{
     public int baz();
 }
 
-public class InstanceOf extends InstanceOfBase implements InstanceOfInterface
+class InstanceOfDerived extends InstanceOfBase implements InstanceOfInterface
 {
     int foo() { return 1; }
     public int baz() { return foo(); }
 
+}
+
+public class InstanceOf
+{
     public static void main(String[] args) {
         InstanceOfBase aa = new InstanceOfBase();
-        InstanceOfBase ab = new InstanceOf();
-        InstanceOfInterface bb = new InstanceOf();
+        InstanceOfBase ab = new InstanceOfDerived();
+        InstanceOfInterface bb = new InstanceOfDerived();
 
         Test.print_boolean_ln(aa instanceof InstanceOfBase);
-        Test.print_boolean_ln(aa instanceof InstanceOf);
+        Test.print_boolean_ln(aa instanceof InstanceOfDerived);
         Test.print_boolean_ln(aa instanceof InstanceOfInterface);
 
         Test.print_boolean_ln(ab instanceof InstanceOfBase);
-        Test.print_boolean_ln(ab instanceof InstanceOf);
+        Test.print_boolean_ln(ab instanceof InstanceOfDerived);
         Test.print_boolean_ln(ab instanceof InstanceOfInterface);
 
         Test.print_boolean_ln(bb instanceof InstanceOfBase);
-        Test.print_boolean_ln(bb instanceof InstanceOf);
+        Test.print_boolean_ln(bb instanceof InstanceOfDerived);
         Test.print_boolean_ln(bb instanceof InstanceOfInterface);
     }
 }

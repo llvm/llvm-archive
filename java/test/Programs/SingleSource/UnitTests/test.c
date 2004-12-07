4328047deb09d9b28542b4a0c010b29408b46cd9
Alkis Evlogimenos <alkis@evlogimenos.com>
2004-12-07 04:55:45 +0000

    Commit sources for the test library.
    llvm-svn: 18590

---
+++ java/test/Programs/SingleSource/UnitTests/test.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+#include "Test.h"
+
+void Java_Test_print_1boolean_1ln(JNIEnv *env, jclass clazz, jboolean aBoolean)
+{
+  if (aBoolean)
+    puts("true");
+  else
+    puts("false");
+}
+
+void Java_Test_print_1int_1ln(JNIEnv *env, jclass clazz, jint aInt)
+{
+  printf("%d\n", aInt);
+}
+
+void Java_Test_print_1long_1ln(JNIEnv *env, jclass clazz, jlong aLong)
+{
+  printf("%Ld\n", aLong);
+}
+
+void Java_Test_print_1float_1ln(JNIEnv *env, jclass clazz, jfloat aFloat)
+{
+  printf("%f\n", aFloat);
+}
+
+void Java_Test_print_1double_1ln(JNIEnv *env, jclass clazz, jdouble aDouble)
+{
+  printf("%lf\n", aDouble);
+}

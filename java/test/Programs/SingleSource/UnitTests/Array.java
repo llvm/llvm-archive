a6e43a381fd60bc48f6f5038faeeaf75c32d46f0
Alkis Evlogimenos <alkis@evlogimenos.com>
2004-12-07 18:38:02 +0000

    Add array test.
    llvm-svn: 18607

---
+++ java/test/Programs/SingleSource/UnitTests/Array.java
@@ -0,0 +1,22 @@
+public class Array
+{
+    public static void main(String[] args) {
+        int[] intArray = new int[10];
+
+        for (int i = 0, e = intArray.length; i != e; ++i)
+            intArray[i] = intArray.length - i;
+
+        for (int i = 0, e = intArray.length; i != e; ++i)
+            Test.print_int_ln(intArray[i]);
+
+
+        Object[] objectArray = new Object[10];
+
+        for (int i = 0, e = objectArray.length; i != e; ++i)
+            objectArray[i] = intArray;
+
+        intArray = (int[]) objectArray[4];
+        for (int i = 0, e = intArray.length; i != e; ++i)
+            Test.print_int_ln(intArray[i]);
+    }
+}

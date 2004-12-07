6a9bbd9073defc4a4f928139ff53f4e29723da7a
Alkis Evlogimenos <alkis@evlogimenos.com>
2004-12-07 18:42:57 +0000

    Add multi array test.
    llvm-svn: 18608

---
+++ java/test/Programs/SingleSource/UnitTests/MultiArray.java
@@ -0,0 +1,14 @@
+public class MultiArray
+{
+    public static void main(String[] args) {
+        int[][] intArray = new int[10][10];
+
+        for (int i = 0; i != intArray.length; ++i)
+            for (int j = 0; j != intArray[i].length; ++j)
+                intArray[i][j] = (j+1) * (i+1);
+
+        for (int i = 0; i != intArray.length; ++i)
+            for (int j = 0; j != intArray[i].length; ++j)
+                Test.print_int_ln(intArray[i][j]);
+    }
+}

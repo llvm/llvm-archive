b4a716919cfeca4037423269cf56a42991c0e8cf
Alkis Evlogimenos <alkis@evlogimenos.com>
2004-12-09 23:21:52 +0000

    Make Test test itself too :-)
    llvm-svn: 18730

--- java/test/Programs/SingleSource/UnitTests/Test.java
+++ java/test/Programs/SingleSource/UnitTests/Test.java
@@ -9,4 +9,17 @@ public class Test
     public static native void print_long_ln(long l);
     public static native void print_float_ln(float f);
     public static native void print_double_ln(double d);
+
+    public static void main(String[] args) {
+        print_boolean_ln(true);
+        print_boolean_ln(false);
+        print_int_ln(123);
+        print_int_ln(-321);
+        print_long_ln(1234567890123456789L);
+        print_long_ln(-1234567890123456789L);
+        print_float_ln(753.46F);
+        print_float_ln(-753.46F);
+        print_double_ln(753.46);
+        print_double_ln(-753.46);
+    }
 }

62e15ea8656b3d23902da02bdcfc62c51d0bd674
Alkis Evlogimenos <alkis@evlogimenos.com>
2004-12-10 09:05:35 +0000

    Make If more interesting. We actually fail this testcase!
    llvm-svn: 18759

--- java/test/Programs/SingleSource/UnitTests/If.java
+++ java/test/Programs/SingleSource/UnitTests/If.java
@@ -1,8 +1,12 @@
 public class If
 {
+    private static boolean isZero(int i) {
+        return i == 0;
+    }
+
     public static void main(String[] args) {
         int i = 0;
-        if (i == 0)
+        if (isZero(i))
             Test.print_int_ln(0);
         else
             Test.print_int_ln(1);

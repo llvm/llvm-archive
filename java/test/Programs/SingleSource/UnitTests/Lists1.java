a05e35185f29648c330694365559e8182c70d41d
Alkis Evlogimenos <alkis@evlogimenos.com>
2004-12-12 02:48:43 +0000

    Add new testcase
    llvm-svn: 18811

---
+++ java/test/Programs/SingleSource/UnitTests/Lists1.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Lists1
+{
+    public static void main(String[] args) {
+        List list = new LinkedList();
+        for (int i = 0; i < 10; ++i) {
+            list.add(new Integer(i));
+        }
+
+        Test.println(list.isEmpty());
+        Object[] array = list.toArray();
+
+        for (int i = 0; i < array.length; ++i) {
+            Test.println(((Integer) array[i]).equals(list.get(i)));
+        }
+
+        list.clear();
+
+        Test.println(list.isEmpty());
+    }
+}

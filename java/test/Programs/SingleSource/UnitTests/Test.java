3faf2d2ff5ecdd7e59e23b4cb727a90d6bf07f28
Alkis Evlogimenos <alkis@evlogimenos.com>
2004-12-12 03:30:57 +0000

    Comment out the println(String) method and add test for the bytearray one.
    llvm-svn: 18814

--- java/test/Programs/SingleSource/UnitTests/Test.java
+++ java/test/Programs/SingleSource/UnitTests/Test.java
@@ -9,7 +9,7 @@ public class Test
     public static native void println(long l);
     public static native void println(float f);
     public static native void println(double d);
-    public static void println(String s) { println(s.getBytes()); }
+//     public static void println(String s) { println(s.getBytes()); }
     private static native void println(byte[] a);
 
     public static void main(String[] args) {
@@ -23,5 +23,6 @@ public class Test
         println(-753.46F);
         println(753.46);
         println(-753.46);
+        println(new byte[] { 'H', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd' });
     }
 }

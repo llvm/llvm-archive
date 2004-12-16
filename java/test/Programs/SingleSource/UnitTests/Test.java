ddd659186a9d9a7fddefa0468b46b0be51a96e6c
Alkis Evlogimenos <alkis@evlogimenos.com>
2004-12-16 22:45:55 +0000

    Use a getBytes(int, int, byte[], int) to print strings so that we don't use an encoding manager to do so.
    llvm-svn: 18997

--- java/test/Programs/SingleSource/UnitTests/Test.java
+++ java/test/Programs/SingleSource/UnitTests/Test.java
@@ -9,7 +9,11 @@ public class Test
     public static native void println(long l);
     public static native void println(float f);
     public static native void println(double d);
-//     public static void println(String s) { println(s.getBytes()); }
+    public static void println(String s) {
+        byte[] bytes = new byte[s.length()];
+        s.getBytes(0, s.length(), bytes, 0);
+        println(bytes);
+    }
     private static native void println(byte[] a);
 
     public static void main(String[] args) {
@@ -24,5 +28,6 @@ public class Test
         println(753.46);
         println(-753.46);
         println(new byte[] { 'H', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd' });
+        println("Hello world");
     }
 }

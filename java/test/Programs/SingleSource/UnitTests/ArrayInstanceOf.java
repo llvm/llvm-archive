dd966fa0176681a2e6a85064ca7db91503c8d965
Alkis Evlogimenos <alkis@evlogimenos.com>
2005-01-11 05:48:53 +0000

    Simplify array instanceof test
    llvm-svn: 19460

--- java/test/Programs/SingleSource/UnitTests/ArrayInstanceOf.java
+++ java/test/Programs/SingleSource/UnitTests/ArrayInstanceOf.java
@@ -19,42 +19,29 @@ public class ArrayInstanceOf
     }
 
     public static void main(String[] args) {
-        Object[] ob = new Base[10];
-        Object[] od = new Derived[10];
-        Base[] bb = new Base[10];
-        Base[] bd = new Derived[10];
-        Interface[] id = new Derived[10];
-        Derived[] dd = new Derived[10];
+	Object o = new Object[10];
+        Object b = new Base[10];
+        Object d = new Derived[10];
+        Object i = new Interface[10];
 
-        Test.println(ob instanceof Object[]);
-        Test.println(ob instanceof Base[]);
-        Test.println(ob instanceof Interface[]);
-        Test.println(ob instanceof Derived[]);
+        Test.println(o instanceof Object[]);
+        Test.println(o instanceof Base[]);
+        Test.println(o instanceof Interface[]);
+        Test.println(o instanceof Derived[]);
 
-        Test.println(od instanceof Object[]);
-        Test.println(od instanceof Base[]);
-        Test.println(od instanceof Interface[]);
-        Test.println(od instanceof Derived[]);
+        Test.println(b instanceof Object[]);
+        Test.println(b instanceof Base[]);
+        Test.println(b instanceof Interface[]);
+        Test.println(b instanceof Derived[]);
 
-        Test.println(bb instanceof Object[]);
-        Test.println(bb instanceof Base[]);
-        Test.println(bb instanceof Interface[]);
-        Test.println(bb instanceof Derived[]);
+        Test.println(d instanceof Object[]);
+        Test.println(d instanceof Base[]);
+        Test.println(d instanceof Interface[]);
+        Test.println(d instanceof Derived[]);
 
-        Test.println(bd instanceof Object[]);
-        Test.println(bd instanceof Base[]);
-        Test.println(bd instanceof Interface[]);
-        Test.println(bd instanceof Derived[]);
-
-        Test.println(id instanceof Object[]);
-        Test.println(id instanceof Base[]);
-        Test.println(id instanceof Interface[]);
-        Test.println(id instanceof Derived[]);
-
-        Test.println(dd instanceof Object[]);
-        Test.println(dd instanceof Base[]);
-        Test.println(dd instanceof Interface[]);
-        Test.println(dd instanceof Derived[]);
-
+        Test.println(i instanceof Object[]);
+        Test.println(i instanceof Base[]);
+        Test.println(i instanceof Interface[]);
+        Test.println(i instanceof Derived[]);
     }
 }

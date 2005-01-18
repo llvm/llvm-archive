de5ae313d4692058cff01201cbe1bc611c6d04d8
Alkis Evlogimenos <alkis@evlogimenos.com>
2005-01-18 02:30:49 +0000

    New test
    llvm-svn: 19653

---
+++ java/test/Programs/SingleSource/UnitTests/Lists3.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Lists3
+{
+    public static void main(String[] args) {
+        LinkedList llist = new LinkedList();
+        for (int i = 0; i < 10; ++i) {
+            llist.add(new Integer(i));
+        }
+
+        Test.println(llist.contains(new Integer(5)));
+        Test.println(llist.getFirst().equals(new Integer(0)));
+        Test.println(llist.getLast().equals(new Integer(9)));
+        Test.println(llist.indexOf(new Integer(3)));
+        Test.println(llist.remove(new Integer(3)));
+        Test.println(llist.indexOf(new Integer(3)));
+        Test.println(((Integer)llist.removeFirst()).intValue());
+        Test.println(((Integer)llist.removeLast()).intValue());
+        Test.println(llist.size());
+
+        for (Iterator i = llist.iterator(); i.hasNext(); )
+            Test.println(((Integer)i.next()).intValue());
+    }
+}

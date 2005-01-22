c169cb4659d5d2964a20d3784299e5b6826d013f
Alkis Evlogimenos <alkis@evlogimenos.com>
2005-01-22 03:28:56 +0000

    New testcase
    llvm-svn: 19744

---
+++ java/test/Programs/SingleSource/UnitTests/Lists4.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Lists4
+{
+    public static void main(String[] args) {
+        LinkedList llist = new LinkedList();
+        llist.add(null);
+        Test.println(llist.getFirst() == null);
+        Test.println(llist.contains(null));
+    }
+}

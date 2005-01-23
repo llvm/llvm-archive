dffd99a74c85d6855ec981f0f17052809506bec2
Alkis Evlogimenos <alkis@evlogimenos.com>
2005-01-23 15:06:33 +0000

    Add test case distilled from Lists3.java.
    llvm-svn: 19785

---
+++ java/test/Programs/SingleSource/UnitTests/Lists5.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Lists5
+{
+    public static void main(String[] args) {
+        LinkedList llist = new LinkedList();
+        Iterator i = llist.iterator();
+        Test.println(i.hasNext());
+    }
+}

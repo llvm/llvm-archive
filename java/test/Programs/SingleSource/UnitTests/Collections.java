0bb2703a403dcf0f7d5902a92a18a7ba8fb0a723
Alkis Evlogimenos <alkis@evlogimenos.com>
2005-01-25 16:45:11 +0000

    Share some code and make sure we are calling methods through the Collection interface
    llvm-svn: 19831

--- java/test/Programs/SingleSource/UnitTests/Collections.java
+++ java/test/Programs/SingleSource/UnitTests/Collections.java
@@ -2,11 +2,19 @@ import java.util.*;
 
 public class Collections
 {
+    public static void fillCollectionWithInts(Collection c) {
+        for (int i = 0; i < 10; ++i)
+            c.add(new Integer(i));
+    }
+
+    public static void printIntCollection(Collection c) {
+        for (Iterator i = c.iterator(); i.hasNext(); )
+            Test.println(((Integer)i.next()).intValue());
+    }
+
     public static void main(String[] args) {
         Collection c1 = new TreeSet();
-        for (int i = 0; i < 100; ++i) {
-            c1.add(new Integer(i));
-        }
+        fillCollectionWithInts(c1);
         Collection c2 = new TreeSet(c1);
 
         Test.println(c1.remove(new Integer(5)));
--- java/test/Programs/SingleSource/UnitTests/Collections2.java
+++ java/test/Programs/SingleSource/UnitTests/Collections2.java
@@ -4,12 +4,7 @@ public class Collections2
 {
     public static void main(String[] args) {
         Collection c1 = new TreeSet();
-        for (int i = 0; i < 100; ++i) {
-            c1.add(new Integer(i));
-        }
-
-        for (Iterator i = c1.iterator(); i.hasNext(); ) {
-            Test.println(((Integer) i.next()).intValue());
-        }
+        Collections.fillCollectionWithInts(c1);
+        Collections.printIntCollection(c1);
     }
 }

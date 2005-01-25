e2933789deedc08688d76b5464b1f47eb83a7b98
Alkis Evlogimenos <alkis@evlogimenos.com>
2005-01-25 15:52:48 +0000

    Make test a bit more interesting
    llvm-svn: 19826

--- java/test/Programs/SingleSource/UnitTests/Collections.java
+++ java/test/Programs/SingleSource/UnitTests/Collections.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Collections
 {
     public static void main(String[] args) {
-        Collection c1 = new LinkedList();
+        Collection c1 = new TreeSet();
         for (int i = 0; i < 100; ++i) {
             c1.add(new Integer(i));
         }

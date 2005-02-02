f202c18613da17e68ab2b6c4e88ae08397128a49
Alkis Evlogimenos <alkis@evlogimenos.com>
2005-02-02 15:54:17 +0000

    java.lang.Math uses JNI calls that are not implemented yet.
    llvm-svn: 19998

--- java/test/Programs/SingleSource/UnitTests/ArrayCopy.java
+++ java/test/Programs/SingleSource/UnitTests/ArrayCopy.java
@@ -2,6 +2,10 @@ import java.util.*;
 
 public class ArrayCopy
 {
+    public static int min(int a, int b) {
+        return a < b ? a : b;
+    }
+
     public static void main(String[] args) {
         int[] iarray1 = new int[10];
         for (int i = 0; i < iarray1.length; ++i)
@@ -9,7 +13,7 @@ public class ArrayCopy
 
         int[] iarray2 = new int[123];
         for (int i = 0; i < iarray2.length; i += iarray1.length)
-            System.arraycopy(iarray1, 0, iarray2, i, Math.min(iarray2.length - i, iarray1.length));
+            System.arraycopy(iarray1, 0, iarray2, i, min(iarray2.length - i, iarray1.length));
 
         for (int i = 0; i < iarray2.length; ++i)
             Test.println(iarray2[i]);

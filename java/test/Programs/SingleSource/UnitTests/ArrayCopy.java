54b0a87f1c39b1d5119e6cd2ec096c974df22f4e
Alkis Evlogimenos <alkis@evlogimenos.com>
2005-02-02 07:12:08 +0000

    Add test
    llvm-svn: 19996

---
+++ java/test/Programs/SingleSource/UnitTests/ArrayCopy.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class ArrayCopy
+{
+    public static void main(String[] args) {
+        int[] iarray1 = new int[10];
+        for (int i = 0; i < iarray1.length; ++i)
+            iarray1[i] = i;
+
+        int[] iarray2 = new int[123];
+        for (int i = 0; i < iarray2.length; i += iarray1.length)
+            System.arraycopy(iarray1, 0, iarray2, i, Math.min(iarray2.length - i, iarray1.length));
+
+        for (int i = 0; i < iarray2.length; ++i)
+            Test.println(iarray2[i]);
+    }
+}

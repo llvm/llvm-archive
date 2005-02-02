45ca19fabcd49710d7229dda60bc84ea94ee834a
Alkis Evlogimenos <alkis@evlogimenos.com>
2005-02-02 18:42:22 +0000

    Add test for ArrayList as a Collection
    llvm-svn: 20007

---
+++ java/test/Programs/SingleSource/UnitTests/Collections3.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Collections3
+{
+    public static void main(String[] args) {
+        Collection c1 = new ArrayList();
+        Collections.fillCollectionWithRandomInts(c1);
+        Collections.printIntCollection(c1);
+    }
+}

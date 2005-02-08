416d33b74ab58f3c2d498639d7f9b2ba0ca1e33c
Alkis Evlogimenos <alkis@evlogimenos.com>
2005-02-08 01:37:50 +0000

    Make this test fail.
    llvm-svn: 20068

--- java/test/Programs/SingleSource/UnitTests/Lists1.java
+++ java/test/Programs/SingleSource/UnitTests/Lists1.java
@@ -4,7 +4,7 @@ public class Lists1
 {
     public static void main(String[] args) {
         List list = new LinkedList();
-        for (int i = 0; i < 10; ++i) {
+        for (int i = 0; i < 17; ++i) {
             list.add(new Integer(i));
         }
 

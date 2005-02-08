89f82aff4ebdb7c92af0a02adcafd3bdd4159725
Alkis Evlogimenos <alkis@evlogimenos.com>
2005-02-08 02:49:03 +0000

    Make test case fail.
    llvm-svn: 20072

--- java/test/Programs/SingleSource/UnitTests/Collections5.java
+++ java/test/Programs/SingleSource/UnitTests/Collections5.java
@@ -2,15 +2,19 @@ import java.util.*;
 
 public class Collections5
 {
+    public static void testList(List l) {
+        Collections.addRandomIntsToCollection(l);
+        Collections.printIntCollection(l);
+        java.util.Collections.rotate(l, Collections.rand.nextInt(50000));
+        Collections.printIntCollection(l);
+        java.util.Collections.shuffle(l, Collections.rand);
+        Collections.printIntCollection(l);
+        java.util.Collections.sort(l);
+        Collections.printIntCollection(l);
+    }
+
     public static void main(String[] args) {
-        List l1 = new ArrayList();
-        Collections.addRandomIntsToCollection(l1);
-        Collections.printIntCollection(l1);
-        java.util.Collections.rotate(l1, Collections.rand.nextInt(50000));
-        Collections.printIntCollection(l1);
-        java.util.Collections.shuffle(l1, Collections.rand);
-        Collections.printIntCollection(l1);
-        java.util.Collections.sort(l1);
-        Collections.printIntCollection(l1);
+        testList(new LinkedList());
+        testList(new ArrayList());
     }
 }

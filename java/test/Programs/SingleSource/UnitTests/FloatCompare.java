dcb43b427381719bf90155cd38818b1d456b4c5c
Alkis Evlogimenos <alkis@evlogimenos.com>
2005-02-18 04:29:50 +0000

    Improve test case.
    llvm-svn: 20235

--- java/test/Programs/SingleSource/UnitTests/FloatCompare.java
+++ java/test/Programs/SingleSource/UnitTests/FloatCompare.java
@@ -1,14 +1,35 @@
 public class FloatCompare
 {
+    private static float[] doubles =
+        new float[]{ -1.0f, 0.0f, -0.0f, 1.0f,
+                     Float.NaN,
+                     Float.NEGATIVE_INFINITY,
+                     Float.POSITIVE_INFINITY };
+
     public static void main(String[] args) {
-        int count = 0;
+        int greater = 0;
+        int equal = 0;
+        int less = 0;
+        int unordered = 0;
 
-        for (float f = 0.0F; f < 10F; f += 1.1F)
-            ++count;
+        for (int i = 0; i < doubles.length; ++i) {
+            double a = doubles[i];
+            for (int j = 0; j < doubles.length; ++j) {
+                double b = doubles[j];
+                if (a > b)
+                    ++greater;
+                else if (a < b)
+                    ++less;
+                else if (a == b)
+                    ++equal;
+                else
+                    ++unordered;
+            }
+        }
 
-        for (double d = 100; d > 0; d -= 11)
-            ++count;
-
-        Test.println(count);
+        Test.println(greater);
+        Test.println(equal);
+        Test.println(less);
+        Test.println(unordered);
     }
 }

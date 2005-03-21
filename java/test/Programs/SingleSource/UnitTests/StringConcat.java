3ace1e1d544c6a053c5dbdbd9b0560806d4a02b5
Alkis Evlogimenos <alkis@evlogimenos.com>
2005-03-21 20:55:22 +0000

    Add string concatenation test
    llvm-svn: 20744

---
+++ java/test/Programs/SingleSource/UnitTests/StringConcat.java
@@ -0,0 +1,10 @@
+public class StringConcat {
+    public static void main(String[] args) {
+        String foo = "foo";
+        Test.println(foo);
+        String bar = "bar";
+        Test.println(bar);
+        String foobar = foo + bar;
+        Test.println(foobar);
+    }
+}

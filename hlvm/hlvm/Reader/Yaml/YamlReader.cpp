2c72555ebdef678a23989abd76641325399f2276
Reid Spencer <rspencer@reidspencer.com>
2006-05-11 15:40:00 +0000

    Add skeleton for YamlReader.
    llvm-svn: 38005

--- hlvm/hlvm/Reader/Yaml/YamlReader.cpp
+++ hlvm/hlvm/Reader/Yaml/YamlReader.cpp
@@ -21,16 +21,50 @@
 /// @brief Declares the class hlvm::YamlReader.cpp
 ////////////////////////////////////////////////////////////////////////////////
 
-#include "YamlReader.h"
+#include <hlvm/Reader/Yaml/YamlReader.h>
+#include <llvm/System/MappedFile.h>
 #include <syck.h>
 
-namespace hlvm {
+namespace {
 
-class YamlReaderImpl : public YamlReader {
+class YamlReaderImpl : public hlvm::YamlReader {
+  public:
+    YamlReaderImpl() {
+      parser_ = syck_new_parser();
+      syck_parser_handler(parser_, SyckNodeHandler(NodeHandler));
+      syck_parser_error_handler(parser_, SyckErrorHandler(ErrorHandler));
+      syck_parser_bad_anchor_handler(parser_, 
+        SyckBadAnchorHandler(BadAnchorHandler));
+    }
+
+    static SYMID NodeHandler(SyckParser*, SyckNode* )
+    {
+      return 0;
+    }
+
+    static void ErrorHandler(SyckParser*, char*)
+    {
+    }
+
+    static SyckNode* BadAnchorHandler(SyckParser*, char*)
+    {
+      return 0;
+    }
+
+    void parse(const llvm::sys::Path& path) {
+      llvm::sys::MappedFile mapfile(path);
+      mapfile.map();
+      syck_parser_str(parser_, mapfile.charBase(), mapfile.size(),0);
+      root_ = syck_parse(parser_);
+    }
+
+  private:
+    SyckParser* parser_;
+    SYMID root_;
 };
 
-YamlReader* YamlReader::create() {
+} // anonymous
+
+hlvm::YamlReader* hlvm::YamlReader::create() {
   return new YamlReaderImpl();
 }
-
-} // hlvm

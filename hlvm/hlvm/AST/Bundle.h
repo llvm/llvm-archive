fc3832a508e542c1579996f6587c6deddbc8143c
Reid Spencer <rspencer@reidspencer.com>
2006-05-12 15:41:12 +0000

    Fix the copyright and license notice on each file.
    llvm-svn: 38011

--- hlvm/hlvm/AST/Bundle.h
+++ hlvm/hlvm/AST/Bundle.h
@@ -1,35 +1,39 @@
+//===-- hlvm/AST/Bundle.h - AST Bundle Class --------------------*- C++ -*-===//
 //
-// Copyright (C) 2006 HLVM Group. All Rights Reserved.
+//                      High Level Virtual Machine (HLVM)
 //
-// This program is open source software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License (GPL) as published by
-// the Free Software Foundation; either version 2 of the License, or (at your
-// option) any later version. You should have received a copy of the GPL in a
-// file named COPYING that was included with this program; if not, you can
-// obtain a copy of the license through the Internet at http://www.fsf.org/
+// Copyright (C) 2006 Reid Spencer. All Rights Reserved.
 //
-// This program is distributed in the hope that it will be useful, but
-// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-// or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-// for more details.
+// This software is free software; you can redistribute it and/or modify it 
+// under the terms of the GNU Lesser General Public License as published by 
+// the Free Software Foundation; either version 2.1 of the License, or (at 
+// your option) any later version.
 //
-////////////////////////////////////////////////////////////////////////////////
+// This software is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for 
+// more details.
+//
+// You should have received a copy of the GNU Lesser General Public License 
+// along with this library in the file named LICENSE.txt; if not, write to the 
+// Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, 
+// MA 02110-1301 USA
+//
+//===----------------------------------------------------------------------===//
 /// @file hlvm/AST/Bundle.h
 /// @author Reid Spencer <reid@hlvm.org> (original author)
 /// @date 2006/05/04
 /// @since 0.1.0
 /// @brief Declares the class hlvm::AST::Bundle
-////////////////////////////////////////////////////////////////////////////////
+//===----------------------------------------------------------------------===//
 
 #ifndef HLVM_AST_BUNDLE_H
 #define HLVM_AST_BUNDLE_H
 
 #include <hlvm/AST/Node.h>
 
-namespace hlvm
-{
-namespace AST
-{
+namespace hlvm {
+namespace AST {
   /// This class represents an HLVM Bundle. A Bundle is simply a collection of
   /// declarations and definitions. It is the root of the AST tree and also
   /// the grouping and namespace construct in HLVM. Every compilation unit is
--- hlvm/hlvm/AST/Function.h
+++ hlvm/hlvm/AST/Function.h
@@ -1,25 +1,31 @@
+//===-- hlvm/AST/Function.h - AST Function Class ----------------*- C++ -*-===//
 //
-// Copyright (C) 2006 HLVM Group. All Rights Reserved.
+//                      High Level Virtual Machine (HLVM)
 //
-// This program is open source software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License (GPL) as published by
-// the Free Software Foundation; either version 2 of the License, or (at your
-// option) any later version. You should have received a copy of the GPL in a
-// file named COPYING that was included with this program; if not, you can
-// obtain a copy of the license through the Internet at http://www.fsf.org/
+// Copyright (C) 2006 Reid Spencer. All Rights Reserved.
 //
-// This program is distributed in the hope that it will be useful, but
-// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-// or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-// for more details.
+// This software is free software; you can redistribute it and/or modify it 
+// under the terms of the GNU Lesser General Public License as published by 
+// the Free Software Foundation; either version 2.1 of the License, or (at 
+// your option) any later version.
 //
-////////////////////////////////////////////////////////////////////////////////
+// This software is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for 
+// more details.
+//
+// You should have received a copy of the GNU Lesser General Public License 
+// along with this library in the file named LICENSE.txt; if not, write to the 
+// Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, 
+// MA 02110-1301 USA
+//
+//===----------------------------------------------------------------------===//
 /// @file hlvm/AST/Function.h
 /// @author Reid Spencer <reid@hlvm.org> (original author)
 /// @date 2006/05/04
 /// @since 0.1.0
 /// @brief Declares the class hlvm::AST::Function
-////////////////////////////////////////////////////////////////////////////////
+//===----------------------------------------------------------------------===//
 
 #ifndef HLVM_AST_FUNCTION_H
 #define HLVM_AST_FUNCTION_H

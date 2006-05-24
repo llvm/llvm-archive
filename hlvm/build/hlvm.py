1cea2b95dab600824ca29d2bea2808b4f9631930
Reid Spencer <rspencer@reidspencer.com>
2006-05-24 16:10:01 +0000

    Support installation of header files.
    llvm-svn: 38094

--- hlvm/build/hlvm.py
+++ hlvm/build/hlvm.py
@@ -1,7 +1,9 @@
 from SCons.Options import Options as Options
 from SCons.Options import BoolOption as BoolOption
 from SCons.Options import PathOption as PathOption
+from SCons.Options import PackageOption as PackageOption
 from SCons.Script.SConscript import SConsEnvironment as SConsEnvironment
+from SCons.Script import COMMAND_LINE_TARGETS as COMMAND_LINE_TARGETS
 from SCons.Environment import Environment as Environment
 from configure import ConfigureHLVM as ConfigureHLVM
 from os.path import join as pjoin
@@ -9,13 +11,13 @@ from string import join as sjoin
 from string import replace as strrepl
 import glob
 
+def GetFiles(env,pat,dir=''):
+  spec = pjoin(env.Dir('.').abspath,dir,pat)
+  spec = strrepl(spec,pjoin(env['BuildDir'],''),'',1)
+  return glob.glob(spec)
+
 def GetAllCXXFiles(env):
-  dir = env.Dir('.').abspath
-  dir = strrepl(dir,pjoin(env['BuildDir'],''),'',1)
-  p1 = glob.glob(pjoin(dir,'*.cpp'))
-  p2 = glob.glob(pjoin(dir,'*.cxx'))
-  p3 = glob.glob(pjoin(dir,'*.C'))
-  return env.Flatten([p1,p2,p3])
+  return env.Flatten([GetFiles(env,'*.cpp'),GetFiles(env,'*.cxx')])
 
 def GetRNGQuoteSource(env):
   from build import filterbuilders
@@ -36,21 +38,22 @@ def Dirs(env,dirlist=[]):
     env.SConscript(sconsfile)
 
 def InstallProgram(env,prog):
-  dir = pjoin(env['prefix'],'bin')
-  env.Install(dir,prog)
-  env.Alias('install',dir)
+  if 'install' in COMMAND_LINE_TARGETS:
+    dir = pjoin(env['prefix'],'bin')
+    env.Install(dir,prog)
   return 1
 
 def InstallLibrary(env,lib):
-  dir = pjoin(env['prefix'],'lib')
-  env.Install(dir,lib)
-  env.Alias('install',dir)
+  if 'install' in COMMAND_LINE_TARGETS:
+    libdir = pjoin(env['prefix'],'lib')
+    env.Install(dir,lib)
   return 1
 
-def InstallHeader(env,hdrname):
-  dir = pjoin(env['prefix'],'include')
-  env.Install(dir,hdrname)
-  env.Alias('install',dir)
+def InstallHeader(env,hdrs):
+  if 'install' in COMMAND_LINE_TARGETS:
+    moddir = strrepl(env.Dir('.').path,pjoin(env['BuildDir'],''),'',1)
+    dir = pjoin(env['prefix'],'include',moddir)
+    env.Install(dir,hdrs)
   return 1
 
 def GetBuildEnvironment(targets,arguments):
@@ -67,8 +70,12 @@ def GetBuildEnvironment(targets,arguments):
     BoolOption('optimize','Build object files with optimization',0),
     BoolOption('profile','Generate profiling aware code',0),
     BoolOption('small','Generate smaller code rather than faster',0),
-    PathOption('prefix','Specify where to install HLVM','/usr/local')
+    PackageOption('llvm','Specify where LLVM is located','search'),
+    PackageOption('apr','Specify where apr is located','search'),
+    PackageOption('apru','Specify where apr-utils is located','search'),
+    PackageOption('xml2','Specify where LibXml2 is located','search'),
   )
+  opts.Add('prefix','Specify where to install HLVM','/usr/local')
   opts.Update(env)
   opts.Save('.options_cache',env)
   env['HLVM_Copyright'] = 'Copyright (c) 2006 Reid Spencer'
@@ -145,6 +152,10 @@ def GetBuildEnvironment(targets,arguments):
   env.BuildDir(pjoin(BuildDir,'tools'),'tools',duplicate=0)
   env.BuildDir(pjoin(BuildDir,'test'),'test',duplicate=0)
   env.SConsignFile(pjoin(BuildDir,'sconsign'))
+  if 'install' in COMMAND_LINE_TARGETS:
+    env.Alias('install',pjoin(env['prefix'],'bin'))
+    env.Alias('install',pjoin(env['prefix'],'lib'))
+    env.Alias('install',pjoin(env['prefix'],'include'))
   env.Help("""
 HLVM Build Environment
 
@@ -153,16 +164,10 @@ Usage Examples::
   scons --clean     - to remove all derived (built) objects
   scons check       - to run the DejaGnu test suite
   scons install     - to install HLVM to a target directory
+  scons doxygen     - to generate the doxygen documentation
 
-Options:                                                     (Default)
-  debug=on/off      - include debug symbols and code?        (on)
-  assrt=on/off      - include code assertions?               (on)
-  optimize=on/off   - optimize generated code?               (off)
-  inline=on/off     - make inline calls inline?              (off)
-  small=on/off      - make smaller rather than faster code?  (off)
-  profile=on/off    - generate code for gprof profiling?     (off)
-  prefix=<path>     - specify where HLVM should be installed (/usr/local)
-""")
+Options:
+""" + opts.GenerateHelpText(env,sort=cmp))
   print "HLVM BUILD MODE:", VariantName
   ConfigureHLVM(env)
   return env

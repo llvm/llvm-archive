67fb21854dbffc782ce4cfad30af0d8a98c4556f
Reid Spencer <rspencer@reidspencer.com>
2006-07-01 17:30:22 +0000

    Count documentation lines as well, but separately.
    llvm-svn: 38333

--- hlvm/utils/countloc.py
+++ hlvm/utils/countloc.py
@@ -27,27 +27,36 @@ import datetime;
 suffixes = ['.h','.cpp','.cxx','.py','.rng','.in','.xsl','.sh','.pm','.hlx',
             '.exp','.ll','.sh']
 
+docsuffixes = ['.html','.css','.pod','.header','.footer','.intro']
+
 filenames = ['SConscript', 'SConstruct','Makefiles' ]
 
 notdirs = ['.svn','Release','Debug','Optimized','Profile','default']
 
 total = 0
+doctotal = 0
 for root, dirs, files in os.walk('.'):
   for name in files:
     (basename,ext) = os.path.splitext(name)
     if ext in suffixes or name in filenames:
-      bytes = getsize(join(root,name))
       lines = 0
       for line in open(join(root,name),'r'):
         lines += 1
-      #print join(root,name), ":", lines,"(",bytes,"bytes )"
+      print join(root,name), ":", lines
       total += lines
+    if ext in docsuffixes:
+      lines = 0
+      for line in open(join(root,name),'r'):
+        lines += 1
+      doctotal += lines
   for d in notdirs:
     if d in dirs:
       dirs.remove(d)
 
 print "Lines Of Code in HLVM:",total
+print "Lines Of Doc  in HLVM:",doctotal
 now = datetime.date.today()
 then = datetime.date(2006,4,24)
 days = now.toordinal() - then.toordinal()
-print "Productivity:",total / ((days * 5) / 7),"loc/day"
+print "Code Productivity:",total / ((days * 5) / 7),"loc/day"
+print "Total Productivity:", (total+doctotal) / ((days * 5) / 7), "lines/day"

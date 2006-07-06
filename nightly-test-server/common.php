5f2d11390113b9f9c982ec8fbf5133ae5b41cd22
Patrick Jenkins <pjenkins@apple.com>
2006-07-06 17:51:10 +0000

    Initial checkin of common.php. This file takes long urls and enters them into the database so we can hand out short urls.
    llvm-svn: 29012

---
+++ nightly-test-server/common.php
@@ -0,0 +1,50 @@
+<?php
+$mysql_link=mysql_connect("127.0.0.1","llvm","ll2002vm") or die("Error: could not connect to database!\n");
+mysql_select_db("nightlytestresults");
+
+$params = $_SERVER['QUERY_STRING'];
+
+if(is_numeric($params)){
+	$query="SELECT url FROM tinyurl WHERE id=$params";
+	$resource = mysql_query($query);
+
+	if($row = mysql_fetch_array($resource)){
+		include("http://{$row['url']}");
+	}
+	else{
+		print "No such page!\n";
+	}
+	mysql_free_result($resource);
+}
+else{
+	$this_url = $params;	
+	$query="SELECT * FROM tinyurl WHERE url=\"$this_url\"";
+        $result = mysql_query($query);
+	if($row = mysql_fetch_array($result)){
+               header( "Location: http://{$_SERVER['SERVER_NAME']}{$_SERVER['SCRIPT_NAME']}?{$row['id']}" );
+               include("http://{$row['url']}");
+        }
+        else{
+		$query="INSERT INTO tinyurl (url) values (\"$this_url\")";
+		$resource = mysql_query($query);
+		mysql_free_result($resource);
+		$query="SELECT * FROM tinyurl WHERE url=\"$this_url\"";
+	        $resource = mysql_query($query);
+	
+	        if($row = mysql_fetch_array($resource)){
+			header( "Location: http://{$_SERVER['SERVER_NAME']}{$_SERVER['SCRIPT_NAME']}?{$row['id']}" );
+			include("http://{$row['url']}");	
+	        }
+	        else{
+	                print "Error, could not create page!!\n";
+	        }
+	        mysql_free_result($resource);
+	}
+	mysql_free_result($result);
+}
+
+
+
+
+
+?>

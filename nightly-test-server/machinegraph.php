df13b16bfb0de0c00b924073f22d7f960b17c53c
Patrick Jenkins <pjenkins@apple.com>
2006-08-17 17:32:07 +0000

    Created a more descriptive error message for when a user tries to create a graph from a machines page without selecting anythign to graph. The previous error message said "ERROR: Incorrect URL" which led users to believe the problem was on the serverside.
    llvm-svn: 29749

--- nightly-test-server/machinegraph.php
+++ nightly-test-server/machinegraph.php
@@ -1,7 +1,7 @@
 <?php
 
 if(!isset($HTTP_GET_VARS['measure'])){
-        die("ERROR: Incorrect URL\n");
+        die("ERROR: Please select a checkbox at the top of a column to create a graph.\n");
 }
 
 $measure=$HTTP_GET_VARS['measure'];

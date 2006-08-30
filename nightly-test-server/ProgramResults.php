4a3ff6049ea40ddef1ed3e796a1c1af4b735d3f3
Jim Laskey <jlaskey@mac.com>
2006-08-30 21:50:06 +0000

    Debugging newly pass fail report attempt #12.
    llvm-svn: 29986

--- nightly-test-server/ProgramResults.php
+++ nightly-test-server/ProgramResults.php
@@ -507,7 +507,7 @@ function getRemovedTests($cur_id, $prev_id, $mysql_link){
     while($row = mysql_fetch_array($program_query)){
       $test_key = "{$row['measure']} - {$row['program']}";
       if(!isset($test_hash[$test_key])){
-        $result .= "{$row['result']}" . ":" . $test_key . "<br>\n";
+        $result .= $test_key . "<br>\n";
       }
     }
     mysql_free_result($program_query);
@@ -561,8 +561,8 @@ function getFixedTests($cur_id, $prev_id, $mysql_link){
     $query = "SELECT * FROM program WHERE night=$prev_id";
     $program_query = mysql_query($query) or die (mysql_error());
     while($row = mysql_fetch_array($program_query)){
-      if(!(strpos("{$row['result']}", "*") === false)) {
-        $test_hash["{$row['program']}"]=$row['result'];
+      if(!(strpos($row['result'], "*") === false)) {
+        $test_hash[$row['program']]=$row['result'];
       }    
     }
     mysql_free_result($program_query);
@@ -570,9 +570,9 @@ function getFixedTests($cur_id, $prev_id, $mysql_link){
     $query = "SELECT * FROM program WHERE night=$cur_id ORDER BY program ASC";
     $program_query = mysql_query($query) or die (mysql_error());
     while($row = mysql_fetch_array($program_query)){
-      $test_key = "{$row['program']}";
-      if(isset($test_hash[$test_key]) && strpos("{$row['result']}", "*") === false){
-        $result .= "{$row['result']}" . ":" . $test_key . "<br>\n";
+      $test_key = $row['program'];
+      if(isset($test_hash[$test_key]) && strpos($row['result'], "*") === false){
+        $result .= $row['result'] . ":" . $test_key . "<br>\n";
       }
     }
     mysql_free_result($program_query);
@@ -626,8 +626,8 @@ function getBrokenTests($cur_id, $prev_id, $mysql_link){
     $query = "SELECT * FROM program WHERE night=$cur_id";
     $program_query = mysql_query($query) or die (mysql_error());
     while($row = mysql_fetch_array($program_query)){
-      if(strpos("{$row['result']}", "*") === false) {
-        $test_hash["{$row['program']}"]=1;
+      if(strpos($row['result'], "*") === false) {
+        $test_hash[$row['program']]=1;
       }    
     }
     mysql_free_result($program_query);
@@ -635,9 +635,9 @@ function getBrokenTests($cur_id, $prev_id, $mysql_link){
     $query = "SELECT * FROM program WHERE night=$prev_id ORDER BY program ASC";
     $program_query = mysql_query($query) or die (mysql_error());
     while($row = mysql_fetch_array($program_query)){
-      $test_key = "{$row['program']}";
-      if(isset($test_hash[$test_key]) && !(strpos("{$row['result']}", "*") === false)){
-        $result .= $test_key . "<br>\n";
+      $test_key = $row['program'];
+      if(isset($test_hash[$test_key]) && !(strpos($row['result'], "*") === false)){
+        $result .= $row['result'] . ":" . $test_key . "<br>\n";
       }
     }
     mysql_free_result($program_query);

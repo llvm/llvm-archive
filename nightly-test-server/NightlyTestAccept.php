7b915fd8da674c482b815dbb6a644a33999afd68
Jim Laskey <jlaskey@mac.com>
2006-09-14 17:15:21 +0000

    testing #33
    llvm-svn: 30366

--- nightly-test-server/NightlyTestAccept.php
+++ nightly-test-server/NightlyTestAccept.php
@@ -827,6 +827,8 @@ foreach ($prog_hash_new as $prog) {
   $prog_new = $prog_hash_new[$prog];
   $prog_old = $prog_hash_old[$prog];
     
+print "$prog\n    $prog_new\n    $prog_old\n";
+
   // get data from server
   $vals_new = split(",", $prog_new);
   $vals_old = split(",", $prog_old);

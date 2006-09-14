bf8c75180da07be6f8e402609e894bcccf09c7cc
Jim Laskey <jlaskey@mac.com>
2006-09-14 15:33:18 +0000

    Set up query test
    llvm-svn: 30358

--- nightly-test-server/EmailReport.php
+++ nightly-test-server/EmailReport.php
@@ -2,57 +2,23 @@
 <body>
 <?php
 
-/*
-if(!isset($HTTP_GET_VARS['machine']) || !is_numeric($HTTP_GET_VARS['machine'])){
-        print "Error: Incorrect URL!\n";
-        die();
-}
-$machine_id = $HTTP_GET_VARS['machine'];
-
-if(!isset($HTTP_GET_VARS['night']) || !is_numeric($HTTP_GET_VARS['night'])){
-        print "Error: Incorrect URL!\n";
-        die();
-}
-$night_id = $HTTP_GET_VARS['night'];
-
-if(!(include "NightlyTester.php")) {
-  print "Error: could not load necessary files!\n";
-  die();
-}
-
-if(!(include"ProgramResults.php")) {
-  print "Error: could not load necessary files!\n";
-  die();
-}
-
-$mysql_link=mysql_connect("127.0.0.1","llvm","ll2002vm") or die("Error: could not connect to database!\n");
+$mysql_link = mysql_connect("127.0.0.1","llvm","ll2002vm") or die("Error: could not connect to database!\n");
 mysql_select_db("nightlytestresults");
 
-$query = "DELETE FROM night WHERE machine = \"56\"";
-$night_query = mysql_query($query) or die (mysql_error());
-/*
-if ($row = mysql_fetch_array($night_query) or die (mysql_error())) {
-  while ($value = current($row)) {
-    $key_name = key($row);
-    print "$key_name = $value<br>\n";
-    next($row);
+$query = $_POST["Query"];
+$query = preg_replace("/\n/", " ", $query);
+$my_query = mysql_query($query) or die (mysql_error());
+
+while ($row = mysql_fetch_array($my_query)) {
+  foreach ($row as $key => $value) {
+    print "$key => $value, ";
   }
+  print "<br>\n";
 }
 
-mysql_free_result($night_query);
-
-$query = "DELETE FROM night WHERE machine = \"56\"";
-$night_query = mysql_query($query) or die (mysql_error());
-mysql_free_result($night_query);
-
-
+mysql_free_result($my_query);
 
 mysql_close($mysql_link);
-*/
-
-$Query = $_POST["Query"];
-
-print "$Query\n";
 
 ?>
 </body>

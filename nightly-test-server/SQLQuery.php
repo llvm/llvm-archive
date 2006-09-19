3e427f8f569b39952b435ca92a03401e1b268d1c
Jim Laskey <jlaskey@mac.com>
2006-09-19 17:46:04 +0000

    make multiple queries easier
    llvm-svn: 30489

--- nightly-test-server/SQLQuery.php
+++ nightly-test-server/SQLQuery.php
@@ -1,11 +1,11 @@
 <?php
 $user = $_POST["User"];
 $password = $_POST["Password"];
-$query = $_POST["Query"];
-$was_query = isset($query);
+$queries = $_POST["Queries"];
+$was_query = isset($queries);
 if (!isset($user)) $user = "";
 if (!isset($password)) $password = "";
-if (!isset($query)) $query = "";
+if (!isset($queries)) $queries = "";
 ?>
 
 <HTML>
@@ -31,7 +31,7 @@ print <<<EOD
 		<TD>Password:</TD> <TD><INPUT NAME="Password" ID="Password" TYPE="password" VALUE="$password" SIZE="30"><BR></TD>
 	</TR>
 	<TR>
-		<TD>Query:</TD> <TD><INPUT NAME="Query" ID="Query" TYPE="text" VALUE="$query" SIZE="100"><BR></TD>
+		<TD>Query:</TD> <TD><TEXTAREA ID="Queries" NAME="Queries" ROWS="10" COLS="100"></TEXTAREA><BR></TD>
 	</TR>
 </TABLE>
 <BUTTON TYPE="submit" ID="Send" NAME="Send" VALUE="Send">Query</BUTTON><BR>
@@ -39,35 +39,44 @@ print <<<EOD
 EOD;
 
 if ($was_query) {
+  $queries = split("\n", $queries);
+
   $mysql_link = mysql_connect("127.0.0.1", $user, $password) or die("Error: could not connect to database!\n");
   mysql_select_db("nightlytestresults");
   
-  $query = $_POST["Query"];
-  $my_query = mysql_query($query) or die (mysql_error());
-  
-  print "<TABLE>\n";
-  $heading = false;
-  
-  while ($row = mysql_fetch_assoc($my_query)) {
-    if (!$heading) {
-      print "  <TR STYLE=\"font-weight: bold;\">\n";
-      foreach ($row as $key => $value) {
-        print "    <TD>$key</TD>\n";
-      }
-      print "  </TR>\n";
-      $heading = true;
-    }
+  foreach ($queries as $query) {
+    $query = rtrim($query);
+    if (strlen($query) == 0) continue;
     
-    print "  <TR>\n";
-    foreach ($row as $key => $value) {
-      print "    <TD>$value</TD>\n";
-    }
-    print "  </TR>\n";
-  }
+    if ($my_query = mysql_query($query)) {
+      print "<TABLE>\n";
+      $heading = false;
+      
+      while ($row = mysql_fetch_assoc($my_query)) {
+        if (!$heading) {
+          print "  <TR STYLE=\"font-weight: bold;\">";
+          foreach ($row as $key => $value) {
+            print "    <TD>$key</TD>";
+          }
+          print "  </TR>\n";
+          $heading = true;
+        }
+        
+        print "  <TR>";
+        foreach ($row as $key => $value) {
+          print "    <TD>$value</TD>";
+        }
+        print "  </TR>\n";
+      }
+      
+      print "</TABLE><BR><BR><BR>\n";
   
-  print "</TABLE>\n";
-
-  mysql_free_result($my_query);
+      mysql_free_result($my_query);
+    } else {
+      $error = mysql_error();
+      print "$error<BR>\n";
+    }
+  }
   
   mysql_close($mysql_link);
 }

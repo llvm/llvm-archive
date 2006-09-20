b993d153021f3bd8a693bcd5b5c45bf1ad2fb843
Jim Laskey <jlaskey@mac.com>
2006-09-20 16:44:06 +0000

    Clean up test records #10
    llvm-svn: 30537

--- nightly-test-server/SQLUtil.php
+++ nightly-test-server/SQLUtil.php
@@ -15,13 +15,13 @@ BODY
 $mysql_link = mysql_connect("127.0.0.1", "llvm", "ll2002vm") or die("Error: could not connect to database!\n");
 mysql_select_db("nightlytestresults");
 
-$query = "SELECT * FROM tests WHERE measure=\"dejagnu\" AND program LIKE \"%/llvm/test/%\"";
+$query = "SELECT * FROM tests WHERE measure=\"dejagnu\" AND program LIKE \"%%\"";
 if ($get_query = mysql_query($query)) {
   $count = 1;
   while ($row = mysql_fetch_assoc($get_query)) {
     $old = $row['program'];
     $subpatterns = explode("/llvm/test/", $old, 2);
-    $new = $subpatterns[1];
+    $after = $subpatterns[1];
     print "$count: $new\n";
     if (isset($new)) {
       $new = "test/".$after;
@@ -29,11 +29,11 @@ if ($get_query = mysql_query($query)) {
       $night =  $row['night'];
       $query = "UPDATE tests SET program=\"$new\" WHERE night=$night AND program=\"$old\" AND result=\"$result\" AND measure=\"dejagnu\"";
       print "$query\n";
-      $set_query = mysql_query($query);
-      mysql_free_result($set_query);
+#      $set_query = mysql_query($query);
+#      mysql_free_result($set_query);
     }
     $count = $count + 1;
-    if (($count % 1000) == 0) break;
+    if (($count % 100) == 0) break;
   }
 
   mysql_free_result($get_query);

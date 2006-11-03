4c7d6bd7a29776f64134fd39cb5a843dbb6feedf
Jim Laskey <jlaskey@mac.com>
2006-11-03 19:48:48 +0000

    Test #2
    llvm-svn: 31421

--- nightly-test-server/ProgramResults.php
+++ nightly-test-server/ProgramResults.php
@@ -143,8 +143,8 @@ function GetDayResults($night_id, $array_of_measures ){
     $data = str_replace("<br>", " ", $data);
     foreach ($array_of_measures as $x){
       $value=array();
-      $reg_exp="/$x:\s*([[0-9\.]+|\*|\-|n\/a|\?])/";
-      #print "{$program} => running preg_match($reg_exp, $data, $value)<br>\n";
+      $reg_exp="/$x:\s*([[0-9\.]+|\*|\-|n\/a|\?],)/";
+      print "{$program} => running preg_match($reg_exp, $data, $value)<br>\n";
       preg_match($reg_exp, $data, $value);
       if(isset($value[1])){
         array_push($result[$program], $value[1]);

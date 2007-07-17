8f7642790d71d85ef86722840e12c7cd210b186a
Reid Spencer <rspencer@reidspencer.com>
2007-07-17 05:09:37 +0000

    Changes to get the support module building from llvm-top
    llvm-svn: 39961

--- support/build-for-llvm-top.sh
+++ support/build-for-llvm-top.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-# This includes the Bourne shell library from llvm-top. Since this file is
-# generally only used when building from llvm-top, it is safe to assume that
-# llvm is checked out into llvm-top in which case .. just works.
-. ../library.sh
-
 is_debug=1
 for arg in "$@" ; do
   case "$arg" in
@@ -14,14 +9,25 @@ for arg in "$@" ; do
     PREFIX=*)
       PREFIX=`echo "$arg" | sed -e 's/PREFIX=//'`
       ;;
+    MODULE=*)
+      MODULE=`echo "$arg" | sed -e 's/MODULE=//'`
+      ;;
     *=*)
       build_opts="$build_opts $arg"
       ;;
     --*)
       config_opts="$config_opts $arg"
       ;;
+    *)
+      die 1 "Unrecognized option: $arg"
+      ;;
   esac
 done
+
+# This includes the Bourne shell library from llvm-top. Since this file is
+# generally only used when building from llvm-top, it is safe to assume that
+# llvm is checked out into llvm-top in which case .. just works.
+. $LLVM_TOP/library.sh
 
 # See if we have previously been configured by sensing the presense
 # of the config.status scripts
@@ -34,6 +40,6 @@ if test ! -x "config.status" ; then
   ./configure $config_options || (echo "Can't configure llvm" ; exit 1)
 fi
 
-msg 0 Building $module with:
-msg 0 "  make" $build_opts tools-only
-make $build_opts tools-only
+msg 0 Building $MODULE with:
+msg 0 "  make" $build_opts
+make $build_opts

4b3e2b25fc94790fb513f5c6f2f77b24d317b0b5
Duncan Sands <baldrick@free.fr>
2007-07-19 12:56:39 +0000

    It seems that you can take a COMPONENT_REF of a transparent union field in gcc-4.2, so set field indices in the transparent case.  I don't know about gcc-4.0.
    llvm-svn: 40053

--- llvm-gcc-4.2/gcc/llvm-types.cpp
+++ llvm-gcc-4.2/gcc/llvm-types.cpp
@@ -1769,12 +1769,21 @@ const Type *TypeConverter::ConvertUNION(tree type, tree orig_type) {
   if (TYPE_TRANSPARENT_UNION(type)) {
     tree Field = TYPE_FIELDS(type);
     assert(Field && "Transparent union must have some elements!");
-    while (TREE_CODE(Field) != FIELD_DECL) {
+    tree FirstFieldDecl = NULL_TREE;
+    while (Field) {
+      if (TREE_CODE(Field) == FIELD_DECL) {
+        if (FirstFieldDecl == NULL_TREE)
+          FirstFieldDecl = Field;
+        // Set the field idx to zero for all fields.
+        SetFieldIndex(Field, 0);
+      }
+
       Field = TREE_CHAIN(Field);
-      assert(Field && "Transparent union must have some elements!");
     }
-    
-    return TypeDB.setType(type, ConvertType(TREE_TYPE(Field)));
+    assert(FirstFieldDecl != NULL_TREE
+           && "Transparent union must have some elements!");
+
+    return TypeDB.setType(type, ConvertType(TREE_TYPE(FirstFieldDecl)));
   }
   
   // Note that we are compiling a struct now.

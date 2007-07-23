157bfd5869dfc2a7843f42f9baceef9fbbe85c65
Duncan Sands <baldrick@free.fr>
2007-07-23 09:26:28 +0000

    We don't want gcc to lower vectors.  Also, the vector lowering seems to create invalid BIT_FIELD_REFs. This fixes 2006-03-16-VectorCtor.c.
    llvm-svn: 40431

--- llvm-gcc-4.2/gcc/passes.c
+++ llvm-gcc-4.2/gcc/passes.c
@@ -496,7 +496,8 @@ init_optimization_passes (void)
   NEXT_PASS (pass_lower_eh);
   NEXT_PASS (pass_build_cfg);
   NEXT_PASS (pass_lower_complex_O0);
-  NEXT_PASS (pass_lower_vector);
+  /* LLVM LOCAL */
+  /* NEXT_PASS (pass_lower_vector); */
   NEXT_PASS (pass_warn_function_return);
   NEXT_PASS (pass_early_tree_profile);
   *p = NULL;

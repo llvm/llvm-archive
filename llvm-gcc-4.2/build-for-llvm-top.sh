d4b51866cb43cbae2b12a80673924b76a92a6a4b
Reid Spencer <rspencer@reidspencer.com>
2007-07-28 19:44:03 +0000

    Make the build directory be build.llvm-gcc-4.2 instead of build.llvm-gcc-4.0 so it doesn't collide with the llvm-gcc-4.0 build.
    llvm-svn: 40573

--- llvm-gcc-4.2/build-for-llvm-top.sh
+++ llvm-gcc-4.2/build-for-llvm-top.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#                      build script for llvm-gcc-4.0   
+#                      build script for llvm-gcc-4.2
 # 
 # This file was developed by Reid Spencer and is distributed under the
 # University of Illinois Open Source License. See LICENSE.TXT for details.
@@ -14,7 +14,7 @@
 process_arguments "$@"
 
 # First, see if the build directory is there. If not, create it.
-build_dir="$LLVM_TOP/build.llvm-gcc-4.0"
+build_dir="$LLVM_TOP/build.llvm-gcc-4.2"
 if test ! -d "$build_dir" ; then
   mkdir -p "$build_dir"
 fi
@@ -27,8 +27,10 @@ if test ! -d "$config_status" -o "$config_status" -ot "$0" ; then
   config_options="--prefix=$PREFIX --enable-llvm=$LLVM_TOP/llvm "
   config_options="$config_options --program-prefix=llvm-"
   config_options="$config_options --enable-languages=c,c++"
+  config_options="$config_options --disable-bootstrap"
+  config_options="$config_options --with-gxx-include-dir=/proj/install/include/c++/4.2.1"
   if test "$OPTIMIZED" -eq 0 ; then
-    config_options="$config_options --enable-checking --enable-libstdcxx-debug"
+    config_options="$config_options --enable-checking"
   fi
   host=`./config.guess`
   if test ! -z `echo "$host" | grep 'linux'` ; then

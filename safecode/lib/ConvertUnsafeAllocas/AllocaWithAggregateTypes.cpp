149c0dcf5c6eef76ae21cc2179ca1b68c0222b08
John Criswell <criswell@uiuc.edu>
2007-07-31 19:57:22 +0000

    Added support for initializing allocas in kernel code. Added code to insert memset() after all allocas in the function's entry block. Added support for allocas that allocate arrays. Fixed some formatting.
    llvm-svn: 87502

--- safecode/lib/ConvertUnsafeAllocas/AllocaWithAggregateTypes.cpp
+++ safecode/lib/ConvertUnsafeAllocas/AllocaWithAggregateTypes.cpp
@@ -1,18 +1,28 @@
+#include "ConvertUnsafeAllocas.h"
+#include "SCUtils.h"
+
 #include "llvm/Pass.h"
 #include "llvm/BasicBlock.h"
 #include "llvm/Type.h"
-#include "ConvertUnsafeAllocas.h"
 #include "llvm/Function.h"
 #include "llvm/DerivedTypes.h"
 #include "llvm/Constants.h"
 #include "llvm/Instructions.h"
 #include "llvm/Module.h"
+#include "llvm/ADT/Statistic.h"
 #include "llvm/ADT/VectorExtras.h"
 
 #include <iostream>
 
 using namespace llvm;
 
+static Statistic<> InitAllocas ("convallocai", "Allocas Initialized");
+
+#ifdef LLVA_KERNEL
+static const unsigned meminitvalue = 0x00;
+#else
+static const unsigned meminitvalue = 0xcc;
+#endif
 
   // Create the command line option for the pass
   //  RegisterOpt<MallocPass> X ("malloc", "Alloca to Malloc Pass");
@@ -60,58 +70,77 @@ using namespace llvm;
   MallocPass::runOnFunction (Function &F)
   {
     bool modified = false;
+    Type * VoidPtrType = PointerType::get(Type::SByteTy);
+
     TargetData &TD = getAnalysis<TargetData>();
     Module *theM = F.getParent();  
     Function *memsetF = 
       theM->getOrInsertFunction("memset", Type::VoidTy, 
 				PointerType::get(Type::SByteTy), Type::IntTy , 
 				Type::UIntTy, NULL);
-    for (Function::iterator I = F.begin(), E = F.end(); I != E; ++I)
-      {
-	for (BasicBlock::iterator IAddrBegin=I->begin(), IAddrEnd = I->end();
-	     IAddrBegin != IAddrEnd;
-	     ++IAddrBegin)
-	  {
-	    //
-	    // Determine if the instruction needs to be changed.
-	    //
-	    if (changeType (IAddrBegin))
-	      {
-		AllocationInst * AllocInst = cast<AllocationInst>(IAddrBegin);
-		/*
-		//
-		// Get the type of object allocated.
-		//
-		const Type * TypeCreated = AllocInst->getAllocatedType ();
-		
-		MallocInst * TheMalloc = 
-		new MallocInst(TypeCreated, 0, AllocInst->getName(), 
-		IAddrBegin);
-		std::cerr << "Found alloca that is struct or array" << std::endl;
+    for (Function::iterator I = F.begin(), E = F.end(); I != E; ++I) {
+      for (BasicBlock::iterator IAddrBegin=I->begin(), IAddrEnd = I->end();
+           IAddrBegin != IAddrEnd;
+           ++IAddrBegin) {
+        //
+        // Determine if the instruction needs to be changed.
+        //
+        if (changeType (IAddrBegin)) {
+          AllocationInst * AllocInst = cast<AllocationInst>(IAddrBegin);
+#if 0
+          //
+          // Get the type of object allocated.
+          //
+          const Type * TypeCreated = AllocInst->getAllocatedType ();
 
-		//
-		// Remove old uses of the old instructions.
-		//
-		AllocInst->replaceAllUsesWith (TheMalloc);
+          MallocInst * TheMalloc = 
+          new MallocInst(TypeCreated, 0, AllocInst->getName(), 
+          IAddrBegin);
+          std::cerr << "Found alloca that is struct or array" << std::endl;
 
-		//
-		// Remove the old instruction.
-		//
-		AllocInst->getParent()->getInstList().erase(AllocInst);
-		modified = true;
-		*/
+          //
+          // Remove old uses of the old instructions.
+          //
+          AllocInst->replaceAllUsesWith (TheMalloc);
 
-		CastInst *CastI = 
-		  new CastInst(AllocInst, 
-			       PointerType::get(Type::SByteTy), "casted", AllocInst->getNext());
-		std::vector<Value *> args(1, CastI);
-		args.push_back(ConstantInt::get(Type::IntTy,204));
-		args.push_back(ConstantInt::get(Type::UIntTy,
-						 TD.getTypeSize(AllocInst->getType())));
-		  new CallInst(memsetF, args, "", CastI->getNext());
-	      }
-	  }
+          //
+          // Remove the old instruction.
+          //
+          AllocInst->getParent()->getInstList().erase(AllocInst);
+          modified = true;
+#endif
+          // Insert object registration at the end of allocas.
+          Instruction *iptI = AllocInst->getNext();
+          if (AllocInst->getParent() == (&(AllocInst->getParent()->getParent()->getEntryBlock()))) {
+            BasicBlock::iterator InsertPt = AllocInst->getParent()->begin();
+            while (&(*(InsertPt)) != AllocInst)
+              ++InsertPt;
+            while (isa<AllocaInst>(InsertPt))
+              ++InsertPt;
+            iptI = InsertPt;
+          }
+
+          // Create a value that calculates the alloca's size
+          const Type * AllocaType = AllocInst->getAllocatedType();
+          Value *AllocSize = ConstantInt::get (Type::UIntTy,
+                                               TD.getTypeSize(AllocaType));
+
+          if (AllocInst->isArrayAllocation())
+            AllocSize = BinaryOperator::create(Instruction::Mul, AllocSize,
+                                               AllocInst->getOperand(0),
+                                               "sizetmp",
+                                               AllocInst);
+
+          Value * TheAlloca = castTo (AllocInst, VoidPtrType, iptI);
+
+          std::vector<Value *> args(1, TheAlloca);
+          args.push_back (ConstantInt::get(Type::IntTy, meminitvalue));
+          args.push_back (AllocSize);
+          new CallInst(memsetF, args, "", iptI);
+          ++InitAllocas;
+        }
       }
+    }
     return modified;
   }
 

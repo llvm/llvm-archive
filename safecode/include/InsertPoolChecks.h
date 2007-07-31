b5c63c253d20c83728f98765ad569e5e0f18d6a9
John Criswell <criswell@uiuc.edu>
2007-07-31 18:35:01 +0000

    Formatting changes; no functionality changes.
    llvm-svn: 87500

--- safecode/include/InsertPoolChecks.h
+++ safecode/include/InsertPoolChecks.h
@@ -93,7 +93,7 @@ struct PreInsertPoolChecks : public ModulePass {
 };
 
 struct InsertPoolChecks : public FunctionPass {
-    public :
+  public :
     const char *getPassName() const { return "Inserting pool checks for array bounds "; }
     virtual bool doInitialization (Module &M);
     virtual bool doFinalization   (Module &M);
@@ -102,8 +102,10 @@ struct InsertPoolChecks : public FunctionPass {
       AU.addRequired<PreInsertPoolChecks>();
       AU.addRequired<ConvertUnsafeAllocas>();
       AU.addRequired<ScalarEvolution>();
-//      AU.addRequired<CompleteBUDataStructures>();
-//      AU.addRequired<TDDataStructures>();
+#if 0
+      AU.addRequired<CompleteBUDataStructures>();
+      AU.addRequired<TDDataStructures>();
+#endif
 #ifndef LLVA_KERNEL      
       AU.addRequired<EquivClassGraphs>();
       AU.addRequired<PoolAllocate>();
@@ -113,95 +115,94 @@ struct InsertPoolChecks : public FunctionPass {
       AU.addRequired<TDDataStructures>();
       AU.addRequired<TargetData>();
 #endif
-      
     };
-    private :
+
+  private :
     PreInsertPoolChecks * preSCPass;
     CUA::ConvertUnsafeAllocas * cuaPass;
     ScalarEvolution * scevPass;
-  TargetData * TD;
+    TargetData * TD;
 #ifndef  LLVA_KERNEL
-  PoolAllocate * paPass;
-  EquivClassGraphs *equivPass;
-  EmbeCFreeRemoval *efPass;
+    PoolAllocate * paPass;
+    EquivClassGraphs *equivPass;
+    EmbeCFreeRemoval *efPass;
 #else
-  TDDataStructures * TDPass;
+    TDDataStructures * TDPass;
 #endif  
-  Function *PoolCheck;
-  Function *PoolCheckArray;
-  Function *PoolCheckIArray;
-  Function *ExactCheck;
-  Function *FunctionCheck;
-  Function *FunctionCheckT;
-  Function *FunctionCheckG;
-  Function *ICCheck;
-  Function *BoundsCheck;
-  Function *UIBoundsCheck;
-  Function *getBounds;
-  Function *UIgetBounds;
-  Function *ExactCheck2;
-  Function *ExactCheck3;
-  Function *GetActualValue;
-  Function *PoolRegister;
-  Function *StackRegister;
-  Function *ObjFree;
-  Function *StackFree;
-  Function *FuncRegister;
-  Function *PoolRegMP;
-  Function *PoolFindMP;
-  Function *getBegin;
-  Function *getEnd;
+    Function *PoolCheck;
+    Function *PoolCheckArray;
+    Function *PoolCheckIArray;
+    Function *ExactCheck;
+    Function *FunctionCheck;
+    Function *FunctionCheckT;
+    Function *FunctionCheckG;
+    Function *ICCheck;
+    Function *BoundsCheck;
+    Function *UIBoundsCheck;
+    Function *getBounds;
+    Function *UIgetBounds;
+    Function *ExactCheck2;
+    Function *ExactCheck3;
+    Function *GetActualValue;
+    Function *PoolRegister;
+    Function *StackRegister;
+    Function *ObjFree;
+    Function *StackFree;
+    Function *FuncRegister;
+    Function *PoolRegMP;
+    Function *PoolFindMP;
+    Function *getBegin;
+    Function *getEnd;
 
-  // Map of DSNode to Function List Global Variables
-  std::map<Value *, GlobalVariable *> FuncListMap;
+    // Map of DSNode to Function List Global Variables
+    std::map<Value *, GlobalVariable *> FuncListMap;
 
-  // Set of DSNodes for which we do full bounds checks
-  std::set<DSNode *> PHNeeded;
+    // Set of DSNodes for which we do full bounds checks
+    std::set<DSNode *> PHNeeded;
 
-  void simplifyGEPList();
-  void addObjFrees(Module& M);
-  void addMetaPools(Module& M, MetaPool* MP, DSNode* N);
-  void addPoolCheckProto(Module &M);
-  void addPoolChecks(Module &M);
-  void addGetElementPtrChecks(Module &M);
-  DSNode* getDSNode(const Value *V, Function *F);
-  unsigned getDSNodeOffset(const Value *V, Function *F);
-  void addLoadStoreChecks (Function & F);
-  void TransformFunction(Function &F);
-  void handleCallInst(CallInst *CI);
-  void handleGetElementPtr(GetElementPtrInst *MAI);
-  void addGetActualValue(SetCondInst *SCI, unsigned operand);
-  void registerAllocaInst(AllocaInst *AI, AllocaInst *AIOrig);
-  GlobalVariable * getOrCreateFunctionTable (DSNode * Node, Value * PH, Module * M);
-  void addExactCheck  (Instruction * GEP, Value * Index, Value * Bound);
-  void addExactCheck  (Value * Pointer, Value * Index, Value * Bound, Instruction * InsertPt);
-  void addExactCheck2 (Value * Base, Value * Result, Value * Bound, Instruction * InsertPt);
-  Value * addExactCheck3 (Value * Source, Value * Result, Value * Bound, Instruction * Next);
-  bool insertExactCheck (GetElementPtrInst * GEP);
-  bool insertExactCheck (Instruction * , Value *, Value *, Instruction *);
-  void insertFunctionCheck(CallInst* CI);
-  void insertICCheck (Value * Pointer, Instruction * InsertPt);
-  Value * insertBoundsCheck (Instruction * , Value *, Value *, Instruction *);
-  bool AggregateGEPs (GetElementPtrInst * GEP, std::set<Instruction *> & GEPs);
-  bool findCheckedPointer (Value * PointerOperand);
-  void addHeapRegs (Module & M);
+    void simplifyGEPList();
+    void addObjFrees(Module& M);
+    void addMetaPools(Module& M, MetaPool* MP, DSNode* N);
+    void addPoolCheckProto(Module &M);
+    void addPoolChecks(Module &M);
+    void addGetElementPtrChecks(Module &M);
+    DSNode* getDSNode(const Value *V, Function *F);
+    unsigned getDSNodeOffset(const Value *V, Function *F);
+    void addLoadStoreChecks (Function & F);
+    void TransformFunction(Function &F);
+    void handleCallInst(CallInst *CI);
+    void handleGetElementPtr(GetElementPtrInst *MAI);
+    void addGetActualValue(SetCondInst *SCI, unsigned operand);
+    void registerAllocaInst(AllocaInst *AI, AllocaInst *AIOrig);
+    GlobalVariable * getOrCreateFunctionTable (DSNode * Node, Value * PH, Module * M);
+    void addExactCheck  (Instruction * GEP, Value * Index, Value * Bound);
+    void addExactCheck  (Value * Pointer, Value * Index, Value * Bound, Instruction * InsertPt);
+    void addExactCheck2 (Value * Base, Value * Result, Value * Bound, Instruction * InsertPt);
+    Value * addExactCheck3 (Value * Source, Value * Result, Value * Bound, Instruction * Next);
+    bool insertExactCheck (GetElementPtrInst * GEP);
+    bool insertExactCheck (Instruction * , Value *, Value *, Instruction *);
+    void insertFunctionCheck(CallInst* CI);
+    void insertICCheck (Value * Pointer, Instruction * InsertPt);
+    Value * insertBoundsCheck (Instruction * , Value *, Value *, Instruction *);
+    bool AggregateGEPs (GetElementPtrInst * GEP, std::set<Instruction *> & GEPs);
+    bool findCheckedPointer (Value * PointerOperand);
+    void addHeapRegs (Module & M);
 
 #ifndef LLVA_KERNEL  
-  void addLSChecks(Value *Vnew, const Value *V, Instruction *I, Function *F);
-  Value * getPoolHandle(const Value *V, Function *F, PA::FuncInfo &FI, bool collapsed = false);
+    void addLSChecks(Value *Vnew, const Value *V, Instruction *I, Function *F);
+    Value * getPoolHandle(const Value *V, Function *F, PA::FuncInfo &FI, bool collapsed = false);
 #else
-  Value* getPD(DSNode* N, Module& M) { 
-    if (!N) return 0;
-    addMetaPools(M, N->getMP(), N);
-    if (N->getMP())
-      return N->getMP()->getMetaPoolValue();
-    else
-      return 0;
-  }
-  void addLSChecks(Value *V, Instruction *I, Function *F);
-  Value * getPoolHandle(const Value *V, Function *F);
+    Value* getPD(DSNode* N, Module& M) { 
+      if (!N) return 0;
+      addMetaPools(M, N->getMP(), N);
+      if (N->getMP())
+        return N->getMP()->getMetaPoolValue();
+      else
+        return 0;
+    }
+    void addLSChecks(Value *V, Instruction *I, Function *F);
+    Value * getPoolHandle(const Value *V, Function *F);
 #endif  
-
 };
 }
 #endif

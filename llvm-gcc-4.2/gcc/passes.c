ff3a2344f50a11a3f5fb99d425829377b469fd16
Duncan Sands <baldrick@free.fr>
2007-08-20 17:02:01 +0000

    The gcc vector lowering pass should only be turned off if LLVM is enabled.
    llvm-svn: 41182

--- llvm-gcc-4.2/gcc/passes.c
+++ llvm-gcc-4.2/gcc/passes.c
@@ -496,8 +496,11 @@ init_optimization_passes (void)
   NEXT_PASS (pass_lower_eh);
   NEXT_PASS (pass_build_cfg);
   NEXT_PASS (pass_lower_complex_O0);
-  /* LLVM LOCAL */
-  /* NEXT_PASS (pass_lower_vector); */
+  /* LLVM LOCAL begin */
+#ifndef ENABLE_LLVM
+  NEXT_PASS (pass_lower_vector);
+#endif
+  /* LLVM LOCAL end */
   NEXT_PASS (pass_warn_function_return);
   NEXT_PASS (pass_early_tree_profile);
   *p = NULL;

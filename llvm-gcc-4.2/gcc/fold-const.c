2fad1ed16f3d0f31aef5eae57fb884c59abedf30
Duncan Sands <baldrick@free.fr>
2007-08-22 14:40:59 +0000

    Make sure that the overflow flag is not lost.
    llvm-svn: 41271

--- llvm-gcc-4.2/gcc/fold-const.c
+++ llvm-gcc-4.2/gcc/fold-const.c
@@ -1890,12 +1890,21 @@ size_binop (enum tree_code code, tree arg0, tree arg1)
   if (TREE_CODE (arg0) == INTEGER_CST && TREE_CODE (arg1) == INTEGER_CST)
     {
       /* And some specific cases even faster than that.  */
-      if (code == PLUS_EXPR && integer_zerop (arg0))
+/* LLVM local begin */
+      if (code == PLUS_EXPR && integer_zerop (arg0)
+	  && !TREE_OVERFLOW (arg0))
+/* LLVM local end */
 	return arg1;
       else if ((code == MINUS_EXPR || code == PLUS_EXPR)
-	       && integer_zerop (arg1))
+/* LLVM local begin */
+	       && integer_zerop (arg1)
+	       && !TREE_OVERFLOW (arg1))
+/* LLVM local end */
 	return arg0;
-      else if (code == MULT_EXPR && integer_onep (arg0))
+/* LLVM local begin */
+      else if (code == MULT_EXPR && integer_onep (arg0)
+	       && !TREE_OVERFLOW (arg0))
+/* LLVM local end */
 	return arg1;
 
       /* Handle general case of two integer constants.  */

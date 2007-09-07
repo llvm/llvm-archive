def5b1b7162ce8ded87689956ae98b092d36d6d6
Devang Patel <dpatel@apple.com>
2007-09-07 21:00:20 +0000

    Use  SmallSetVector insetad of SmallPtrSet to ensure determinstic order.
    llvm-svn: 41773

--- llvm-gcc-4.2/gcc/llvm-backend.cpp
+++ llvm-gcc-4.2/gcc/llvm-backend.cpp
@@ -85,7 +85,7 @@ llvm::OStream *AsmOutFile = 0;
 static cl::opt<bool> DisableLLVMOptimizations("disable-llvm-optzns");
 
 std::vector<std::pair<Function*, int> > StaticCtors, StaticDtors;
-SmallPtrSet<Constant*, 32> AttributeUsedGlobals;
+SmallSetVector<Constant*, 32> AttributeUsedGlobals;
 std::vector<Constant*> AttributeNoinlineFunctions;
 std::vector<Constant*> AttributeAnnotateGlobals;
 
@@ -483,7 +483,7 @@ void llvm_asm_file_end(void) {
   if (!AttributeUsedGlobals.empty()) {
     std::vector<Constant *> AUGs;
     const Type *SBP= PointerType::get(Type::Int8Ty);
-    for (SmallPtrSet<Constant *,32>::iterator AI = AttributeUsedGlobals.begin(),
+    for (SmallSetVector<Constant *,32>::iterator AI = AttributeUsedGlobals.begin(),
            AE = AttributeUsedGlobals.end(); AI != AE; ++AI) {
       Constant *C = *AI;
       AUGs.push_back(ConstantExpr::getBitCast(C, SBP));
@@ -815,7 +815,7 @@ void emit_global_to_llvm(tree decl) {
                                              GV->getName(), TheModule);
     GV->replaceAllUsesWith(ConstantExpr::getBitCast(NGV, GV->getType()));
     if (AttributeUsedGlobals.count(GV)) {
-      AttributeUsedGlobals.erase(GV);
+      AttributeUsedGlobals.remove(GV);
       AttributeUsedGlobals.insert(NGV);
     }
     delete GV;

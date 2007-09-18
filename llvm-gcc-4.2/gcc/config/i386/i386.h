c5144e1a50148ca8d93171237c3bed8c2fb1c8d9
Dale Johannesen <dalej@apple.com>
2007-09-18 18:07:38 +0000

    Make long double 80 bits on x86. This changes the ABI.
    llvm-svn: 42100

--- llvm-gcc-4.2/gcc/config/i386/i386.h
+++ llvm-gcc-4.2/gcc/config/i386/i386.h
@@ -565,14 +565,6 @@ extern const char *host_detect_local_cpu (int argc, const char **argv);
 
 /* target machine storage layout */
 
-/* LLVM LOCAL begin */
-#if defined(ENABLE_LLVM) || defined(__llvm__)
-#define LONG_DOUBLE_TYPE_SIZE 64
-#else
-#define LONG_DOUBLE_TYPE_SIZE 80
-#endif
-/* LLVM LOCAL end */
-
 /* Set the value of FLT_EVAL_METHOD in float.h.  When using only the
    FPU, assume that the fpcw is set to extended precision; when using
    only SSE, rounding is correct; when using both SSE and the FPU,

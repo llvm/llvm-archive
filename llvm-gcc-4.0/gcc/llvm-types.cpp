5bed5cf9b02dd1a19c3647d54dcfda866cd6b925
Duncan Sands <baldrick@free.fr>
2007-11-01 20:57:38 +0000

    Change most uses of getTypeSize to getTypeStoreSize or getABITypeSize, hopefully correctly.
    llvm-svn: 43622

--- llvm-gcc-4.0/gcc/llvm-types.cpp
+++ llvm-gcc-4.0/gcc/llvm-types.cpp
@@ -1149,7 +1149,7 @@ struct StructTypeConversionInfo {
   /// getTypeSize - Return the size of the specified type in bytes.
   ///
   unsigned getTypeSize(const Type *Ty) const {
-    return TD.getTypeSize(Ty);
+    return Packed ? TD.getTypeStoreSize(Ty) : TD.getABITypeSize(Ty);
   }
   
   /// getLLVMType - Return the LLVM type for the specified object.
@@ -1849,7 +1849,7 @@ const Type *TypeConverter::ConvertUNION(tree type, tree orig_type) {
     
     const Type *TheTy = ConvertType(TREE_TYPE(Field));
     bool isPacked = false;
-    unsigned Size     = TD.getTypeSize(TheTy);
+    unsigned Size  = TD.getABITypeSize(TheTy);
     unsigned Align = TD.getABITypeAlignment(TheTy);
     if (const StructType *STy = dyn_cast<StructType>(TheTy)) 
       if (STy->isPacked())
@@ -1882,7 +1882,7 @@ const Type *TypeConverter::ConvertUNION(tree type, tree orig_type) {
   std::vector<const Type*> UnionElts;
   unsigned UnionSize = 0;
   if (UnionTy) {            // Not an empty union.
-    UnionSize = TD.getTypeSize(UnionTy);
+    UnionSize = TD.getABITypeSize(UnionTy);
     UnionElts.push_back(UnionTy);
   }
   

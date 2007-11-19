064819dd6d67a549c80b395efe002568546a1a22
John Criswell <criswell@uiuc.edu>
2007-11-19 21:41:30 +0000

    Make the kernel callback functions use weak linkage.  This allows the kernel to supply its own version of these functions but user-space applications to use those defined in UserPoolSystem.cpp.
    llvm-svn: 87548

--- safecode/runtime/KPoolCheck/PoolSystem.h
+++ safecode/runtime/KPoolCheck/PoolSystem.h
@@ -18,12 +18,12 @@
 extern "C" {
 #endif
   /* Functions that need to be provided by the pool allocation run-time */
-  void poolcheckfail (const char * msg, int, void*);
-  void poolcheckfatal (const char * msg, int);
-  void poolcheckinfo (const char * msg, int);
-  void poolcheckinfo2 (const char * msg, int, int);
-  void * poolcheckmalloc (unsigned int size);
-  void printpoolinfo (void *Pool);
+  void poolcheckfail (const char * msg, int, void*) __attribute__ ((weak));
+  void poolcheckfatal (const char * msg, int) __attribute__ ((weak));
+  void poolcheckinfo (const char * msg, int) __attribute__ ((weak));
+  void poolcheckinfo2 (const char * msg, int, int) __attribute__ ((weak));
+  void * poolcheckmalloc (unsigned int size) __attribute__ ((weak));
+  void printpoolinfo (void *Pool) __attribute__ ((weak));
   void poolcheckglobals ();
 #ifdef __cplusplus
 }

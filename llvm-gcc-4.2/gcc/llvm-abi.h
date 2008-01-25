81ef41601eda95514a4abc98049c0775cc7e7899
Evan Cheng <evan.cheng@apple.com>
2008-01-25 23:02:28 +0000

    Drop zero-sized aggregate arguments.
    llvm-svn: 46375

--- llvm-gcc-4.2/gcc/llvm-abi.h
+++ llvm-gcc-4.2/gcc/llvm-abi.h
@@ -120,6 +120,13 @@ static tree isSingleElementStructOrArray(tree type) {
   }
 }
 
+/// isAggregareOfSizeZero - Returns true if this is an aggregate with size zero.
+///
+static bool isAggregareOfSizeZero(tree type) {
+  if (!isAggregateTreeType(type)) return false;
+  return int_size_in_bytes(type) == 0;
+}
+
 // LLVM_SHOULD_PASS_AGGREGATE_USING_BYVAL_ATTR - Return true if this aggregate
 // value should be passed by value, i.e. passing its address with the byval
 // attribute bit set. The default is false.
@@ -227,6 +234,9 @@ public:
         PassInMixedRegisters(type, Ty, Elts);
       } else if (LLVM_SHOULD_PASS_AGGREGATE_IN_INTEGER_REGS(type)) {
         PassInIntegerRegisters(type, Ty);
+      } else if (isAggregareOfSizeZero(type)) {
+        // Zero sized aggregare, just drop it!
+        ;
       } else if (TREE_CODE(type) == RECORD_TYPE) {
         for (tree Field = TYPE_FIELDS(type); Field; Field = TREE_CHAIN(Field))
           if (TREE_CODE(Field) == FIELD_DECL) {

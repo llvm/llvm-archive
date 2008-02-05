c4746037b470a67082f0d922124d7066bd74f761
Dale Johannesen <dalej@apple.com>
2008-02-05 22:22:23 +0000

    Fix per review feedback.
    llvm-svn: 46773

--- llvm-gcc-4.2/gcc/config/i386/llvm-i386-target.h
+++ llvm-gcc-4.2/gcc/config/i386/llvm-i386-target.h
@@ -71,7 +71,7 @@ extern int ix86_regparm;
           else                                                  \
             local_fp_regparm = 0;                               \
       } else if (TREE_CODE(Type) == INTEGER_TYPE ||             \
-                 TREE_CODE(Type) == ENUMERAL_TYPE) {            \
+                 TREE_CODE(Type) == POINTER_TYPE) {             \
           int words =                                           \
                   (Size + BITS_PER_WORD - 1) / BITS_PER_WORD;   \
           local_regparm -= words;                               \

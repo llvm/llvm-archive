4eb7e5f4028e98e9827f49ab9953ba550506e1c1
Dale Johannesen <dalej@apple.com>
2008-02-13 19:30:03 +0000

    Whitespace change to have a place to put this comment, which belonged on 47077: Fixes t001 and t029 in struct-layout-1 in g++ testsuite.
    llvm-svn: 47081

--- llvm-gcc-4.2/gcc/llvm-convert.cpp
+++ llvm-gcc-4.2/gcc/llvm-convert.cpp
@@ -1,4 +1,4 @@
-/* LLVM LOCAL begin (ENTIRE FILE!)  */
+/* LLVM LOCAL begin (ENTIRE FILE!) */
 /* High-level LLVM backend interface 
 Copyright (C) 2005, 2006, 2007 Free Software Foundation, Inc.
 Contributed by Chris Lattner (sabre@nondot.org)

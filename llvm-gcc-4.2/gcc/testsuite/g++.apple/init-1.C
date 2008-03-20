751d3bd0a76c2e5c546d0392a50b193fc0210a63
Bill Wendling <isanbard@gmail.com>
2008-03-20 21:26:03 +0000

    Merge error. Tests included redundantly.
    llvm-svn: 48611

--- llvm-gcc-4.2/gcc/testsuite/g++.apple/init-1.C
+++ llvm-gcc-4.2/gcc/testsuite/g++.apple/init-1.C
@@ -7,12 +7,3 @@ struct Foo {
   ~Foo() { }
 };
 Foo foo;
-/* APPLE LOCAL file elide global inits 5642351 */
-/* { dg-do compile } */
-/* { dg-options "-fno-use-cxa-atexit" } */
-/* { dg-final { scan-assembler-not "_GLOBAL__I_" } } */
-
-struct Foo {
-  ~Foo() { }
-};
-Foo foo;
--- llvm-gcc-4.2/gcc/testsuite/g++.apple/init-2.C
+++ llvm-gcc-4.2/gcc/testsuite/g++.apple/init-2.C
@@ -5,10 +5,3 @@
 typedef float vFloat __attribute__ ((__vector_size__ (16)));
 static const vFloat _minusZero = (const vFloat) { -0.0f, -0.0f, -0.0f, -0.0f };
 const vFloat *p = &_minusZero;
-/* APPLE LOCAL file AltiVec 5527030 */
-/* { dg-do compile } */
-/* { dg-final { scan-assembler-not "_GLOBAL__I_" } } */
-
-typedef float vFloat __attribute__ ((__vector_size__ (16)));
-static const vFloat _minusZero = (const vFloat) { -0.0f, -0.0f, -0.0f, -0.0f };
-const vFloat *p = &_minusZero;

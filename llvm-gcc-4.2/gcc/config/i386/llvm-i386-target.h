5c3fbd936ea53256830f95aa3156033feb8f6843
Evan Cheng <evan.cheng@apple.com>
2008-04-30 19:18:07 +0000

    x86-64 abi specifies small aggregates can be broken up and passed in registers. However, this is only true when there are enough argument registers to pass the whole aggregate in registers. Keep track of the number of scalar parameter registers used so far. If there are only enough registers to "partially" satisfy the requirement, pass the aggregate byval in memory instead. Note, if there are no argument registers left at all, then treat it as if it's being passed in registers and left codegen do the right thing.
    llvm-svn: 50491

--- llvm-gcc-4.2/gcc/config/i386/llvm-i386-target.h
+++ llvm-gcc-4.2/gcc/config/i386/llvm-i386-target.h
@@ -164,10 +164,20 @@ extern bool
 llvm_x86_32_should_pass_aggregate_in_mixed_regs(tree, const Type *Ty,
                                                 std::vector<const Type*>&);
 
-#define LLVM_SHOULD_PASS_AGGREGATE_IN_MIXED_REGS(T, TY, E)            \
-  (TARGET_64BIT ?                                                     \
-   llvm_x86_64_should_pass_aggregate_in_mixed_regs((T), (TY), (E)) :  \
+#define LLVM_SHOULD_PASS_AGGREGATE_IN_MIXED_REGS(T, TY, E)           \
+  (TARGET_64BIT ?                                                    \
+   llvm_x86_64_should_pass_aggregate_in_mixed_regs((T), (TY), (E)) : \
    llvm_x86_32_should_pass_aggregate_in_mixed_regs((T), (TY), (E)))
+
+extern
+bool llvm_x86_64_aggregate_partially_passed_in_regs(std::vector<const Type*>&,
+                                                    std::vector<const Type*>&);
+
+#define LLVM_AGGREGATE_PARTIALLY_PASSED_IN_REGS(E, SE)         \
+  (TARGET_64BIT ?                                              \
+   llvm_x86_64_aggregate_partially_passed_in_regs((E), (SE)) : \
+   false)
+
 #endif /* LLVM_ABI_H */
 
 /* LLVM LOCAL end (ENTIRE FILE!)  */

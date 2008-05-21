fca5cfc34da498c677fcdf73ea4077799e6d02f1
Evan Cheng <evan.cheng@apple.com>
2008-05-21 23:47:48 +0000

    Revert Duncan's ipa related changes for now. This is causing significant performance regressions, e.g. WebKit.
    llvm-svn: 51397

--- llvm-gcc-4.2/gcc/passes.c
+++ llvm-gcc-4.2/gcc/passes.c
@@ -481,20 +481,18 @@ init_optimization_passes (void)
 #define NEXT_PASS(PASS)  (p = next_pass_1 (p, &PASS))
   /* Interprocedural optimization passes.  */
   p = &all_ipa_passes;
-  /* LLVM local begin */
-#ifndef ENABLE_LLVM
   NEXT_PASS (pass_early_ipa_inline);
   NEXT_PASS (pass_early_local_passes);
   NEXT_PASS (pass_ipa_cp);
-#endif
-  NEXT_PASS (pass_ipa_inline); /* LLVM: inline functions marked always_inline */
+  NEXT_PASS (pass_ipa_inline);
+/* LLVM LOCAL begin */
 #ifndef ENABLE_LLVM
   NEXT_PASS (pass_ipa_reference);
   NEXT_PASS (pass_ipa_pure_const); 
   NEXT_PASS (pass_ipa_type_escape);
   NEXT_PASS (pass_ipa_pta);
 #endif
-  /* LLVM local end */
+/* LLVM LOCAL end */
   *p = NULL;
 
   /* All passes needed to lower the function into shape optimizers can
@@ -506,9 +504,9 @@ init_optimization_passes (void)
   NEXT_PASS (pass_lower_cf);
   NEXT_PASS (pass_lower_eh);
   NEXT_PASS (pass_build_cfg);
+  NEXT_PASS (pass_lower_complex_O0);
   /* LLVM LOCAL begin */
 #ifndef ENABLE_LLVM
-  NEXT_PASS (pass_lower_complex_O0);
   NEXT_PASS (pass_lower_vector);
 #endif
   /* LLVM LOCAL end */
@@ -518,6 +516,16 @@ init_optimization_passes (void)
   NEXT_PASS (pass_early_tree_profile);
 #endif
   /* LLVM LOCAL end */
+  *p = NULL;
+
+  p = &pass_early_local_passes.sub;
+  /* LLVM LOCAL begin */
+#ifndef ENABLE_LLVM
+  NEXT_PASS (pass_tree_profile);
+#endif
+  /* LLVM LOCAL end */
+  NEXT_PASS (pass_cleanup_cfg);
+  NEXT_PASS (pass_rebuild_cgraph_edges);
   *p = NULL;
 
   /* LLVM LOCAL begin */
@@ -530,14 +538,6 @@ init_optimization_passes (void)
   *p = NULL;
 #endif
   /* LLVM LOCAL end */
-
-  /* LLVM LOCAL begin - Do not pull in symbols. */
-#ifndef ENABLE_LLVM
-  p = &pass_early_local_passes.sub;
-  NEXT_PASS (pass_tree_profile);
-  NEXT_PASS (pass_cleanup_cfg);
-  NEXT_PASS (pass_rebuild_cgraph_edges);
-  *p = NULL;
 
   p = &all_passes;
   NEXT_PASS (pass_fixup_cfg);
@@ -684,6 +684,8 @@ init_optimization_passes (void)
   NEXT_PASS (pass_dce_loop);
   *p = NULL;
 
+  /* LLVM LOCAL begin */
+#ifndef ENABLE_LLVM
   p = &pass_loop2.sub;
   NEXT_PASS (pass_rtl_loop_init);
   NEXT_PASS (pass_rtl_move_loop_invariants);
@@ -759,7 +761,7 @@ init_optimization_passes (void)
   NEXT_PASS (pass_final);
   *p = NULL;
 #endif
-  /* LLVM local end */
+  /* LLVM LOCAL end */
 
 #undef NEXT_PASS
 
@@ -773,12 +775,11 @@ init_optimization_passes (void)
   register_dump_files (all_extra_lowering_passes, false,
 		       PROP_gimple_any | PROP_gimple_lcf | PROP_gimple_leh
 		       | PROP_cfg);
-#else
+#endif
+  /* LLVM LOCAL end */
   register_dump_files (all_passes, false,
 		       PROP_gimple_any | PROP_gimple_lcf | PROP_gimple_leh
 		       | PROP_cfg);
-#endif
-  /* LLVM LOCAL end */
 }
 
 static unsigned int last_verified;

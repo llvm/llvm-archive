1a43b9905d299eb79c7a8f904fc8708d6859552f
Duncan Sands <baldrick@free.fr>
2008-05-22 14:31:18 +0000

    Turn off all gcc optimizations again, except for the gcc inliner.  Before the inliner was left on too, but only enabled for always_inline functions.  This time it is completely enabled. My testing shows that the WebKit slowdown (PR2353) was due to disabling the inliner, and not due to turning off the other gcc optimization passes. As for why the gcc inliner helps so much here - well, that still remains to be analysed.
    llvm-svn: 51430

--- llvm-gcc-4.2/gcc/ipa-inline.c
+++ llvm-gcc-4.2/gcc/ipa-inline.c
@@ -941,7 +941,11 @@ cgraph_decide_inlining (void)
 	/* At the moment, no IPA passes change function bodies before inlining.
 	   Save some time by not recomputing function body sizes if early inlining
 	   already did so.  */
+        /* LLVM local begin - Don't rely on pass_early_ipa_inline being run.  */
+#ifndef ENABLE_LLVM
 	if (!flag_early_inlining)
+#endif
+        /* LLVM local end */
 	  node->local.self_insns = node->global.insns
 	     = estimate_num_insns (node->decl);
 
@@ -1028,9 +1032,19 @@ cgraph_decide_inlining (void)
 		 overall_insns - old_insns);
     }
 
+  /* LLVM local begin */
+#ifdef ENABLE_LLVM
+  if (1) /* FIXME: 1 should be 0 some day, see PR2353.  */
+#endif
+  /* LLVM local end*/
   if (!flag_really_no_inline)
     cgraph_decide_inlining_of_small_functions ();
 
+  /* LLVM local begin */
+#ifdef ENABLE_LLVM
+  if (1) /* FIXME: 1 should be 0 some day, see PR2353.  */
+#endif
+  /* LLVM local end*/
   if (!flag_really_no_inline
       && flag_inline_functions_called_once)
     {
@@ -1148,6 +1162,11 @@ cgraph_decide_inlining_incrementally (struct cgraph_node *node, bool early)
       }
 
   /* Now do the automatic inlining.  */
+  /* LLVM local begin */
+#ifdef ENABLE_LLVM
+  if (1) /* FIXME: 1 should be 0 some day, see PR2353.  */
+#endif
+  /* LLVM local end */
   if (!flag_really_no_inline)
     for (e = node->callees; e; e = e->next_callee)
       if (e->callee->local.inlinable

927d882efab3f054fca0a085c6c59af70ce821b0
Bill Wendling <isanbard@gmail.com>
2008-07-03 23:13:57 +0000

    Pass in std::vector by reference instead of returning by value.
    llvm-svn: 53124

--- llvm-gcc-4.2/gcc/llvm-debug.cpp
+++ llvm-gcc-4.2/gcc/llvm-debug.cpp
@@ -880,20 +880,20 @@ void DebugInfo::readLLVMDebugInfo() {
   MachineModuleInfo MMI;
   MMI.AnalyzeModule(*TheModule);
 
-  std::vector<SubprogramDesc *> Subprograms =
-    MMI.getAnchoredDescriptors<SubprogramDesc>(*TheModule);
+  std::vector<SubprogramDesc *> Subprograms;
+  MMI.getAnchoredDescriptors<SubprogramDesc>(*TheModule, Subprograms);
 
   if (!Subprograms.empty())
     SubprogramAnchor = Subprograms[0]->getAnchor();
 
-  std::vector<CompileUnitDesc *> CUs =
-    MMI.getAnchoredDescriptors<CompileUnitDesc>(*TheModule);
+  std::vector<CompileUnitDesc *> CUs;
+  MMI.getAnchoredDescriptors<CompileUnitDesc>(*TheModule, CUs);
 
   if (!CUs.empty())
     CompileUnitAnchor = CUs[0]->getAnchor();
 
-  std::vector<GlobalVariableDesc *> GVs =
-    MMI.getAnchoredDescriptors<GlobalVariableDesc>(*TheModule);
+  std::vector<GlobalVariableDesc *> GVs;
+  MMI.getAnchoredDescriptors<GlobalVariableDesc>(*TheModule, GVs);
 
   if (!GVs.empty())
     GlobalVariableAnchor = GVs[0]->getAnchor();

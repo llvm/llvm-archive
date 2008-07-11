2ffaf454aeda70efdf660b0212e076c31e9b77ef
Chris Lattner <sabre@nondot.org>
2008-07-11 16:50:12 +0000

    Revert Rafael's patch for now, which is GPL3.
    llvm-svn: 53457

--- llvm-gcc-4.2/gcc/cp/decl2.c
+++ llvm-gcc-4.2/gcc/cp/decl2.c
@@ -1879,12 +1879,9 @@ constrain_class_visibility (tree type)
 	int subvis = type_visibility (ftype);
 
 	if (subvis == VISIBILITY_ANON)
-	  {
-	    if (!in_main_input_context ())
-	      warning (0, "\
+	  warning (0, "\
 %qT has a field %qD whose type uses the anonymous namespace",
 		   type, t);
-	  }
 	else if (IS_AGGR_TYPE (ftype)
 		 && vis < VISIBILITY_HIDDEN
 		 && subvis >= VISIBILITY_HIDDEN)
@@ -1899,12 +1896,9 @@ constrain_class_visibility (tree type)
       int subvis = type_visibility (TREE_TYPE (t));
 
       if (subvis == VISIBILITY_ANON)
-        {
-	  if (!in_main_input_context())
-	    warning (0, "\
+	warning (0, "\
 %qT has a base %qT whose type uses the anonymous namespace",
 		 type, TREE_TYPE (t));
-	}
       else if (vis < VISIBILITY_HIDDEN
 	       && subvis >= VISIBILITY_HIDDEN)
 	warning (OPT_Wattributes, "\

d5cde7318166810ae0d5a1bb745dbb4c57632fcf
Bill Wendling <isanbard@gmail.com>
2008-07-14 18:25:25 +0000

    Renaming Tak to Xcode-3.1.
    llvm-svn: 53569

--- llvm-gcc-4.2/gcc/testsuite/gcc.apple/5490617.c
+++
@@ -1,6 +0,0 @@
-/* APPLE LOCAL file mainline candidate */
-/* { dg-do compile } */
-static char _my_endbss[1];
-char *my_endbss_static = _my_endbss;
-/* LLVM LOCAL */
-/* { dg-final { scan-assembler ".lcomm\t__my_endbss" } } */

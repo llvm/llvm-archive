67953d9149060aa9fef71e29f3dac6549fb8420d
Bill Wendling <isanbard@gmail.com>
2008-07-30 04:54:19 +0000

    Merges from Apple's GCC 4.2 r148430
    llvm-svn: 54197

--- llvm-gcc-4.2/gcc/version.c
+++ llvm-gcc-4.2/gcc/version.c
@@ -11,12 +11,12 @@
 /* APPLE LOCAL begin Apple version */
 #ifdef ENABLE_LLVM
 #ifdef LLVM_VERSION_INFO
-#define VERSUFFIX " (Based on Apple Inc. build 5555) (LLVM build " LLVM_VERSION_INFO ")"
+#define VERSUFFIX " (Based on Apple Inc. build 5615) (LLVM build " LLVM_VERSION_INFO ")"
 #else
-#define VERSUFFIX " (Based on Apple Inc. build 5555) (LLVM build)"
+#define VERSUFFIX " (Based on Apple Inc. build 5615) (LLVM build)"
 #endif
 #else
-#define VERSUFFIX " (Based on Apple Inc. build 5555)"
+#define VERSUFFIX " (Based on Apple Inc. build 5615)"
 #endif
 /* APPLE LOCAL end Apple version */
 

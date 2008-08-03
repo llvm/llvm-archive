f4b822c383afe3645e7cdaca1bfad746b572ff79
Bill Wendling <isanbard@gmail.com>
2008-08-03 04:13:46 +0000

    Update comment about BLOCK_POINTER_TYPEs.
    llvm-svn: 54291

--- llvm-gcc-4.2/gcc/llvm-internal.h
+++ llvm-gcc-4.2/gcc/llvm-internal.h
@@ -127,9 +127,9 @@ class TypeConverter {
   bool ConvertingStruct;
   
   /// PointersToReresolve - When ConvertingStruct is true, we handling of
-  /// POINTER_TYPE and REFERENCE_TYPE is changed to return opaque*'s instead of
-  /// recursively calling ConvertType.  When this happens, we add the
-  /// POINTER_TYPE to this list.
+  /// POINTER_TYPE, REFERENCE_TYPE, and BLOCK_POINTER_TYPE is changed to return
+  /// opaque*'s instead of recursively calling ConvertType.  When this happens,
+  /// we add the POINTER_TYPE to this list.
   ///
   std::vector<tree_node*> PointersToReresolve;
 

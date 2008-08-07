9f0babdfd12074d7041b96386c2eac79ac00e859
Bill Wendling <isanbard@gmail.com>
2008-08-07 18:14:25 +0000

    Revert r54245. Matthijs did a check and verified that this ugly hack isn't needed anymore.
    llvm-svn: 54479

--- llvm-gcc-4.2/gcc/limitx.h
+++ llvm-gcc-4.2/gcc/limitx.h
@@ -2,9 +2,6 @@
    if the system has its own version of limits.h.  */
 
 /* APPLE LOCAL begin 4401222 */
-/* LLVM LOCAL */
-#ifdef CONFIG_DARWIN_H
-
 #ifndef _LIBC_LIMITS_H_
 /* Use "..." so that we find syslimits.h only in this same directory.  */
 #include "syslimits.h"
@@ -13,23 +10,4 @@
 #include_next <limits.h>
 #undef _GCC_NEXT_LIMITS_H
 #endif
-
-/* LLVM LOCAL begin */
-#endif /* not CONFIG_DARWIN_H */
-
-/* We use _GCC_LIMITS_H_ because we want this not to match
-   any macros that the system's limits.h uses for its own purposes.  */
-/* LLVM LOCAL */
-#if !defined(_GCC_LIMITS_H_) && !defined(CONFIG_DARWIN_H)  /* Terminated in limity.h.  */
-#define _GCC_LIMITS_H_
-
-#ifndef _LIBC_LIMITS_H_
-/* Use "..." so that we find syslimits.h only in this same directory.  */
-#include "syslimits.h"
-#endif
-#ifdef _GCC_NEXT_LIMITS_H
-#include_next <limits.h>
-#undef _GCC_NEXT_LIMITS_H
-#endif
-/* LLVM LOCAL end */
 /* APPLE LOCAL end 4401222 */

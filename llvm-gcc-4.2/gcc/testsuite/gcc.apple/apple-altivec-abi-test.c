48d417f1119da675445744175d11635527768c30
Bill Wendling <isanbard@gmail.com>
2008-09-07 00:10:45 +0000

    Check for spaces in output to match LLVM output
    llvm-svn: 55874

--- llvm-gcc-4.2/gcc/testsuite/gcc.apple/apple-altivec-abi-test.c
+++ llvm-gcc-4.2/gcc/testsuite/gcc.apple/apple-altivec-abi-test.c
@@ -1,19 +1,21 @@
 /* APPLE LOCAL file AltiVec */
 /* { dg-do compile { target powerpc*-*-darwin* } } */
 /* { dg-options "-O -faltivec" } */
-/* { dg-final { scan-assembler "vspltisw v2,1" } } */
-/* { dg-final { scan-assembler "vspltisw v3,2" } } */
-/* { dg-final { scan-assembler "vspltisw v4,3" } } */
-/* { dg-final { scan-assembler "vspltisw v5,4" } } */
-/* { dg-final { scan-assembler "vspltisw v6,5" } } */
-/* { dg-final { scan-assembler "vspltisw v7,6" } } */
-/* { dg-final { scan-assembler "vspltisw v8,7" } } */
-/* { dg-final { scan-assembler "vspltisw v9,8" } } */
-/* { dg-final { scan-assembler "vspltisw v10,9" } } */
-/* { dg-final { scan-assembler "vspltisw v11,10" } } */
-/* { dg-final { scan-assembler "vspltisw v12,11" } } */
-/* { dg-final { scan-assembler "vspltisw v13,12" } } */
-
+/* LLVM LOCAL begin check for arbitrary spaces in asm output */
+/* { dg-final { scan-assembler "vspltisw v2, *1" } } */
+/* { dg-final { scan-assembler "vspltisw v3, *2" } } */
+/* { dg-final { scan-assembler "vspltisw v4, *3" } } */
+/* { dg-final { scan-assembler "vspltisw v5, *4" } } */
+/* { dg-final { scan-assembler "vspltisw v6, *5" } } */
+/* { dg-final { scan-assembler "vspltisw v7, *6" } } */
+/* { dg-final { scan-assembler "vspltisw v8, *7" } } */
+/* { dg-final { scan-assembler "vspltisw v9, *8" } } */
+/* { dg-final { scan-assembler "vspltisw v10, *9" } } */
+/* { dg-final { scan-assembler "vspltisw v11, *10" } } */
+/* { dg-final { scan-assembler "vspltisw v12, *11" } } */
+/* { dg-final { scan-assembler "vspltisw v13, *12" } } */
+/* LLVM LOCAL end check for arbitrary spaces in asm output */
+ 
 void
 foo (vector signed int v0, vector signed int v1, vector signed int v2,
      vector signed int v3, vector signed int v4, vector signed int v5,

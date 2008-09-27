c96ab6364e9c8cad51397717448ae46319e826dc
Bill Wendling <isanbard@gmail.com>
2008-09-27 00:48:50 +0000

    Remove params and move multiplication to i386/i386.c.
    llvm-svn: 56739

--- llvm-gcc-4.2/gcc/config/i386/emmintrin.h
+++ llvm-gcc-4.2/gcc/config/i386/emmintrin.h
@@ -1476,10 +1476,10 @@ _mm_srli_si128 (__m128i __A, int __B)
   return ((__m128i)__builtin_ia32_pslldqi128 (__A, __B * 8));
 }
 #else
-#define _mm_srli_si128(__A, __B) \
-  ((__m128i)__builtin_ia32_psrldqi128 (__A, (__B) * 8))
-#define _mm_slli_si128(__A, __B) \
-  ((__m128i)__builtin_ia32_pslldqi128 (__A, (__B) * 8))
+/* APPLE LOCAL begin 5919583 */
+#define _mm_srli_si128  (__m128i)__builtin_ia32_psrldqi128
+#define _mm_slli_si128  (__m128i)__builtin_ia32_pslldqi128
+/* APPLE LOCAL end 5919583 */
 #endif
 
 #if 0

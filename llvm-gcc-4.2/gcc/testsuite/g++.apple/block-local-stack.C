4bd743b92d24579bdc449bcc8173bdaba047fa48
Nicolas Geoffray <nicolas.geoffray@lip6.fr>
2008-10-20 09:01:05 +0000

    Create release_024 branch.
    llvm-svn: 57822

--- llvm-gcc-4.2/gcc/testsuite/g++.apple/block-local-stack.C
+++
@@ -1,8 +0,0 @@
-/* APPLE LOCAL file radar 5732232 - blocks */
-/* { dg-do compile } */
-/* { dg-options "-fblocks" } */
-
-void (^test3())(void) {
-  return ^{};   /* { dg-error "returning block that lives on the local stack" } */
-}
-

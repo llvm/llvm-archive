69c1a35b59fdc2893cfca2175504222e8baef158
Bill Wendling <isanbard@gmail.com>
2008-11-03 20:42:17 +0000

    Call gcc_assert instead of returning a value.
    llvm-svn: 58640

--- llvm-gcc-4.2/gcc/stub-c.c
+++ llvm-gcc-4.2/gcc/stub-c.c
@@ -185,6 +185,6 @@ create_init_utf16_var (const unsigned char * inbuf  ATTRIBUTE_UNUSED,
 tree
 build_generic_block_struct_type (void)
 {
-  return NULL_TREE;
+  gcc_assert(0);
 }
 /* APPLE LOCAL end radar 6300081  */

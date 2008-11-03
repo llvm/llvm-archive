2881c807d655fa275545b407ed75019229606e25
Bill Wendling <isanbard@gmail.com>
2008-11-03 01:59:34 +0000

    Update APPLE LOCAL markers.
    llvm-svn: 58587

--- llvm-gcc-4.2/gcc/fold-const.c
+++ llvm-gcc-4.2/gcc/fold-const.c
@@ -10571,11 +10571,13 @@ fold_binary (enum tree_code code, tree type, tree op0, tree op1)
       /* bool_var != 1 becomes !bool_var. */
       if (TREE_CODE (TREE_TYPE (arg0)) == BOOLEAN_TYPE && integer_onep (arg1)
           && code == NE_EXPR)
+        /* APPLE LOCAL file radar 6286881 */
         return fold_build1 (TRUTH_NOT_EXPR, type, fold_convert (type, arg0));
 
       /* bool_var == 0 becomes !bool_var. */
       if (TREE_CODE (TREE_TYPE (arg0)) == BOOLEAN_TYPE && integer_zerop (arg1)
           && code == EQ_EXPR)
+        /* APPLE LOCAL file radar 6286881 */
         return fold_build1 (TRUTH_NOT_EXPR, type, fold_convert (type, arg0));
 
       /*  ~a != C becomes a != ~C where C is a constant.  Likewise for ==.  */

2cec9f855a19b6b56a08e6131225bf356fd31ec9
Dale Johannesen <dalej@apple.com>
2009-01-25 18:48:46 +0000

    Allow for different spacing from llvm-gcc.
    llvm-svn: 62970

--- llvm-gcc-4.2/gcc/testsuite/gcc.dg/darwin-comm.c
+++ llvm-gcc-4.2/gcc/testsuite/gcc.dg/darwin-comm.c
@@ -1,5 +1,6 @@
 /* APPLE LOCAL darwin10 */
 /* { dg-do compile { target *-*-darwin[912]* } } */
-/* { dg-final { scan-assembler ".comm _foo,1,15" } } */
+/* LLVM LOCAL allow for whitespace difference */
+/* { dg-final { scan-assembler ".comm( |\t)_foo,1,15" } } */
 
 char foo __attribute__ ((aligned(32768)));

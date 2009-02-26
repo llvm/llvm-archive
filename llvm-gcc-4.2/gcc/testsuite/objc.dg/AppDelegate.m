843e070db8fbedfa02dee0a6ad8c371cee8a21f1
Dale Johannesen <dalej@apple.com>
2009-02-26 18:29:12 +0000

    Fix local marker
    llvm-svn: 65550

--- llvm-gcc-4.2/gcc/testsuite/objc.dg/AppDelegate.m
+++ llvm-gcc-4.2/gcc/testsuite/objc.dg/AppDelegate.m
@@ -3,6 +3,7 @@
 /*  AppDelegate.m, AppDelegate.h  */
 /* { dg-do compile } */
 /* { dg-options " -O0 -gdwarf-2 -c -dA -mmacosx-version-min=10.5" { target powerpc*-*-darwin* i?86*-*-darwin* } } */
+/* LLVM LOCAL allow for extra space */
 /* { dg-final { scan-assembler "AppDelegate.m ?:12" } } */
 
 #import "AppDelegate.h"

74d03960b83a2ee4f92bf679521f60399217a986
Bill Wendling <isanbard@gmail.com>
2009-03-19 18:44:44 +0000

    Don't redefine MACHO_DYNAMIC_NO_PIC_P.
    llvm-svn: 67317

--- llvm-gcc-4.2/gcc/config/arm/arm.h
+++ llvm-gcc-4.2/gcc/config/arm/arm.h
@@ -30,8 +30,11 @@
 /* Overridden by arm/darwin.h, whether it is included first or not. */
 #ifndef TARGET_MACHO
 #define TARGET_MACHO 0
-/* LLVM LOCAL */
+/* LLVM LOCAL begin */
+#ifndef MACHO_DYNAMIC_NO_PIC_P
 #define MACHO_DYNAMIC_NO_PIC_P 0
+#endif
+/* LLVM LOCAL end */
 #endif
 /* APPLE LOCAL end ARM darwin target */
 

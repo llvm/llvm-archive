427a59feff37819b3302853ae73ae7a3265ec402
John Criswell <criswell@uiuc.edu>
2009-04-22 19:56:35 +0000

    Back-ported a change in CVS Mainline SAFECode that removes exactchecks when the GEP operation can be proven to stay within the bounds of the object.
    llvm-svn: 88348

--- safecode/lib/InsertPoolChecks/insert.cpp
+++ safecode/lib/InsertPoolChecks/insert.cpp
@@ -137,6 +137,8 @@ static Statistic<> NullBoundsChecks ("safecode",
 static Statistic<> NoSHGBoundsChecks ("safecode",
                                       "Missed bounds checks - no SHG DSNode");
 
+static Statistic<> StaticChecks ("safecode", "GEP Checks Done Statically");
+static Statistic<> TotalStatic  ("safecode", "GEP Checks Examined Statically");
 
 static Statistic<> MissedIncompleteChecks ("safecode",
                                "Poolchecks missed because of incompleteness");
@@ -184,6 +186,93 @@ static std::map<Value *, Value * > GEPBounds;
 ////////////////////////////////////////////////////////////////////////////
 // Static Functions
 ////////////////////////////////////////////////////////////////////////////
+
+//
+// Function: isSafe()
+//
+// Description:
+//  Determine whether the GEP will always generate a pointer that lands within
+//  the bounds of the object.
+//
+// Inputs:
+//  TD  - The TargetData pass which should be used for finding type-sizes and
+//        offsets of elements within a derived type.
+//  GEP - The getelementptr instruction to check.
+//
+// Return value:
+//  true  - The GEP never generates a pointer outside the bounds of the object.
+//  false - The GEP may generate a pointer outside the bounds of the object.
+//          There may also be cases where we know that the GEP *will* return an
+//          out-of-bounds pointer; we let pointer rewriting take care of those
+//          cases.
+//
+static bool
+isSafe (TargetData * TD, GetElementPtrInst * GEP) {
+  //
+  // Update the total number of GEPs that we're trying to check statically.
+  //
+  ++TotalStatic;
+
+  //
+  // Determine whether the indices of the GEP are all constant.  If not, then
+  // we don't bother to prove anything.
+  //
+  for (unsigned index = 1; index < GEP->getNumOperands(); ++index) {
+    if (ConstantInt * CI = dyn_cast<ConstantInt>(GEP->getOperand(index))) {
+      if (CI->getSExtValue() < 0) {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  }
+
+  //
+  // Check to see if we're indexing off the beginning of a known object.  If
+  // so, then find the size of the object.  Otherwise, assume the size is zero.
+  //
+  Value * PointerOperand = GEP->getPointerOperand();
+  unsigned int type_size = 0;
+  if (GlobalVariable * GV = dyn_cast<GlobalVariable>(PointerOperand)) {
+    type_size = TD->getTypeSize (GV->getType()->getElementType());
+  } else if (AllocationInst * AI = dyn_cast<AllocationInst>(PointerOperand)) {
+    type_size = TD->getTypeSize (AI->getAllocatedType());
+    if (AI->isArrayAllocation()) {
+      if (ConstantInt * CI = dyn_cast<ConstantInt>(AI->getArraySize())) {
+        if (CI->getSExtValue() > 0) {
+          type_size *= CI->getSExtValue();
+        } else {
+          return false;
+        }
+      }
+    }
+  }
+
+  //
+  // If the type size is non-zero, then we did, in fact, find an object off of
+  // which the GEP is indexing.  Statically determine if the indexing operation
+  // is always within bounds.
+  //
+  if (type_size) {
+    std::vector<Value *> Indices;
+
+    for (unsigned index = 1; index < GEP->getNumOperands(); ++index) {
+      Indices.push_back(GEP->getOperand(index));
+    }
+
+    unsigned offset = TD->getIndexedOffset (PointerOperand->getType(), Indices);
+
+    if (offset < type_size) {
+      ++StaticChecks;
+      return true;
+    }
+  }
+
+  //
+  // We cannot statically prove that the GEP is safe.
+  //
+  return false;
+}
 
 //
 // Function: isNodeRegistered()
@@ -2372,6 +2461,12 @@ InsertPoolChecks::deferBoundsCheck (GetElementPtrInst * GEP) {
 //
 bool
 InsertPoolChecks::insertExactCheck (GetElementPtrInst * GEP) {
+  //
+  // First, attempt to prove that the GEP is safe.
+  //
+  if (isSafe (TD, GEP))
+    return true;
+
   // The GEP instruction casted to the correct type
   Instruction *Casted = GEP;
 

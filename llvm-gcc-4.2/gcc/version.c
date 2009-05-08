33a8e0d615319e33f9db77a6c98638cf30ef432a
Bill Wendling <isanbard@gmail.com>
2009-05-08 23:13:30 +0000

    Merge Dib to Apple gcc 5646.
    llvm-svn: 71286

--- llvm-gcc-4.2/gcc/version.c
+++ llvm-gcc-4.2/gcc/version.c
@@ -11,12 +11,12 @@
 /* APPLE LOCAL begin Apple version */
 #ifdef ENABLE_LLVM
 #ifdef LLVM_VERSION_INFO
-#define VERSUFFIX " (Based on Apple Inc. build 5645) (LLVM build " LLVM_VERSION_INFO ")"
+#define VERSUFFIX " (Based on Apple Inc. build 5646) (LLVM build " LLVM_VERSION_INFO ")"
 #else
-#define VERSUFFIX " (Based on Apple Inc. build 5645) (LLVM build)"
+#define VERSUFFIX " (Based on Apple Inc. build 5646) (LLVM build)"
 #endif
 #else
-#define VERSUFFIX " (Based on Apple Inc. build 5645)"
+#define VERSUFFIX " (Based on Apple Inc. build 5646)"
 #endif
 /* APPLE LOCAL end Apple version */
 

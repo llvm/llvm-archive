cbfd9460bb59e23ac83a9ef26dde655d54ac38fd
Bill Wendling <isanbard@gmail.com>
2009-06-29 21:46:47 +0000

    --- Reverse-merging (from foreign repository) r74458 into 'gcc/llvm-convert.cpp': U    gcc/llvm-convert.cpp
    Temporarily revert r74458. It was causing bootstrap failures.
    
    llvm-svn: 74461

--- llvm-gcc-4.2/gcc/llvm-convert.cpp
+++ llvm-gcc-4.2/gcc/llvm-convert.cpp
@@ -4467,8 +4467,8 @@ void TreeToLLVM::EmitMemoryBarrier(bool ll, bool ls, bool sl, bool ss) {
   C[1] = ConstantInt::get(Type::Int1Ty, ls);
   C[2] = ConstantInt::get(Type::Int1Ty, sl);
   C[3] = ConstantInt::get(Type::Int1Ty, ss);
-  // Be conservatively correct.
-  C[4] = ConstantInt::get(Type::Int1Ty, true1);
+  // We assume like gcc appears to, that this only applies to cached memory.
+  C[4] = ConstantInt::get(Type::Int1Ty, true);
 
   Builder.CreateCall(Intrinsic::getDeclaration(TheModule,
                                                Intrinsic::memory_barrier),

d1ed699b338d62777ccb95bab9ba003015a27fa2
Bill Wendling <isanbard@gmail.com>
2009-07-01 23:14:04 +0000

    Hack to get this file to compile with LLVMContext stuff.
    llvm-svn: 74656

--- llvm-gcc-4.2/gcc/llvm-linker-hack.cpp
+++ llvm-gcc-4.2/gcc/llvm-linker-hack.cpp
@@ -46,10 +46,12 @@ Software Foundation, 59 Temple Place - Suite 330, Boston, MA
 ///
 void dummy_function() {
   llvm::ModuleProvider *MP = new llvm::ExistingModuleProvider(0);
+  llvm::LLVMContext Ctxt();
+
   llvm::createVerifierPass();
   llvm::CreateBitcodeWriterPass(*llvm::cout);
   llvm::WriteBitcodeToFile(0, *llvm::cout);
-  llvm::ParseBitcodeFile(NULL);
+  llvm::ParseBitcodeFile(NULL, Ctxt);
   llvm::MemoryBuffer::getNewMemBuffer(0);
 
   llvm::createInstructionCombiningPass();

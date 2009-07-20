21f98a4c951aee5a2c713653aed7b883f895b9c9
Duncan Sands <baldrick@free.fr>
2009-07-20 08:50:24 +0000

    Remove plugin logic to llvm-backend, now that this compiles.
    llvm-svn: 76413

--- dragonegg/llvm-backend.cpp
+++ dragonegg/llvm-backend.cpp
@@ -65,18 +65,23 @@ extern "C" {
 #include "target.h"
 #include "tree.h"
 
-#include "flags.h"
-#include "diagnostic.h"
-#include "output.h"
-#include "toplev.h"
-#include "timevar.h"
-#include "tm.h"
-#include "function.h"
-#include "tree-inline.h"
-#include "langhooks.h"
 #include "cgraph.h"
+#include "diagnostic.h"
+#include "flags.h"
+#include "function.h"
+#include "gcc-plugin.h"
+extern "C" {
+#include "intl.h"
+}
+#include "langhooks.h"
+#include "output.h"
 #include "params.h"
 #include "plugin-version.h"
+#include "tm.h"
+#include "toplev.h"
+#include "tree-inline.h"
+#include "tree-flow.h"
+#include "tree-pass.h"
 
 // Plugin headers
 #include "llvm-internal.h"
@@ -1794,4 +1799,206 @@ void llvm_emit_file_scope_asm(const char *string) {
 const char* extractRegisterName(tree decl) {
   const char* Name = IDENTIFIER_POINTER(DECL_ASSEMBLER_NAME(decl));
   return (*Name==1) ? Name+1 : Name;
+}
+
+
+//===----------------------------------------------------------------------===//
+//                             Plugin interface
+//===----------------------------------------------------------------------===//
+
+// This plugin's code is licensed under the GPLv2.  The LLVM libraries use
+// the GPL compatible University of Illinois/NCSA Open Source License.
+int plugin_is_GPL_compatible; // This plugin is GPL compatible.
+
+/// execute_emit_llvm - Turn a gimple function into LLVM IR.
+static unsigned int
+execute_emit_llvm (void)
+{
+//TODO Don't want to use sorry at this stage...
+//TODO  if (cfun->nonlocal_goto_save_area)
+//TODO    sorry("%Jnon-local gotos not supported by LLVM", fndecl);
+
+//TODO Do we want to do this?  Will the warning set sorry_count etc?
+//TODO    enum symbol_visibility vis = DECL_VISIBILITY (current_function_decl);
+//TODO
+//TODO    if (vis != VISIBILITY_DEFAULT)
+//TODO      // "asm_out.visibility" emits an important warning if we're using a
+//TODO      // visibility that's not supported by the target.
+//TODO      targetm.asm_out.visibility(current_function_decl, vis);
+
+  // There's no need to defer outputting this function any more; we
+  // know we want to output it.
+  DECL_DEFER_OUTPUT(current_function_decl) = 0;
+
+  // Convert the AST to raw/ugly LLVM code.
+//FIXME  TreeToLLVM Emitter(current_function_decl);
+cout << "Yo!\n";
+
+//TODO#if 0
+//TODO  if (dump_file) {
+//TODO    fprintf (dump_file,
+//TODO             "\n\n;;\n;; Full LLVM generated for this function:\n;;\n");
+//TODO    Fn->dump();
+//TODO  }
+//TODO#endif
+//TODO
+//TODO  performLateBackendInitialization();
+//TODO  createPerFunctionOptimizationPasses();
+//TODO
+//TODO  if (PerFunctionPasses)
+//TODO    PerFunctionPasses->run(*Fn);
+//TODO
+//TODO  // TODO: Nuke the .ll code for the function at -O[01] if we don't want to
+//TODO  // inline it or something else.
+
+  // Finally, we have written out this function!
+  TREE_ASM_WRITTEN(current_function_decl) = 1;
+
+  execute_free_datastructures ();
+  return 0;
+}
+
+/// pass_emit_llvm - RTL pass that turns gimple functions into LLVM IR.
+static struct rtl_opt_pass pass_emit_llvm =
+{
+    {
+      RTL_PASS,
+      "emit_llvm",				/* name */
+      NULL,					/* gate */
+      execute_emit_llvm,			/* execute */
+      NULL,					/* sub */
+      NULL,					/* next */
+      0,					/* static_pass_number */
+      TV_EXPAND,				/* tv_id */
+      PROP_gimple_lcf | PROP_gimple_leh |
+	PROP_gimple_lomp | PROP_cfg,		/* properties_required */
+      0,					/* properties_provided */
+      PROP_ssa | PROP_trees,			/* properties_destroyed */
+      TODO_dump_func | TODO_verify_flow
+        | TODO_verify_stmts,			/* todo_flags_start */
+      TODO_ggc_collect				/* todo_flags_finish */
+    }
+};
+
+
+/// gate_null - Gate method for a pass that does nothing.
+static bool
+gate_null (void)
+{
+  return false;
+}
+
+
+/// pass_gimple_null - Gimple pass that does nothing.
+static struct gimple_opt_pass pass_gimple_null =
+{
+    {
+      GIMPLE_PASS,
+      NULL,					/* name */
+      gate_null,				/* gate */
+      NULL,					/* execute */
+      NULL,					/* sub */
+      NULL,					/* next */
+      0,					/* static_pass_number */
+      TV_NONE,					/* tv_id */
+      0,					/* properties_required */
+      0,					/* properties_provided */
+      0,					/* properties_destroyed */
+      0,					/* todo_flags_start */
+      0						/* todo_flags_finish */
+    }
+};
+
+/// pass_rtl_null - RTL pass that does nothing.
+static struct rtl_opt_pass pass_rtl_null =
+{
+    {
+      RTL_PASS,
+      NULL,					/* name */
+      gate_null,				/* gate */
+      NULL,					/* execute */
+      NULL,					/* sub */
+      NULL,					/* next */
+      0,					/* static_pass_number */
+      TV_NONE,					/* tv_id */
+      0,					/* properties_required */
+      0,					/* properties_provided */
+      0,					/* properties_destroyed */
+      0,					/* todo_flags_start */
+      0						/* todo_flags_finish */
+    }
+};
+
+/// plugin_init - The initialization routine called by GCC.  Defined in
+/// gcc-plugin.h.
+int plugin_init (struct plugin_name_args *plugin_info,
+                 struct plugin_gcc_version *version) {
+  const char *plugin_name = plugin_info->base_name;
+  bool disable_gcc_optimizations = true;
+  struct plugin_pass pass_info;
+
+  // Check that the running gcc is the same as the gcc we were built against.
+  // If not, refuse to load.  This seems wise when developing against a fast
+  // moving gcc tree.
+  // TODO: Make the check milder if doing a "release build".
+  if (!plugin_default_version_check (version, &gcc_version)) {
+    // TODO: calling a gcc routine when there is a version mismatch is
+    // dangerous.  On the other hand, failing without an explanation is
+    // obscure.  Could send a message to std::cerr instead, but bypassing
+    // the gcc error reporting and translating mechanism is kind of sucky.
+    error(G_("plugin %qs: gcc version mismatch"), plugin_name);
+    return 1;
+  }
+
+  // Process any plugin arguments.
+  {
+    int argc = plugin_info->argc;
+    struct plugin_argument *argv = plugin_info->argv;
+
+    for (int i = 0; i < argc; ++i) {
+      if (!strcmp (argv[i].key, "enable-gcc-optzns")) {
+        if (argv[i].value)
+          warning (0, G_("option '-fplugin-arg-%s-%s=%s' ignored"
+                         " (superfluous '=%s')"),
+                   plugin_name, argv[i].key, argv[i].value, argv[i].value);
+        else
+          disable_gcc_optimizations = false;
+      } else {
+        warning (0, G_("plugin %qs: unrecognized argument %qs ignored"),
+                 plugin_name, argv[i].key);
+      }
+    }
+  }
+
+  // Replace rtl expansion with gimple to LLVM conversion.
+  pass_info.pass = &pass_emit_llvm.pass;
+  pass_info.reference_pass_name = "expand";
+  pass_info.ref_pass_instance_number = 0;
+  pass_info.pos_op = PASS_POS_REPLACE;
+  register_callback (plugin_name, PLUGIN_PASS_MANAGER_SETUP, NULL, &pass_info);
+
+  // Turn off all rtl passes.
+  pass_info.pass = &pass_gimple_null.pass;
+  pass_info.reference_pass_name = "*rest_of_compilation";
+  pass_info.ref_pass_instance_number = 0;
+  pass_info.pos_op = PASS_POS_REPLACE;
+  register_callback (plugin_name, PLUGIN_PASS_MANAGER_SETUP, NULL, &pass_info);
+
+  pass_info.pass = &pass_rtl_null.pass;
+  pass_info.reference_pass_name = "*clean_state";
+  pass_info.ref_pass_instance_number = 0;
+  pass_info.pos_op = PASS_POS_REPLACE;
+  register_callback (plugin_name, PLUGIN_PASS_MANAGER_SETUP, NULL, &pass_info);
+
+  if (disable_gcc_optimizations) {
+    // Turn off all gcc optimization passes.
+    // TODO: figure out a good way of turning off ipa passes.
+    pass_info.pass = &pass_gimple_null.pass;
+    pass_info.reference_pass_name = "*all_optimizations";
+    pass_info.ref_pass_instance_number = 0;
+    pass_info.pos_op = PASS_POS_REPLACE;
+    register_callback (plugin_name, PLUGIN_PASS_MANAGER_SETUP, NULL, &pass_info);
+  }
+
+  return 0;
 }

d559b7e89f34e6d14dbb97d94785445229df3f43
Duncan Sands <baldrick@free.fr>
2009-07-22 16:38:20 +0000

    Implement SET_DECL_LLVM and friends in the simplest possible way.
    llvm-svn: 76748

--- dragonegg/bits_and_bobs.cpp
+++ dragonegg/bits_and_bobs.cpp
@@ -16,20 +16,28 @@ extern "C" {
 #include "tree.h"
 }
 
+// Plugin headers
+extern "C" {
+#include "llvm-cache.h"
+}
+
 using namespace llvm;
 
 bool flag_odr = false;
 
 void llvm_set_decl (tree t, Value *V) {
-abort();
+  assert(HAS_RTL_P(t) && "Expected a gcc decl with RTL!");
+  llvm_set_cached(t, V);
 }
 
 Value *llvm_get_decl(tree t) {
-abort();
+  assert(HAS_RTL_P(t) && "Expected a gcc decl with RTL!");
+  return (Value *)llvm_get_cached(t);
 }
 
 bool llvm_set_decl_p(tree t) {
-abort();
+  assert(HAS_RTL_P(t) && "Expected a gcc decl with RTL!");
+  llvm_has_cached(t);
 }
 
 void eraseLocalLLVMValues() {

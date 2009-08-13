1995c4ab0e4a01012f2d84bcb9141d28456f9000
Dale Johannesen <dalej@apple.com>
2009-08-13 20:56:26 +0000

    This patch is now in our main gcc tree which means the LLVM LOCAL comments are not needed.  Obviously:)
    llvm-svn: 78936

--- llvm-gcc-4.2/gcc/config/i386/i386.c
+++ llvm-gcc-4.2/gcc/config/i386/i386.c
@@ -22965,14 +22965,12 @@ iasm_print_op (char *buf, tree arg, unsigned argnum, tree *uses,
 	      }
 	  }
 
-/* LLVM LOCAL begin */
         /* Complicated expression as JMP or CALL target. */
         if (e->modifier && strcmp(e->modifier, "A") == 0)
           {
             strcat (buf, "*");
             e->modifier = 0;
           }
-/* LLVM LOCAL end */
 	e->as_immediate = true;
 	iasm_print_operand (buf, op1, argnum, uses,
 			    must_be_reg, must_not_be_reg, e);

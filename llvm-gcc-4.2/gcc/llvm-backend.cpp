b57bb5fca5c0822ff9ce6ca4d69296210d3d0504
Bill Wendling <isanbard@gmail.com>
2009-09-24 18:07:53 +0000

    Finally and truly remove exception handling code.
    llvm-svn: 82695

--- llvm-gcc-4.2/gcc/llvm-backend.cpp
+++ llvm-gcc-4.2/gcc/llvm-backend.cpp
@@ -1386,7 +1386,7 @@ void emit_global_to_llvm(tree decl) {
 #endif
   }
 
-  // No debug info for globals when optimization is on. While this is
+  // No debug info for globals when optimization is on.  While this is
   // something that would be accurate and useful to a user, it currently
   // affects some optimizations that, e.g., count uses.
   if (TheDebugInfo && !optimize) {

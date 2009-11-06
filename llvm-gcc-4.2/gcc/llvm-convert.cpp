6fdcd25cbed12eed7ecf192d88f2078b98224ccf
Chris Lattner <sabre@nondot.org>
2009-11-06 04:27:34 +0000

    match mainline llvm
    llvm-svn: 86232

--- llvm-gcc-4.2/gcc/llvm-convert.cpp
+++ llvm-gcc-4.2/gcc/llvm-convert.cpp
@@ -8149,8 +8149,7 @@ Constant *TreeConstantToLLVM::EmitLV_COMPONENT_REF(tree exp) {
     FieldPtr = TheFolder->CreateGetElementPtr(StructAddrLV, Ops+1, 2);
 
     FieldPtr = ConstantFoldInstOperands(Instruction::GetElementPtr,
-                                        FieldPtr->getType(), Ops,
-                                        3, Context, &TD);
+                                        FieldPtr->getType(), Ops, 3, &TD);
 
     // Now that we did an offset from the start of the struct, subtract off
     // the offset from BitStart.

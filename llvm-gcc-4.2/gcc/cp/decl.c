1fba14892c6ad3a37da49c9be0eb1e81daaaf2cb
Stuart Hastings <stuart@apple.com>
2010-04-14 01:31:38 +0000

    Prevent a very unlikely ICE in the presence of an ungrammatical test fragment.  Radar 7212824.
    llvm-svn: 101204

--- llvm-gcc-4.2/gcc/cp/decl.c
+++ llvm-gcc-4.2/gcc/cp/decl.c
@@ -2210,9 +2210,11 @@ redeclaration_error_message (tree newdecl, tree olddecl)
 	   union { int i; };
 
 	   is invalid.  */
-      if (DECL_ANON_UNION_VAR_P (newdecl)
-	  || DECL_ANON_UNION_VAR_P (olddecl))
-	return "redeclaration of %q#D";
+      if (TREE_CODE (olddecl) == VAR_DECL
+          && TREE_CODE (newdecl) == VAR_DECL
+          && (DECL_ANON_UNION_VAR_P (newdecl)
+              || DECL_ANON_UNION_VAR_P (olddecl)))
+          return "redeclaration of %q#D";
       /* If at least one declaration is a reference, there is no
 	 conflict.  For example:
 

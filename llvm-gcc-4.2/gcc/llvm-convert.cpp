61df76af5351d290a1768fca704f048943ee1567
Bill Wendling <isanbard@gmail.com>
2010-04-14 03:55:33 +0000

    Put the magic EH variable into the llvm.metadata section so that the ASM printer doesn't output it ever.
    llvm-svn: 101216

--- llvm-gcc-4.2/gcc/llvm-convert.cpp
+++ llvm-gcc-4.2/gcc/llvm-convert.cpp
@@ -2096,7 +2096,7 @@ void TreeToLLVM::EmitLandingPads() {
     foreach_reachable_handler(i, false, AddHandler, &Handlers);
 
     bool HasCleanup = false;
-    static Value *CatchAll = 0;
+    static GlobalVariable *CatchAll = 0;
 
     for (std::vector<struct eh_region *>::iterator I = Handlers.begin(),
          E = Handlers.end(); I != E; ++I) {
@@ -2131,6 +2131,7 @@ void TreeToLLVM::EmitLandingPads() {
             CatchAll = new GlobalVariable(*TheModule, Init->getType(), true,
                                           GlobalVariable::LinkOnceAnyLinkage,
                                           Init, ".llvm.eh.catch.all.value");
+            CatchAll->setSection("llvm.metadata");
           }
 
           Args.push_back(CatchAll);
@@ -2173,6 +2174,7 @@ void TreeToLLVM::EmitLandingPads() {
             CatchAll = new GlobalVariable(*TheModule, Init->getType(), true,
                                           GlobalVariable::LinkOnceAnyLinkage,
                                           Init, ".llvm.eh.catch.all.value");
+            CatchAll->setSection("llvm.metadata");
           }
 
           Args.push_back(CatchAll);

8288e35d7e824a66d775220bd74152015dac451a
Jakob Stoklund Olesen <stoklund@2pi.dk>
2010-04-28 23:42:58 +0000

    Obey the alignment specified on byval arguments.
    This could cause problems in a function like this:
    
      typedef struct {
          long double location;
          long double length;
      } mstr;
    
      mstr x;
    
      void bar(int i, int j, mstr y) {
        x = y;
      }
    
    The %y argument to bar() has align 4:
    
      define void @bar(i32 %i, i32 %j, %struct.mstr* byval align 4 %y) nounwind ssp {
    
    But a 16-byte aligned memcpy was emitted:
    
      call void @llvm.memcpy.p0i8.p0i8.i32(i8* bitcast (%struct.mstr* @x to i8*), i8* %y1, i32 32, i32 16, i1 false)
    
    llvm-svn: 102561

--- llvm-gcc-4.2/gcc/llvm-convert.cpp
+++ llvm-gcc-4.2/gcc/llvm-convert.cpp
@@ -7395,6 +7395,13 @@ LValue TreeToLLVM::EmitLV_DECL(tree exp) {
       Alignment = DECL_ALIGN(exp) / 8;
   }
 
+  // Function arguments may have extra alignment attributes.
+  if (Argument *Arg = dyn_cast<Argument>(Decl)) {
+    unsigned pa = Arg->getParent()->getParamAlignment(Arg->getArgNo()+1);
+    if (pa && pa < Alignment)
+      Alignment = pa;
+  }
+
   return LValue(BitCastToType(Decl, PTy), Alignment);
 }
 

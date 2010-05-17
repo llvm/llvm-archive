f62f55eeb2cbf343ca500c7f578ddb1b7c6e16c7
Bill Wendling <isanbard@gmail.com>
2010-05-17 21:06:04 +0000

    Turn on "--disable-non-leaf-fp-elim" if -momit-leaf-frame-pointer is specified.
    llvm-svn: 103976

--- llvm-gcc-4.2/gcc/config/i386/i386.h
+++ llvm-gcc-4.2/gcc/config/i386/i386.h
@@ -3933,33 +3933,31 @@ enum ix86_builtins
      (ISDIGIT (*nm) ? reg_names[REG_NUM] : nm); })
 
 /* Propagate code model setting to backend */
-/*
-  FIXME: (See <rdar://problem/7946145>) This option was mis-compiling a library.
-
-    if (TARGET_OMIT_LEAF_FRAME_POINTER)
-      argvec.push_back("--disable-non-leaf-fp-elim");
-*/
-#define LLVM_SET_MACHINE_OPTIONS(argvec)	   \
-  switch (ix86_cmodel) {			   \
-  default:                                         \
-    sorry ("code model %<%s%> not supported yet", ix86_cmodel_string);  \
-    break;                                         \
-  case CM_SMALL:				   \
-  case CM_SMALL_PIC:				   \
-    argvec.push_back("--code-model=small");	   \
-    break;					   \
-  case CM_KERNEL:				   \
-    argvec.push_back("--code-model=kernel");	   \
-    break;					   \
-  case CM_MEDIUM:				   \
-  case CM_MEDIUM_PIC:				   \
-    argvec.push_back("--code-model=medium");	   \
-    break;					   \
-  case CM_32:					   \
-    argvec.push_back("--code-model=default");	   \
-    break;					   \
-  }
-
+#define LLVM_SET_MACHINE_OPTIONS(argvec)                \
+  do {                                                  \
+    switch (ix86_cmodel) {                              \
+    default:                                            \
+      sorry ("code model %<%s%> not supported yet",     \
+             ix86_cmodel_string);                       \
+      break;                                            \
+    case CM_SMALL:                                      \
+    case CM_SMALL_PIC:                                  \
+      argvec.push_back("--code-model=small");           \
+      break;                                            \
+    case CM_KERNEL:                                     \
+      argvec.push_back("--code-model=kernel");          \
+      break;                                            \
+    case CM_MEDIUM:                                     \
+    case CM_MEDIUM_PIC:                                 \
+      argvec.push_back("--code-model=medium");          \
+      break;                                            \
+    case CM_32:                                         \
+      argvec.push_back("--code-model=default");         \
+      break;                                            \
+    }                                                   \
+    if (TARGET_OMIT_LEAF_FRAME_POINTER)                 \
+      argvec.push_back("--disable-non-leaf-fp-elim");   \
+  } while (0)
 #endif /* ENABLE_LLVM */
 /* LLVM LOCAL end */
 

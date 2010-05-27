f67e15a49e89ea61d70fe4c4f5ac00ce8de865ba
Evan Cheng <evan.cheng@apple.com>
2010-05-27 08:48:48 +0000

    Merge 98165.
    llvm-svn: 104830

--- llvm-gcc-4.2/gcc/config/arm/arm.h
+++ llvm-gcc-4.2/gcc/config/arm/arm.h
@@ -3548,6 +3548,16 @@ enum neon_builtins
                                     DESTTY, OPS)                              \
         TargetIntrinsicLower(EXP, BUILTIN_CODE, DESTLOC, RESULT, DESTTY, OPS);
 
+/* LLVM_GET_REG_NAME - The registers known to llvm as "r10", "r11", and "r12"
+   may have different names in GCC.  Register "r12" is called "ip", and on
+   non-Darwin OSs, "r10" is "sl" and "r11" is "fp".  Translate those names,
+   and use the default register names for everything else.  */
+#define LLVM_GET_REG_NAME(REG_NAME, REG_NUM) \
+  ((REG_NUM) == 10 ? "r10" \
+   : (REG_NUM) == 11 ? "r11" \
+   : (REG_NUM) == 12 ? "r12" \
+   : reg_names[REG_NUM])
+
 #endif /* ENABLE_LLVM */
 /* LLVM LOCAL end */
 

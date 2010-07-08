1e79ecc88b41f20027297a4b1704fc864f19af1c
Stuart Hastings <stuart@apple.com>
2010-07-08 20:34:49 +0000

    Fix line # info for local decls.  Radar 8152866.
    llvm-svn: 107908

--- llvm-gcc-4.2/gcc/llvm-debug.cpp
+++ llvm-gcc-4.2/gcc/llvm-debug.cpp
@@ -509,7 +509,7 @@ void DebugInfo::EmitDeclare(tree decl, unsigned Tag, const char *Name,
   Instruction *Call = DebugFactory.InsertDeclare(AI, D, 
                                                  Builder.GetInsertBlock());
   
-  Call->setDebugLoc(DebugLoc::get(CurLineNo, 0, VarScope.getNode()));
+  Call->setDebugLoc(DebugLoc::get(Loc.line, 0, VarScope.getNode()));
 }
 
 

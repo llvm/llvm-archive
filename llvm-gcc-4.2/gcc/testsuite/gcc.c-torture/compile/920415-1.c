06234c51dc5ca9a8b29489ed61f24c2ed344b1a5
Stuart Hastings <stuart@apple.com>
2010-07-29 18:33:06 +0000

    XFAIL a nested-function test.  Radar 8251322.
    llvm-svn: 109789

--- llvm-gcc-4.2/gcc/testsuite/gcc.c-torture/compile/920415-1.c
+++ llvm-gcc-4.2/gcc/testsuite/gcc.c-torture/compile/920415-1.c
@@ -1,6 +1,8 @@
 /* APPLE LOCAL begin testsuite nested functions */
 /* { dg-options "-fnested-functions" } */
 /* APPLE LOCAL end testsuite nested functions */
+/* LLVM LOCAL 8251263 */
+/* { dg-xfail-if "" { *-*-darwin* } { "*" } { "" } } */
 
 extern void abort (void);
 extern void exit (int);

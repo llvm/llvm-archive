45142e58dce31518176d19e49e5b24707124da9c
Bill Wendling <isanbard@gmail.com>
2010-08-10 07:46:23 +0000

    Fix spacing, but the strings still do not match.
    llvm-svn: 110662

--- llvm-gcc-4.2/gcc/testsuite/g++.dg/abi/key2.C
+++ llvm-gcc-4.2/gcc/testsuite/g++.dg/abi/key2.C
@@ -3,12 +3,13 @@
 
 // { dg-do compile { target *-*-darwin* } }
 // LLVM LOCAL begin rearrange tests to check current syntax
-// { dg-final { scan-assembler "	.section	__DATA,__const_coal,coalesced\\n	.globl	__ZTV1f\\n	.weak_definition __ZTV1f\\n" } }
-// { dg-final { scan-assembler "	.section	__TEXT,__const_coal,coalesced\\n	.globl	__ZTS1f\\n	.weak_definition __ZTS1f\\n" } }
+// { dg-final { scan-assembler ".section\[ \t]+__DATA,__const_coal,coalesced\\n\[ \t]+.globl\[ \t]+__ZTV1f\\n\[ \t]+.weak_definition\[ \t]+__ZTV1f\\n" } }
+// { dg-final { scan-assembler ".section\[ \t]+__TEXT,__const_coal,coalesced\\n\[ \t]+.globl\[ \t]+__ZTS1f\\n\[ \t]+.weak_definition\[ \t]+__ZTS1f\\n" } }
+
 //  With llvm ZTI1f is in the right place, but the ordering is different
 // so the .section directive is not needed.  Do the best we can.
 // (It belongs in the same place as ZTV1f.)
-// { dg-final { scan-assembler ".globl	__ZTI1f\\n	.weak_definition __ZTI1f\\n" } }
+// { dg-final { scan-assembler ".globl\[ \t]+__ZTI1f\\n\[ \t]+.weak_definition\[ \t]+__ZTI1f\\n" } }
 // LLVM LOCAL end
 
 class f

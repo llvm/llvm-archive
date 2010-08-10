e99005f131366a0d9cf285c0a61a149813bf88ed
Bill Wendling <isanbard@gmail.com>
2010-08-10 11:10:32 +0000

    llvm emits the symbol into the bss section with a .zerofill directive. The symbol has the correct linkage and section via nm.
    llvm-svn: 110672

--- llvm-gcc-4.2/gcc/testsuite/gcc.apple/5490617.c
+++ llvm-gcc-4.2/gcc/testsuite/gcc.apple/5490617.c
@@ -4,4 +4,4 @@ static char _my_endbss[1];
 char *my_endbss_static = _my_endbss;
 
 /* LLVM LOCAL allow different spacing */
-/* { dg-final { scan-assembler ".lcomm( |\t)__my_endbss" } } */
+/* { dg-final { scan-assembler ".zerofill\[ \t]+__DATA,__bss,__my_endbss" } } */

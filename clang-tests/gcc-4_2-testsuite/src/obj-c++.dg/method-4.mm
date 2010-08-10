12eafd61b64da1183c7ccb1075d73325ce45fae3
Fariborz Jahanian <fjahanian@apple.com>
2010-08-10 20:49:19 +0000

    Fix the test.
    llvm-svn: 110725

--- clang-tests/gcc-4_2-testsuite/src/obj-c++.dg/method-4.mm
+++ clang-tests/gcc-4_2-testsuite/src/obj-c++.dg/method-4.mm
@@ -2,23 +2,23 @@
    -Wno-strict-selector-match is on.  */
 
 /* { dg-do compile } */
-/* { dg-options "-Wstrict-selector-match" } */
+/* { dg-options "-Wno-strict-selector-match" } */
 
 #include <objc/objc.h>
 
 @interface Base
-- (id) meth1: (Base *)arg1; /* { dg-warning "using .\\-\\(id\\)meth1:\\(Base \\*\\)arg1." } */
-- (id) window;              /* { dg-warning "using .\\-\\(id\\)window" } */
+- (id) meth1: (Base *)arg1; 
+- (id) window; 
 @end
 
 @interface Derived: Base
-- (id) meth1: (Derived *)arg1; /* { dg-warning "also found .\\-\\(id\\)meth1:\\(Derived \\*\\)arg1." } */
-- (Base *) window;             /* { dg-warning "also found .\\-\\(Base \\*\\)window." } */
+- (id) meth1: (Derived *)arg1;
+- (Base *) window;
 @end
 
 void foo(void) {
   id r;
 
-  [r meth1:r];  /* { dg-warning "multiple methods named .\\-meth1:. found" } */
-  [r window];   /* { dg-warning "multiple methods named .\\-window. found" } */
+  [r meth1:r];
+  [r window];
 }

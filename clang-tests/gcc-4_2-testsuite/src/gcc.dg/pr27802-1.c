250390989fd71ca5e5011b01c8c3df0f059215da
Fariborz Jahanian <fjahanian@apple.com>
2010-08-20 00:02:05 +0000

    Added -Wno-invalid-noreturn option.
    llvm-svn: 111589

--- clang-tests/gcc-4_2-testsuite/src/gcc.dg/pr27802-1.c
+++ clang-tests/gcc-4_2-testsuite/src/gcc.dg/pr27802-1.c
@@ -1,8 +1,8 @@
 /* Noreturn functions returning FP types used to confuse reg-stack on x86.  */
 /* { dg-do compile } */
-/* { dg-options "-O2" } */
+/* { dg-options "-Wno-invalid-noreturn" } */
 
-double bar1() __attribute__((noreturn));
+void bar1() __attribute__((noreturn));
 void foo1() { bar1(); }
 
 double bar2() __attribute__((noreturn));
@@ -11,6 +11,6 @@ double foo2() { return bar2(); }
 void bar3() __attribute__((noreturn));
 double foo3() { bar3(); }
 
-double bar4() __attribute__((noreturn));
+void bar4() __attribute__((noreturn));
 double foo4() { bar4(); }
 

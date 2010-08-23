6178bf44be5562b27de952c47e3d58fc4415fd24
Dan Gohman <gohman@apple.com>
2010-08-23 21:14:06 +0000

    Reinstate the code for emitting an initial debug type for a struct, to handle the case where the struct is only forward-declared. In this case, a temporary MDNode is not needed and not desired.
    llvm-svn: 111841

--- llvm-gcc-4.2/gcc/llvm-debug.cpp
+++ llvm-gcc-4.2/gcc/llvm-debug.cpp
@@ -1007,11 +1007,22 @@ DIType DebugInfo::createStructType(tree type) {
     if (MDNode *TN = dyn_cast_or_null<MDNode>(I->second))
       return DIType(TN);
   
-  llvm::DIType FwdDecl = DebugFactory.CreateTemporaryType();
-  
   // forward declaration, 
-  if (TYPE_SIZE(type) == 0) 
+  if (TYPE_SIZE(type) == 0) {
+    llvm::DICompositeType FwdDecl =
+      DebugFactory.CreateCompositeType(Tag, 
+                                       TyContext,
+                                       GetNodeName(type),
+                                       getOrCreateFile(Loc.file), 
+                                       Loc.line, 
+                                       0, 0, 0,
+                                       SFlags | llvm::DIType::FlagFwdDecl,
+                                       llvm::DIType(), llvm::DIArray(),
+                                       RunTimeLang);
     return FwdDecl;
+  }
+  
+  llvm::DIType FwdDecl = DebugFactory.CreateTemporaryType();
   
   // Insert into the TypeCache so that recursive uses will find it.
   llvm::MDNode *FDN = FwdDecl;

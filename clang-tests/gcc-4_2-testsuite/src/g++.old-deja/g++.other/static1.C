883f51acc331c81dce8fced9decba043f2d7763b
Fariborz Jahanian <fjahanian@apple.com>
2010-08-25 16:09:31 +0000

    Make this test pass with clang++
    llvm-svn: 112051

--- clang-tests/gcc-4_2-testsuite/src/g++.old-deja/g++.other/static1.C
+++ clang-tests/gcc-4_2-testsuite/src/g++.old-deja/g++.other/static1.C
@@ -3,7 +3,7 @@ extern "C" void abort();
 
 struct S
 {
-  static const int i = 3;
+  static const int i = 3;  // { dg-error "note" }
 };
 
-const int S::i = 2; // { dg-error "" } duplicate initialization
+const int S::i = 2; // { dg-error "redefinition" }

6d29a498da6426f67ce511a01182d55c6364c063
Bill Wendling <isanbard@gmail.com>
2010-09-04 01:59:53 +0000

    Creating release_28 branch from llvm-gcc-4.2 trunk.
    llvm-svn: 113052

--- clang-tests/gcc-4_2-testsuite/src/gcc.dg/unused-5.c
+++
@@ -1,8 +0,0 @@
-/* { dg-do compile } */
-/* { dg-options "-Wunused" } */
-/* { dg-final { scan-assembler "string_to_look_for" } } */
-
-/* 'volatile' variables get output and don't produce a warning about being
-   unused.  */
-static volatile char string[]  __attribute__((used))
-  = "string_to_look_for"; 
--- safecode/test/BOdiagsuite-20050808/Scripts/ResultForCorrectProgram.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if [ -s $1 ]; then
-    echo "FAILED"
-else
-    echo "PASSED"
-fi

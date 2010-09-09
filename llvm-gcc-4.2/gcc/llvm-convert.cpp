a24b02970c5b161d7c7826cc32b7dc6ce1c5da6c
Bill Wendling <isanbard@gmail.com>
2010-09-09 21:36:18 +0000

    Create llvmgcc42-2333.5 from llvmgcc42-2333.4 + patch to change .llvm.eh.catch.all.value to llvm.eh.catch.all.value.
    llvm-svn: 113544

--- llvm-gcc-4.2/gcc/llvm-convert.cpp
+++ llvm-gcc-4.2/gcc/llvm-convert.cpp
@@ -2160,7 +2160,7 @@ void TreeToLLVM::EmitLandingPads() {
 
             CatchAll = new GlobalVariable(*TheModule, Init->getType(), true,
                                           GlobalVariable::LinkOnceAnyLinkage,
-                                          Init, ".llvm.eh.catch.all.value");
+                                          Init, "llvm.eh.catch.all.value");
             CatchAll->setSection("llvm.metadata");
           }
 
@@ -2203,7 +2203,7 @@ void TreeToLLVM::EmitLandingPads() {
 
             CatchAll = new GlobalVariable(*TheModule, Init->getType(), true,
                                           GlobalVariable::LinkOnceAnyLinkage,
-                                          Init, ".llvm.eh.catch.all.value");
+                                          Init, "llvm.eh.catch.all.value");
             CatchAll->setSection("llvm.metadata");
           }
 

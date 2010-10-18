ba80c1b0e472f4fb7e6a6b5e0ee9429a904d38a1
Fariborz Jahanian <fjahanian@apple.com>
2010-10-18 18:25:41 +0000

    Make test to clang's liking.
    llvm-svn: 116726

--- clang-tests/gcc-4_2-testsuite/src/g++.old-deja/g++.jason/scoping12.C
+++ clang-tests/gcc-4_2-testsuite/src/g++.old-deja/g++.jason/scoping12.C
@@ -1,10 +1,10 @@
-// { dg-do assemble  }
+// { dg-do compile  }
 void f ()
 {
   struct A {
-    friend void g (); // { dg-error "no matching function found in local scope"}
+    friend void g (); // { dg-error "no matching function found in local scope" }
   };
 }
 void h () {
-  g ();				// { dg-error "use of undeclared identifier"}
+  g ();				// { dg-error "use of undeclared identifier" }
 }

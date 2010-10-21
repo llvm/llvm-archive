a0712ff8332243c21a69a31de4a9b073c318f490
Daniel Dunbar <daniel@zuster.org>
2010-10-21 17:57:14 +0000

    Fix dg-do syntax.
    llvm-svn: 117036

--- clang-tests/gcc-4_2-testsuite/src/objc.dg/const-str-11-64bit.m
+++ clang-tests/gcc-4_2-testsuite/src/objc.dg/const-str-11-64bit.m
@@ -2,7 +2,7 @@
 /* Test if ObjC constant string layout is checked properly, regardless of how
    constant string classes get derived.  */
 /* { dg-options "-fnext-runtime -fno-constant-cfstrings -fconstant-string-class=XStr -m64 -fobjc-abi-version=1" } */
-/* { dg-do compile { i?86*-*-darwin* } } */
+/* { dg-do compile { target i?86*-*-darwin* } } */
 
 #include <objc/Object.h>
 

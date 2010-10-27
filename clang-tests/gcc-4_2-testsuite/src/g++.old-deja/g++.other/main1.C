c9b5990428c470064ec163e70e044e4410c4bfcf
Fariborz Jahanian <fjahanian@apple.com>
2010-10-27 17:58:04 +0000

    Make this test to clang's liking.
    llvm-svn: 117464

--- clang-tests/gcc-4_2-testsuite/src/g++.old-deja/g++.other/main1.C
+++ clang-tests/gcc-4_2-testsuite/src/g++.old-deja/g++.other/main1.C
@@ -1,12 +1,12 @@
 // { dg-do compile }
 
-int main()  // { dg-error "previous declaration" }
+int main()  // { dg-error "note" }
 {
   return 0;
 }
 
 
-int main(int, const char**) // { dg-error "conflicts" }
+int main(int, const char**) // { dg-error "conflicting types" }
 {
   return 0;
 }

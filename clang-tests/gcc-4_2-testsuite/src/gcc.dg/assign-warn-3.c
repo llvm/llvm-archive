f12bca251cd4da3d59e172ef32871505b5182ad9
Daniel Dunbar <daniel@zuster.org>
2010-10-29 04:31:54 +0000

    Fix Tcl quoting.
    llvm-svn: 117639

--- clang-tests/gcc-4_2-testsuite/src/gcc.dg/assign-warn-3.c
+++ clang-tests/gcc-4_2-testsuite/src/gcc.dg/assign-warn-3.c
@@ -6,8 +6,8 @@
 
 /* This is valid to execute, so maybe shouldn't warn at all.  */
 void f0(x) signed char *x; { }
-void g0(unsigned char *x) { f0(x); } /* { dg-error "passing 'unsigned char *' to parameter of type 'signed char *'" } */
+void g0(unsigned char *x) { f0(x); } /* { dg-error "passing 'unsigned char \\*' to parameter of type 'signed char \\*'" } */
 
 /* This is undefined on execution but still must compile.  */
 void f1(x) int *x; { }
-void g1(unsigned int *x) { f1(x); } /* { dg-error "passing 'unsigned int *' to parameter of type 'int *'" } */
+void g1(unsigned int *x) { f1(x); } /* { dg-error "passing 'unsigned int \\*' to parameter of type 'int \\*'" } */

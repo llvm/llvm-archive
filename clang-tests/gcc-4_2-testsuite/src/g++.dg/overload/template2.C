6da0573a5d4c663a2633e797896462fd947c8281
Fariborz Jahanian <fjahanian@apple.com>
2010-11-08 19:34:51 +0000

    Make test to clang's liking.
    llvm-svn: 118432

--- clang-tests/gcc-4_2-testsuite/src/g++.dg/overload/template2.C
+++ clang-tests/gcc-4_2-testsuite/src/g++.dg/overload/template2.C
@@ -1,8 +1,10 @@
+// { dg-options "-Wno-unused-value" }
+
 typedef int PyObject;
 typedef PyObject *(*PyCFunction)(PyObject *, PyObject *);
-template<class T> int _clear(PyObject* self);
+template<class T> int _clear(PyObject* self);	// { dg-error "note" }
 
 void _typeInfo() 
 {
-  reinterpret_cast<PyCFunction>(_clear); // { dg-error "overloaded function" }
+  reinterpret_cast<PyCFunction>(_clear); // { dg-error "reinterpret_cast cannot resolve overloaded function" }
 }

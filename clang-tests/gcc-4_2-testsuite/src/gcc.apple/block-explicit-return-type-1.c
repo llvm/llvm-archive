3580ad7bd5fdbdeb47ac781df92ddf9038143b0d
Fariborz Jahanian <fjahanian@apple.com>
2011-01-26 00:10:58 +0000

    Fix test to pass with clang.
    llvm-svn: 124254

--- clang-tests/gcc-4_2-testsuite/src/gcc.apple/block-explicit-return-type-1.c
+++ clang-tests/gcc-4_2-testsuite/src/gcc.apple/block-explicit-return-type-1.c
@@ -7,7 +7,7 @@ int (*funcptr)(long);
 int sz1 = sizeof (int (*(^)(char x))(long));
 int sz2 = sizeof (^int(*)(int p1) { return 0; });
 
-int (*fun(int p1))(double) { }
+int (*fun(int p1))(double) {return 0;  }
 
 void (^b1)() = ^{ };
 int (^b2)() = ^{ return 0; };
@@ -21,15 +21,15 @@ int (*(^b9)(char))(long) = ^int (*(char x))(long) {return funcptr;};
 void (^b10)(int) = ^(int p1){ };
 int (*(*funcptr2)(long))[5];
 int (*(*(^b11)(char))(long))[5] =
-  ^int (*(*(char x))(long))[5] { if (x) 0; return funcptr2;};
+  ^int (*(*(char x))(long))[5] { if (x) x='a'; return funcptr2;};
 
-int sz4 = sizeof (^int(*)(int p1)(int) { return 0; }); /* { dg-error "declared as function returning a function" } */
+int sz4 = sizeof (^int(*)(int p1)(int) { return 0; }); /* { dg-error "block cannot return function type" } */
 
 int (*funcptr3[5])(long);
 int (*funcptr4)(long);
 int sz5 = sizeof (^(int i) { return i; });
 int sz6 = sizeof (^int (int i) { return i; });
 int sz7 = sizeof(^int (*())(long) {return funcptr4;});
-int sz8 = sizeof(^int (*[5])(long) {return funcptr3;});	/* { dg-error "block declared as returning an array" } */
+int sz8 = sizeof(^int (*[5])(long) {return (int)funcptr3;});	/* { dg-error " block cannot return array type" } */
 int sz9 = sizeof (^int (*((*)[5]))(long j) { j; });	/* { dg-error "undeclared" } */
 /* { dg-error "for each function it appears in"  "" { target *-*-* } 34 } */

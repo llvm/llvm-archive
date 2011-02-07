5573aeb558f776a7eb8a116d989c507c5eb2b2ba
John McCall <rjmccall@apple.com>
2011-02-07 18:47:36 +0000

    Update the arbitrary names tested for by this test.  We really ought to use a better system than this.
    llvm-svn: 125022

--- clang-tests/gcc-4_2-testsuite/src/gcc.apple/block-unique-name.c
+++ clang-tests/gcc-4_2-testsuite/src/gcc.apple/block-unique-name.c
@@ -20,8 +20,8 @@ int main(void)
         return 0;
 }
 /* { dg-final { scan-assembler "___main_block_invoke_0" } } */
-/* { dg-final { scan-assembler "___main_block_invoke_06" } } */
+/* { dg-final { scan-assembler "___main_block_invoke_010" } } */
 /* { dg-final { scan-assembler "___DOO_block_invoke_0" } } */
-/* { dg-final { scan-assembler "___DOO_block_invoke_02" } } */
+/* { dg-final { scan-assembler "___DOO_block_invoke_03" } } */
 
 

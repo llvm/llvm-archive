dd6adc0b66b5dd3bd946189c10b7d4a33b85f2bc
Duncan Sands <baldrick@free.fr>
2011-03-02 20:30:39 +0000

    DIFactory has been removed from the LLVM core.  Put a copy of it here so that llvm-gcc continues to work.  Patch by Devang.
    llvm-svn: 126872

--- llvm-gcc-4.2/gcc/llvm-linker-hack.cpp
+++ llvm-gcc-4.2/gcc/llvm-linker-hack.cpp
@@ -96,7 +96,6 @@ void dummy_function() {
   llvm::Type::getInt8Ty(llvm::getGlobalContext());
 
   llvm::PrettyStackTraceProgram(0, 0);
-  llvm::DIFactory(*static_cast<llvm::Module *>(0));
   std::string Err;
   llvm::TargetRegistry::lookupTarget("", Err);
 

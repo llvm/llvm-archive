40fe5ecea256b0e48d2f536da0d9eef485dfb2eb
Fariborz Jahanian <fjahanian@apple.com>
2011-03-24 17:42:03 +0000

    With fix to radar 9108698, we should test the fix again.
    llvm-svn: 128221

---
+++ clang-tests/gcc-4_2-testsuite/src/g++.dg/other/component1.C
@@ -0,0 +1,29 @@
+// { dg-do compile }
+
+// Copyright (C) 2001, 2002 Free Software Foundation, Inc.
+// Contributed by Nathan Sidwell 28 Dec 2001 <nathan@codesourcery.com>
+
+// PR 5123. ICE
+
+struct C {
+  template<class T> void f(T); // { dg-error "candidate function" "" }
+  void g (); // { dg-error "candidate function" "" }
+  void g (int); // { dg-error "candidate function" "" }
+};
+
+void Foo () {
+  C c;
+
+  (c.g) ();
+  (c.f) (1);
+  
+  (c.f<int>) (2);
+
+  c.g;			// { dg-error "address of overloaded" "" }
+  c.f;		        // { dg-error "address of overloaded" "" }
+  c.f<int>;		// { dg-error "a bound member function may only be called" }
+  
+  c.g == 1;		// { dg-error "invalid" "" }
+  c.f == 1;		// { dg-error "invalid" "" }
+  c.f<int> == 1;	// { dg-error "comparison between pointer and integer" }
+}

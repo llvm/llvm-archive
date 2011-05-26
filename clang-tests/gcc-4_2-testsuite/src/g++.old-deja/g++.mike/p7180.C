2828ff9c5a1ef584769a6e7d014f75f1ecf11143
Eli Friedman <eli.friedman@gmail.com>
2011-05-26 19:00:24 +0000

    Attempt to fix recent testsuite failures.
    llvm-svn: 132133

--- clang-tests/gcc-4_2-testsuite/src/g++.old-deja/g++.mike/p7180.C
+++ clang-tests/gcc-4_2-testsuite/src/g++.old-deja/g++.mike/p7180.C
@@ -3,11 +3,11 @@
 
 class String {
 public:
-   String(const char*);
+   String(const char*); // { dg-error "note" }
    ~String();
 };
  
-String::String(const char* str = "") {
+String::String(const char* str = "") { // { dg-error "addition of default argument on redeclaration" }
 }
  
 String::~String(void) {

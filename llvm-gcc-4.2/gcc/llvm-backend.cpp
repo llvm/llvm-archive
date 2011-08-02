13a726e19b23b2b4e96a432a320e6bab0af56b8b
Rafael Espindola <rafael.espindola@gmail.com>
2011-08-02 23:41:45 +0000

    Update for llvm api change.
    llvm-svn: 136747

--- llvm-gcc-4.2/gcc/llvm-backend.cpp
+++ llvm-gcc-4.2/gcc/llvm-backend.cpp
@@ -49,7 +49,7 @@ Software Foundation, 59 Temple Place - Suite 330, Boston, MA
 #include "llvm/ADT/StringMap.h"
 #include "llvm/Support/ManagedStatic.h"
 #include "llvm/Support/MemoryBuffer.h"
-#include "llvm/Support/PassManagerBuilder.h"
+#include "llvm/Transforms/IPO/PassManagerBuilder.h"
 #include "llvm/Support/FormattedStream.h"
 #include "llvm/Support/Program.h"
 

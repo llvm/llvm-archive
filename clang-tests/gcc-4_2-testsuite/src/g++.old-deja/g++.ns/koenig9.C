5ac9a1d3a672ec9ca33e8959d54f9fa9e5ccb303
Eli Friedman <eli.friedman@gmail.com>
2011-08-17 22:04:50 +0000

    Fix test.
    llvm-svn: 137883

--- clang-tests/gcc-4_2-testsuite/src/g++.old-deja/g++.ns/koenig9.C
+++ clang-tests/gcc-4_2-testsuite/src/g++.old-deja/g++.ns/koenig9.C
@@ -3,7 +3,7 @@
 // Copyright (C) 2000 Free Software Foundation, Inc.
 // Contributed by Theodore.Papadopoulo 23 Jun 2000 <Theodore.Papadopoulo@sophia.inria.fr>
 
-#include <algorithm>
+template<typename T> void count(T) {} //  { dg-warning "" }
 
 void foo(const char*,...);
 

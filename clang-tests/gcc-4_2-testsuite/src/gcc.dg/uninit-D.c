291753cce0f3bc5e1b9c54aabfd9b05481ae845f
Ted Kremenek <kremenek@apple.com>
2011-10-13 23:59:44 +0000

    Remove uninit-D.c and uninit-E.c.  These tests already appear in the Clang test suite, and Clang has intentionally different behavior from GCC here.
    llvm-svn: 141918

--- clang-tests/gcc-4_2-testsuite/src/gcc.dg/uninit-D.c
+++
@@ -1,9 +0,0 @@
-/* Test we do not warn about initializing variable with self. */
-/* { dg-do compile } */
-/* { dg-options "-O -Wuninitialized" } */
-
-int f()
-{
-  int i = i; /* { dg-error "" } */
-  return i; /* { dg-warning "variable 'i' is uninitialized when used here" } */
-}
--- clang-tests/gcc-4_2-testsuite/src/gcc.dg/uninit-E.c
+++
@@ -1,9 +0,0 @@
-/* Test we do warn about initializing variable with self when -Winit-self is supplied. */
-/* { dg-do compile } */
-/* { dg-options "-O -Wuninitialized -Winit-self" } */
-
-int f()
-{
-  int i = i; /* { dg-error "" } */
-  return i; /* { dg-warning "variable 'i' is uninitialized when used here" }  */
-}

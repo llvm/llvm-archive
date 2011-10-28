5800f2e94bdc7d501a1ba20fd13eec98bf1b02e2
Chandler Carruth <chandlerc@gmail.com>
2011-10-28 20:43:47 +0000

    Experiment #3 in getting these to work. Now based on the failures in #2 and looking at more of the rest of the test suite. This still won't be correct, but once I see how it fails, I'll definitely be able to synthesize the working pattern.
    llvm-svn: 143221

--- clang-tests/gcc-4_2-testsuite/src/gcc.dg/debug/dwarf2/var1.c
+++ clang-tests/gcc-4_2-testsuite/src/gcc.dg/debug/dwarf2/var1.c
@@ -1,8 +1,8 @@
 /* PR 23190 */
 /* { dg-do compile } */
 /* { dg-options "-gdwarf-2 -dA" } */
-/* { dg-final { scan-assembler "\\.Lstring3\[^\\n\\r\]+DW_AT_name" } } */
-/* { dg-final { scan-assembler "\\.Lstring3:\[\\n\\r \\t\]+\\.ascii\\s+.xyzzy." } } */
+/* { dg-final { scan-assembler "\\.Lstring3\[ \\t\]+DW_AT_name" } } */
+/* { dg-final { scan-assembler "\\.Lstring3:\n\[ \\t\]*\\.ascii\[ \\t\]+\"xyzzy\"" } } */
 
 void f(void)
 {

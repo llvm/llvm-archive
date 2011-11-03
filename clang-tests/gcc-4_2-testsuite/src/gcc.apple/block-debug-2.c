732db6360bb976dfdee876ca6046291aec836736
Chandler Carruth <chandlerc@gmail.com>
2011-11-03 23:03:59 +0000

    Update these debug tests to what I really hope will be working patterns at long long last. :: crosses fingers ::
    llvm-svn: 143665

--- clang-tests/gcc-4_2-testsuite/src/gcc.apple/block-debug-2.c
+++ clang-tests/gcc-4_2-testsuite/src/gcc.apple/block-debug-2.c
@@ -4,10 +4,10 @@
 
 /* { dg-do compile } */
 /* { dg-options "-g -O0 -fblocks -dA" } */
-/* { dg-final { scan-assembler "\\.Lstring11\[ \\t\]+DW_AT_name" } } */
-/* { dg-final { scan-assembler "\\.Lstring11:\n\[ \\t\]*\\.ascii\[ \\t\]+\"__block_descriptor\"" } } */
-/* { dg-final { scan-assembler "Lstring12\[ \\t\]+DW_AT_name" } } */
-/* { dg-final { scan-assembler "Lstring12:\[ \\t\]*\n\[ \\t\]*\\.ascii.*__block_literal_generic" } } */
+/* { dg-final { scan-assembler "Lstring11\[ \t\]*DW_AT_name" } } */
+/* { dg-final { scan-assembler "Lstring11:\[ \t\]*\n\[ \t\]*\.ascii.*__block_descriptor" } } */
+/* { dg-final { scan-assembler "Lstring12\[ \t\]*DW_AT_name" } } */
+/* { dg-final { scan-assembler "Lstring12:\[ \t\]*\n\[ \t\]*\.ascii.*__block_literal_generic" } } */
 
 void (^os)();
 
--- clang-tests/gcc-4_2-testsuite/src/gcc.dg/debug/dwarf2/var1.c
+++ clang-tests/gcc-4_2-testsuite/src/gcc.dg/debug/dwarf2/var1.c
@@ -1,8 +1,8 @@
 /* PR 23190 */
 /* { dg-do compile } */
 /* { dg-options "-gdwarf-2 -dA" } */
-/* { dg-final { scan-assembler "\\.Lstring3\[ \\t\]+DW_AT_name" } } */
-/* { dg-final { scan-assembler "\\.Lstring3:\n\[ \\t\]*\\.ascii\[ \\t\]+\"xyzzy\"" } } */
+/* { dg-final { scan-assembler "Lstring3\[ \t\]*DW_AT_name" } } */
+/* { dg-final { scan-assembler "Lstring3:\[ \t\]*\n\[ \t\]*\.ascii.*xyzzy" } } */
 
 void f(void)
 {

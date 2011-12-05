1c128c159df5d2ef10d535f2978c0fce8e081a88
Fariborz Jahanian <fjahanian@apple.com>
2011-12-05 19:14:47 +0000

    Match to clang's recent block return changes.
    llvm-svn: 145836

--- clang-tests/gcc-4_2-testsuite/src/g++.apple/block-return.C
+++ clang-tests/gcc-4_2-testsuite/src/g++.apple/block-return.C
@@ -6,42 +6,42 @@ typedef void (^CL)(void);
 
 CL foo() {
   short y;
-  short (^add1)(void) = ^{ return y+1; };  /* { dg-error "cannot convert" } */
+  short (^add1)(void) = ^{ return y+1; };  /* { dg-error "cannot initialize a variable" } */
 
   CL X = ^{
     if (2)
       return;
-    return 1;		/* { dg-error "void block should not return a value" } */
+    return 1;		/* { dg-error "return type 'int' must match previous return type 'void' when block literal has unspecified explicit return type" } */
   };
 
   int (^Y) (void)  = ^{
     if (3)
       return 1;
     else
-      return;		/* { dg-error "non-void block should return a value" } */
+      return;		/* { dg-error "return type 'void' must match previous return type 'int' when block literal has unspecified explicit return type" } */
   };
 
-  char *(^Z)(void) = ^{
+  char *(^Z)(void) = ^{ /* { dg-error "cannot initialize a variable" } */
     if (3)
       return "";
     else
-      return (char*)0;	/* { dg-error "incompatible type returning" } */ 
-  };			/* { dg-error "cannot convert" } */
+      return (char*)0;	/* { dg-error "return type 'char *' must match previous return type 'const char *' when block literal has unspecified explicit return type" } */ 
+  };
 
-  double (^A)(void) = ^ {
+  double (^A)(void) = ^ { /* { dg-error "cannot initialize a variable" } */
     if (1)
       return (float)1.0;
     else
       if (2)
-	return (double)2.0;	/* { dg-error "incompatible type returning" } */
-    return 1;		/* { dg-error "incompatible type returning" } */
-  };			/* { dg-error "cannot convert" } */
-  char *(^B)(void) = ^{
+	return (double)2.0;	/* { dg-error "return type 'double' must match previous return type 'float' when block literal has unspecified explicit return type" } */
+    return 1;		/* { dg-error "return type 'int' must match previous return type 'float' when block literal has unspecified explicit return type" } */
+  };
+  char *(^B)(void) = ^{ /* { dg-error "cannot initialize a variable" } */
     if (3)
       return "";
     else
-      return 2;		/* { dg-error "incompatible type returning" } */
-  };			/* { dg-error "cannot convert" } */
+      return 2;		/* { dg-error "return type 'int' must match previous return type 'const char *' when block literal has unspecified explicit return type" } */
+  };
 
   __block int i;
   int& (^o)() = ^ int& {
@@ -55,5 +55,5 @@ CL foo() {
   };
   o() = 1;
 
-  return ^{ return 1; };	/* { dg-error "cannot convert" } */
+  return ^{ return 1; };	/* { dg-error "annot initialize return object of type" } */
 }

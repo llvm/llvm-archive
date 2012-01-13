6e11966a6a32875e731bbad2207c5a6c59a569af
Fariborz Jahanian <fjahanian@apple.com>
2012-01-13 19:04:29 +0000

    Fixes test.
    llvm-svn: 148130

--- clang-tests/gcc-4_2-testsuite/src/gcc.dg/decl-8.c
+++ clang-tests/gcc-4_2-testsuite/src/gcc.dg/decl-8.c
@@ -3,8 +3,8 @@
 /* { dg-do compile } */
 /* { dg-options "" } */
 
-typedef int I; /* { dg-error "error: previous declaration of 'I' was here" } */
-typedef int I; /* { dg-error "error: redefinition of typedef 'I'" } */
+typedef int I; /* { dg-warning "previous definition" } */
+typedef int I; /* { dg-warning "warning: redefinition of typedef 'I' is a C11 feature" } */
 
 typedef int I1; /* { dg-error "error: previous declaration of 'I1' was here" } */
 typedef long I1; /* { dg-error "error: conflicting types for 'I1'" } */

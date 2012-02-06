2a47a0725d6c3aa9280361e409bc6b076d09fb6c
David Dean <david_dean@apple.com>
2012-02-06 18:23:19 +0000

    Update test case to match new compiler output
    llvm-svn: 149884

--- clang-tests/gcc-4_2-testsuite/src/obj-c++.dg/comp-types-4.mm
+++ clang-tests/gcc-4_2-testsuite/src/obj-c++.dg/comp-types-4.mm
@@ -1,5 +1,6 @@
 /* Test assignments and comparisons between protocols (obscure case).  */
 /* Author: Nicola Pero <nicola@brainstorm.co.uk>.  */
+/* { dg-options "-Wno-empty-body" } */
 /* { dg-do compile } */
 
 #include <objc/objc.h>
@@ -26,24 +27,24 @@ int main()
   id<MyProtocolAB> obj_ab = nil;
   id<MyProtocolAC> obj_ac = nil;
 
-  obj_a = obj_b;  /* { dg-warning "does not conform" } */
+  obj_a = obj_b;  /* { dg-error "incompatible type" } */
   obj_a = obj_ab; /* Ok */
   obj_a = obj_ac; /* Ok */
   
-  obj_b = obj_a;  /* { dg-warning "does not conform" } */
+  obj_b = obj_a;  /* { dg-error "incompatible type" } */
   obj_b = obj_ab; /* Ok */
-  obj_b = obj_ac; /* { dg-warning "does not conform" } */
+  obj_b = obj_ac; /* { dg-error "incompatible type" } */
   
-  obj_ab = obj_a;  /* { dg-warning "does not conform" } */
-  obj_ab = obj_b;  /* { dg-warning "does not conform" } */
-  obj_ab = obj_ac; /* { dg-warning "does not conform" } */
+  obj_ab = obj_a;  /* { dg-warning "incompatible pointer types assigning to" } */
+  obj_ab = obj_b;  /* { dg-warning "incompatible pointer types assigning to" } */
+  obj_ab = obj_ac; /* { dg-error "incompatible type" } */
   
-  obj_ac = obj_a;  /* { dg-warning "does not conform" } */
-  obj_ac = obj_b;  /* { dg-warning "does not conform" } */
-  obj_ac = obj_ab; /* { dg-warning "does not conform" } */
+  obj_ac = obj_a;  /* { dg-warning "incompatible pointer types assigning to" } */
+  obj_ac = obj_b;  /* { dg-error "incompatible type" } */
+  obj_ac = obj_ab; /* { dg-error "incompatible type" } */
 
-  if (obj_a == obj_b) ; /* { dg-warning "lacks a cast" } */
-  if (obj_b == obj_a) ; /* { dg-warning "lacks a cast" } */
+  if (obj_a == obj_b) ; /* { dg-warning "comparison of distinct pointer types" } */
+  if (obj_b == obj_a) ; /* { dg-warning "comparison of distinct pointer types" } */
 
   if (obj_a == obj_ab) ; /* Ok */
   if (obj_ab == obj_a) ; /* Ok */ /* Spurious 2.95.4 warning here */
@@ -54,11 +55,11 @@ int main()
   if (obj_b == obj_ab) ; /* Ok */ 
   if (obj_ab == obj_b) ; /* Ok */ /* Spurious 2.95.4 warning here */
 
-  if (obj_b == obj_ac) ; /* { dg-warning "lacks a cast" } */ 
-  if (obj_ac == obj_b) ; /* { dg-warning "lacks a cast" } */ 
+  if (obj_b == obj_ac) ; /* { dg-warning "comparison of distinct pointer types" } */ 
+  if (obj_ac == obj_b) ; /* { dg-warning "comparison of distinct pointer types" } */ 
 
-  if (obj_ab == obj_ac) ; /* { dg-warning "lacks a cast" } */ 
-  if (obj_ac == obj_ab) ; /* { dg-warning "lacks a cast" } */ 
+  if (obj_ab == obj_ac) ; /* { dg-warning "comparison of distinct pointer types" } */ 
+  if (obj_ac == obj_ab) ; /* { dg-warning "comparison of distinct pointer types" } */ 
 
   return 0;
 }

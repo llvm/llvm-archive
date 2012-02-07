f168c3d188eff9e72eb6243651cc0386a8748ab6
David Dean <david_dean@apple.com>
2012-02-07 23:59:02 +0000

    Update test case to match compiler output
    llvm-svn: 150017

--- clang-tests/gcc-4_2-testsuite/src/obj-c++.dg/lookup-neg-1.mm
+++ clang-tests/gcc-4_2-testsuite/src/obj-c++.dg/lookup-neg-1.mm
@@ -1,15 +1,15 @@
 /* APPLE LOCAL file radar 4829851 */
 /* Test that objective-c++ compiler catches duplicate declarations in objective-c++'s 
    global namespace. */
-@class One;
-int One; 	/* { dg-error "redeclaration of Objective-C class" } */
+@class One;  /* { dg-warning "previous definition is here" } */
+int One;     /* { dg-error "redefinition of .One. as different kind of symbol" } */
 
-@class Foo;
+@class Foo;  /* { dg-warning "previous definition is here" } */
 
-namespace Foo { int x; } /* { dg-error "redeclaration of Objective-C class" } */
+namespace Foo { int x; } /* { dg-error "redefinition of .Foo. as different kind of symbol" } */
 
-@class X;
+@class X;    /* { dg-warning "previous definition is here" } */
 
-struct X {
+struct X {/* { dg-error "redefinition of .X. as different kind of symbol" } */
     int X;
 };

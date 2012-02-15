bd0f0f4716d1ce3068004380ae922358c7142711
David Dean <david_dean@apple.com>
2012-02-15 00:41:37 +0000

    Update test case to match compiler output
    llvm-svn: 150543

--- clang-tests/gcc-4_2-testsuite/src/obj-c++.dg/method-7.mm
+++ clang-tests/gcc-4_2-testsuite/src/obj-c++.dg/method-7.mm
@@ -3,19 +3,16 @@
 
 /* { dg-do compile } */
 
-@class UnderSpecified;
+@class UnderSpecified; /* { dg-warning "forward declaration of class here" } */
 typedef struct NotAClass {
   int a, b;
 } NotAClass;
 
 void foo(UnderSpecified *u, NotAClass *n) {
-  [n nonexistent_method];    /* { dg-warning "invalid receiver type" } */
-       /* { dg-warning "no .\\-nonexistent_method. method found" "" { target *-*-* } 12 } */
-  [NotAClass nonexistent_method]; /* { dg-error ".NotAClass. is not an Objective\\-C class name or alias" } */
-  [u nonexistent_method]; /* { dg-warning "no .\\-nonexistent_method. method found" } */
-  [UnderSpecified nonexistent_method]; /* { dg-warning "no .\\+nonexistent_method. method found" } */
+  [n nonexistent_method];   /* { dg-warning "receiver type .NotAClass .. is not .id. or interface pointer, consider casting it to .id" } */
+                            /* { dg-warning "instance method ..nonexistent_method. not found" "" { target *-*-* } 12 } */
+  [NotAClass nonexistent_method]; /* { dg-error "receiver type .NotAClass. is not an Objective.C class" } */
+  [u nonexistent_method]; /* { dg-warning "instance method ..nonexistent_method. not found" } */
+  [UnderSpecified nonexistent_method]; /* { dg-warning "receiver .UnderSpecified. is a forward class and corresponding .interface may not exist" } */
+                            /* { dg-warning "class method ..nonexistent_method. not found" "" { target *-*-* } 16 } */
 }
-
-/* { dg-warning "Messages without a matching method signature" "" { target *-*-* } 0 } */
-/* { dg-warning "will be assumed to return .id. and accept" "" { target *-*-* } 0 } */
-/* { dg-warning ".\.\.\.. as arguments" "" { target *-*-* } 0 } */

9741c43adbd61ccf4969168820d79500c41dd8b2
David Dean <david_dean@apple.com>
2012-02-15 00:41:04 +0000

    Update test case to match compiler output
    llvm-svn: 150541

--- clang-tests/gcc-4_2-testsuite/src/obj-c++.dg/cond-expr-warn.mm
+++ clang-tests/gcc-4_2-testsuite/src/obj-c++.dg/cond-expr-warn.mm
@@ -6,5 +6,5 @@
 
 void foo (int i, NSKey *NSKeyValueCoding_NullValue, UpdatesList *nukedUpdatesList)
 {
-  i ? NSKeyValueCoding_NullValue : nukedUpdatesList; /* { dg-warning "conditional expression" } */
+  i ? NSKeyValueCoding_NullValue : nukedUpdatesList; /* { dg-error "incompatible operand types" } */
 }

4358c13faab296c3294370ea14171e8d3d2aefbd
David Dean <david_dean@apple.com>
2012-03-27 23:39:02 +0000

    Update test to match change in compiler output
    llvm-svn: 153546

--- clang-tests/gcc-4_2-testsuite/src/gcc.dg/Wswitch-enum.c
+++ clang-tests/gcc-4_2-testsuite/src/gcc.dg/Wswitch-enum.c
@@ -50,13 +50,13 @@ foo (int i, int j, enum e ei, enum e ej, enum e ek, enum e el,
     {
     case e1: return 1;
     case e2: return 2;
-    case 3: return 3; /* { dg-warning "case value not in enumerated type 'e'" } */
+    case 3: return 3; /* { dg-warning "case value not in enumerated type" } */
     }
   switch (ep)
     {
     case e1: return 1;
     case e2: return 2;
-    case 3: return 3; /* { dg-warning "case value not in enumerated type 'e'" } */
+    case 3: return 3; /* { dg-warning "case value not in enumerated type" } */
     default: break;
     }
   return 0;

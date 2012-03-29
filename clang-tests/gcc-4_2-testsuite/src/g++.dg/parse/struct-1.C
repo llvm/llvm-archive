8934b87b4fcc49eee4a88f186ca1b6c233f442f4
David Dean <david_dean@apple.com>
2012-03-29 16:47:51 +0000

    Update test to match change in compiler output
    llvm-svn: 153656

--- clang-tests/gcc-4_2-testsuite/src/g++.dg/parse/struct-1.C
+++ clang-tests/gcc-4_2-testsuite/src/g++.dg/parse/struct-1.C
@@ -1,4 +1,4 @@
 // Origin: Volker Reichelt <reichelt@igpm.rwth-aachen.de>
 // PR c++/18731
 
-template<typename T> struct T::A {}; // { dg-error "invalid class name" }
+template<typename T> struct T::A {}; // { dg-error "nested name specifier 'T::' for declaration does not refer into a class, class template or class template partial specialization" }

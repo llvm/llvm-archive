ffe262cd424bb23a9697695deb8725279c3ea404
Bill Wendling <isanbard@gmail.com>
2012-04-18 21:39:01 +0000

    Creating release_31 branch
    llvm-svn: 155055

--- clang-tests/gcc-4_2-testsuite/src/g++.dg/parse/struct-1.C
+++
@@ -1,4 +0,0 @@
-// Origin: Volker Reichelt <reichelt@igpm.rwth-aachen.de>
-// PR c++/18731
-
-template<typename T> struct T::A {}; // { dg-error "nested name specifier 'T::' for declaration does not refer into a class, class template or class template partial specialization" }

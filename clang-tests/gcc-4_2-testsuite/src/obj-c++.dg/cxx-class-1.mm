cfcb0cd9565cb8ac3f70f35a180afbfef2a665d7
Fariborz Jahanian <fjahanian@apple.com>
2012-05-16 23:43:10 +0000

    remove extra semi.
    llvm-svn: 156964

--- clang-tests/gcc-4_2-testsuite/src/obj-c++.dg/cxx-class-1.mm
+++ clang-tests/gcc-4_2-testsuite/src/obj-c++.dg/cxx-class-1.mm
@@ -3,16 +3,16 @@
 
 struct foo
 {
-  foo(void *a) {};
+  foo(void *a) {}
 };
 
 struct bar : foo
 {
-  bar() : foo((char*)0) {};
+  bar() : foo((char*)0) {}
 };
 
 class apple : foo
 {
 public:
-  apple() : foo(0) { };
+  apple() : foo(0) { }
 };

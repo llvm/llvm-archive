17779f1ced6b4b3089eb6fddff4139f21452bfa9
Anna Zaks <ganna@apple.com>
2012-05-19 05:08:39 +0000

    Make sure we produce executable: Remove dependency on non existant SDK and fix main signature
    llvm-svn: 157098

--- clang-tests/gcc-4_2-testsuite/src/g++.apple/debug.C
+++ clang-tests/gcc-4_2-testsuite/src/g++.apple/debug.C
@@ -1,6 +1,5 @@
 /* APPLE LOCAL file 10.5 debug mode 6621704 */
 /* { dg-do run { target *-*-darwin* } } */
-/* { dg-options "-isysroot /Developer/SDKs/MacOSX10.5.sdk -mmacosx-version-min=10.5" } */
 
 #define _GLIBCXX_DEBUG 1
 
@@ -16,7 +15,7 @@ using namespace std;
 string a1;
 map<int, int> m1;
 
-main() {
+int main(int argc, char *argv[]) {
   m[1]=2;
   a += "World\n";
   m1[1]=2;

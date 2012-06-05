e76f7211fecde592b6c65c9d505308fcc95255b3
Fariborz Jahanian <fjahanian@apple.com>
2012-06-05 22:55:56 +0000

    Fix test.
    llvm-svn: 158033

--- clang-tests/gcc-4_2-testsuite/src/objc.dg/format-security-attribute-3.m
+++ clang-tests/gcc-4_2-testsuite/src/objc.dg/format-security-attribute-3.m
@@ -1,9 +1,10 @@
 /* APPLE LOCAL file radar 5096648 */
-#import <CoreFoundation/CFString.h>
 /* Test for implementation of (__format__ (__CFString__, m, n)) attribute */
 /* { dg-options "-fconstant-cfstrings -Wformat -Wformat-security" } */
 /* { dg-do compile { target *-*-darwin* } } */
 
+typedef const struct __CFString * CFStringRef;
+#define CFSTR(cStr)  ((CFStringRef) __builtin___CFStringMakeConstantString ("" cStr "")) // expected-warning {{expanded from macro 'CFSTR'}}
 #define SECURITY_ATTR    __attribute__ ((__format__ (__CFString__, 1, 2)))
 
 extern void CFLog(CFStringRef format, ...) SECURITY_ATTR;
@@ -15,9 +16,9 @@ int main()
     CFStringRef foo;
         CFLog (foo); /* { dg-warning "format string is not a string literal" } */
     CFLog (foo, d);  // ok
-    CFLog(CFSTR("foo is %@"), CFSTR("foo is %@"), foo); // OK
+    CFLog(CFSTR("foo is %@"), CFSTR("foo is %@"), foo); // expected-warning {{data argument not used by format string}}
     CFLog(CFSTR("foo is %@"), CFSTR("foo is %@")); // OK
-    CFLog(CFSTR("foo is %@"), CFSTR("foo is %@"), foo); // OK
-    CFLog(CFSTR("foo is %@")); // OK
+    CFLog(CFSTR("foo is %@"), CFSTR("foo is %@"), foo); // expected-warning {{data argument not used by format string}}
+    CFLog(CFSTR("foo is %@")); // expected-warning {{more '%' conversions than data arguments}}
 }
 

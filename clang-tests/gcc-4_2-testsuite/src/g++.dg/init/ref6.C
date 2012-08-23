82bb429945632a24ed9e538e75488fecc74f0c1f
Fariborz Jahanian <fjahanian@apple.com>
2012-08-23 17:38:51 +0000

    Fix test for new warning.
    llvm-svn: 162449

--- clang-tests/gcc-4_2-testsuite/src/g++.dg/init/ref6.C
+++ clang-tests/gcc-4_2-testsuite/src/g++.dg/init/ref6.C
@@ -4,7 +4,7 @@ struct B {
 
 struct A {
   void f() {
-    B &b = b;
+    B &b = b; // { dg-warning "reference 'b' is not yet bound to a value when used within its own initialization" }
     b.g();
   }
 };

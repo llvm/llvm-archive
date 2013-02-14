eb82d6bda240c7c06e1617acf151b23a5f35adc0
Jordan Rose <jordan_rose@apple.com>
2013-02-14 18:17:54 +0000

    Escape '+' in dg-warning to fix buildbot.
    llvm-svn: 175188

--- clang-tests/gcc-4_2-testsuite/src/gcc.dg/cpp/ucnid-6.c
+++ clang-tests/gcc-4_2-testsuite/src/gcc.dg/cpp/ucnid-6.c
@@ -2,4 +2,4 @@
 /* { dg-options "-std=c89" } */
 #define a b(
 #define b(x) q
-int a\u00aa); /* { dg-warning "universal character names are only valid in C99 or C++" } */
+int a\u00aa); /* { dg-warning "universal character names are only valid in C99 or C\+\+" } */

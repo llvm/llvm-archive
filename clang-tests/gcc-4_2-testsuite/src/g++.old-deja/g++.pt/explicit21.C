c629b114b1d24a0c9993dbf9c483b0685558dfb8
Fariborz Jahanian <fjahanian@apple.com>
2013-02-21 17:27:13 +0000

    fix up test. // rdar://13145317
    llvm-svn: 175772

--- clang-tests/gcc-4_2-testsuite/src/g++.old-deja/g++.pt/explicit21.C
+++ clang-tests/gcc-4_2-testsuite/src/g++.old-deja/g++.pt/explicit21.C
@@ -1,4 +1,4 @@
-// { dg-do assemble  }
+// { dg-do compile  }
 // GROUPS passed templates
 template <class T>
 T foo(T* t);
@@ -8,4 +8,4 @@ int foo<char>(char c); // { dg-error "" } does not match declaration.
 
 template <>      // { dg-error "" }
 int bar<char>(); // { dg-error "" } no template bar.
-		 // { dg-error "" "" { target *-*-* } 10 } */
+		 // { dg-error "" "" { target *-*-* } 10 }  bogus code

b2a9115d5599fb52a052fc0964bf031bdc59e5e6
Fariborz Jahanian <fjahanian@apple.com>
2013-02-22 17:04:53 +0000

    fixes test to match recent changes in clang.
    llvm-svn: 175899

--- clang-tests/gcc-4_2-testsuite/src/g++.old-deja/g++.robertl/eb8.C
+++ clang-tests/gcc-4_2-testsuite/src/g++.old-deja/g++.robertl/eb8.C
@@ -2,8 +2,8 @@
 
 class foo {
 public:
-  operator <<(const void *);  //{ dg-error "" } no return type
   operator <<(char *);        //{ dg-error "" } no return type
+  operator <<(const void *);  //{ dg-error "" } no return type
 };
 
 void main()		      // { dg-error "must return .int" }

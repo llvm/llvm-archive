097ca589ff86840073db364172f021529fb1cf08
Gael Thomas <gael.thomas@lip6.fr>
2014-09-19 20:12:14 +0000

    Create a branch to test the aot
    llvm-svn: 218146

--- dragonegg/test/validator/c/VariableAlias.c
+++
@@ -1,9 +0,0 @@
-// RUN: %dragonegg -S %s -o - | FileCheck %s
-
-int qux;
-extern int foo __attribute__ ((alias ("qux")));
-// CHECK: @foo = alias i32* @qux
-extern int bar __attribute__ ((weak, alias ("foo")));
-// CHECK: @bar = weak alias i32* @qux
-extern int baz __attribute__ ((alias ("bar")));
-// CHECK: @baz = alias i32* @qux
